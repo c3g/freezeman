3259e28fe77908ae48bac704cd74848b36b0f945
ksenia1zaytseva <ksenia1zaytseva@gmail.com>
2020-04-06 19:25:50 -0400

    set comment for revision when importing from template

--- fms_core/resources.py
+++ fms_core/resources.py
@@ -1,5 +1,6 @@
 from import_export import resources
 from .models import Container, Sample, Individual
+import reversion
 
 
 class ContainerResource(resources.ModelResource):
@@ -9,6 +10,11 @@ class ContainerResource(resources.ModelResource):
         import_id_fields = ('barcode',)
         fields = ('kind', 'name', 'barcode', 'location','coordinates',)
         clean_model_instances = True
+        skip_unchanged = True
+
+    def after_save_instance(self, instance, dry_run, using_transactions=False):
+        if not dry_run:
+            reversion.set_comment("Imported from template.")
 
 
 class SampleResource(resources.ModelResource):
@@ -17,6 +23,7 @@ class SampleResource(resources.ModelResource):
         model = Sample
         import_id_fields = ('name',)
         clean_model_instances = True
+        skip_unchanged = True
 
 
 class IndividualResource(resources.ModelResource):
@@ -25,3 +32,4 @@ class IndividualResource(resources.ModelResource):
         model = Individual
         import_id_fields = ('participant_id',)
         clean_model_instances = True
+        skip_unchanged = True
