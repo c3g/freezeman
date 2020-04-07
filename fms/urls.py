57a2d4a1138a08574b61bcec736dd3c4e819edbf
David Lougheed <david.lougheed@gmail.com>
2020-04-07 18:35:39 -0400

    rearrange urls

--- fms/urls.py
+++ fms/urls.py
@@ -5,8 +5,8 @@ from rest_framework_simplejwt import views as jwt_views
 from fms_core.router import router
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include(router.urls)),
+    path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
