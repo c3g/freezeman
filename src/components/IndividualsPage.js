b4b97972e98caca2eb663169d769b15c57d8dadb
David Lougheed <david.lougheed@gmail.com>
2020-04-16 18:32:02 -0400

    Add individual table functionality

--- src/components/IndividualsPage.js
+++ src/components/IndividualsPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {connect} from "react-redux";
 
 import {Table} from "antd";
 import "antd/es/table/style/css";
@@ -8,20 +9,42 @@ import PageContainer from "./PageContainer";
 
 const TABLE_COLUMNS = [
     {
-        title: "ID",
-        dataIndex: "participant_id"
-    },
-    {
         title: "Name",
         dataIndex: "name"
     },
     {
         title: "Taxon",
-        dataIndex: "taxon"
+        dataIndex: "taxon",
+        filters: [
+            {
+                text: "Homo sapiens",
+                value: "Homo sapiens",
+            },
+            {
+                text: "Mus musculus",
+                value: "Mus musculus",
+            },
+        ],
+        onFilter: (value, record) => record.taxon === value,
     },
     {
         title: "Sex",
-        dataIndex: "sex"
+        dataIndex: "sex",
+        filters: [
+            {
+                text: "Male",
+                value: "M",
+            },
+            {
+                text: "Female",
+                value: "F",
+            },
+            {
+                text: "Unknown",
+                value: "Unknown",
+            },
+        ],
+        onFilter: (value, record) => record.sex === value,
     },
     {
         title: "Pedigree",
@@ -33,11 +56,21 @@ const TABLE_COLUMNS = [
     }
 ];
 
-const IndividualsPage = () => <PageContainer>
+const IndividualsPage = ({individuals, isFetching}) => <PageContainer>
     <AppPageHeader title="Individuals" />
     <div style={{padding: "16px 24px 8px 24px", overflowX: "auto"}}>
-        <Table size="small" bordered={true} columns={TABLE_COLUMNS} />
+        <Table size="small"
+               bordered={true}
+               columns={TABLE_COLUMNS}
+               dataSource={individuals}
+               rowKey="name"
+               loading={isFetching} />
     </div>
 </PageContainer>;
 
-export default IndividualsPage;
+const mapStateToProps = state => ({
+    individuals: state.individuals.items,
+    isFetching: state.individuals.isFetching,
+});
+
+export default connect(mapStateToProps)(IndividualsPage);
