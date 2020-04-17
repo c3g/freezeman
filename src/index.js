fbac7fc4c8b76e5fc0aaef9f68a27675e7a36d1e
David Lougheed <david.lougheed@gmail.com>
2020-04-17 11:21:00 -0400

    Persist auth

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import {render} from "react-dom";
 
+import {persistStore} from "redux-persist";
+import {PersistGate} from "redux-persist/integration/react";
+
 import {applyMiddleware, createStore, compose} from "redux";
 import thunkMiddleware from "redux-thunk";
 
@@ -14,13 +17,16 @@ import rootReducer from "./reducers";
 // noinspection JSUnresolvedVariable
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunkMiddleware)));
+const persistor = persistStore(store);
 
 document.addEventListener("DOMContentLoaded", () => {
     render(
         <Provider store={store}>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
+            <PersistGate loading={null} persistor={persistor}>
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            </PersistGate>
         </Provider>,
         document.getElementById("root")
     );
--- src/reducers.js
+++ src/reducers.js
@@ -1,4 +1,6 @@
 import {combineReducers} from "redux";
+import {persistReducer} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import {auth} from "./modules/auth/reducers";
 import {containerKinds, containers} from "./modules/containers/reducers";
@@ -7,8 +9,14 @@ import {samples} from "./modules/samples/reducers";
 import {users} from "./modules/users/reducers";
 import {versions} from "./modules/versions/reducers";
 
+const AUTH_PERSIST_CONFIG = {
+    key: "auth",
+    blacklist: ["isFetching"],
+    storage,
+};
+
 export default combineReducers({
-    auth,
+    auth: persistReducer(AUTH_PERSIST_CONFIG, auth),
     containerKinds,
     containers,
     individuals,
