ae97db635a460d3bce08e1ebc4ab059c58f15ac8
ksenia1zaytseva <ksenia1zaytseva@gmail.com>
2020-04-28 09:27:24 -0400

    add save file to process_import

--- fms_core/utils_admin.py
+++ fms_core/utils_admin.py
@@ -1,7 +1,13 @@
 import tablib
+import os
+import time
+from django.conf import settings
+from django.core.exceptions import PermissionDenied
+from django.utils.encoding import force_str
 from import_export.admin import ImportMixin, ExportActionMixin, ExportMixin
 from import_export.formats.base_formats import CSV, TablibFormat
 from reversion.admin import VersionAdmin
+from .models import ImportedFile
 
 
 __all__ = [
@@ -50,6 +56,42 @@ class PaddedXLSX(TablibFormat):
 class CustomImportMixin(ImportMixin):
     formats = [CSV, PaddedXLSX]
     change_list_template = "admin/fms_core/download_import.html"
+
+    def process_import(self, request, *args, **kwargs):
+        """
+        Perform the actual import action (after the user has confirmed the import)
+        """
+        if not self.has_import_permission(request):
+            raise PermissionDenied
+
+        form_type = self.get_confirm_import_form()
+        confirm_form = form_type(request.POST)
+        if confirm_form.is_valid():
+            import_formats = self.get_import_formats()
+            input_format = import_formats[
+                int(confirm_form.cleaned_data['input_format'])
+            ]()
+            tmp_storage = self.get_tmp_storage_class()(name=confirm_form.cleaned_data['import_file_name'])
+            data = tmp_storage.read(input_format.get_read_mode())
+            if not input_format.is_binary() and self.from_encoding:
+                data = force_str(data, self.from_encoding)
+            new_path = os.path.join(settings.MEDIA_ROOT, 'uploads/')
+            dataset = input_format.create_dataset(data)
+            time_string = time.strftime("%Y%m%d-%H%M%S")
+            get_user_name = request.user.username
+            new_file_name = confirm_form.cleaned_data['original_file_name'].split('.xlsx')[0]\
+                            + time_string +f"_{get_user_name}" + '.xlsx'
+            file_path = os.path.join(new_path, new_file_name)
+            with open(file_path, 'wb') as f_output:
+                f_output.write(dataset.xlsx)
+                imf = ImportedFile.objects.create(filename=new_file_name)
+                print(imf.__dict__)
+
+            result = self.process_dataset(dataset, confirm_form, request, *args, **kwargs)
+
+            tmp_storage.remove()
+
+            return self.process_result(result, request)
 
 
 class AggregatedAdmin(CustomImportMixin, ExportActionMixin, ExportMixin, VersionAdmin):
