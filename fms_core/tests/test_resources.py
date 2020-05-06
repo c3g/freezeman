566790ec73ac61f1feeadef357c3b7920506de03
David Lougheed <david.lougheed@gmail.com>
2020-05-06 17:34:43 -0400

    Test sample update & container move resources Fix example CSVs

--- fms_core/tests/test_resources.py
+++ fms_core/tests/test_resources.py
@@ -1,11 +1,19 @@
 import os
 import reversion
+from decimal import Decimal
 from django.test import TestCase
 from pathlib import Path
 from tablib import Dataset
 
 from ..models import Container, Sample, ExtractedSample
-from ..resources import skip_rows, ContainerResource, SampleResource, ExtractionResource
+from ..resources import (
+    skip_rows,
+    ContainerResource,
+    ExtractionResource,
+    ContainerMoveResource,
+    SampleResource,
+    SampleUpdateResource,
+)
 
 
 def get_ds():
@@ -32,6 +40,17 @@ class ResourcesTestCase(TestCase):
         self.cr = ContainerResource()
         self.sr = SampleResource()
         self.er = ExtractionResource()
+        self.ur = SampleUpdateResource()
+        self.mr = ContainerMoveResource()
+
+    def load_samples(self):
+        with reversion.create_revision(manage_manually=True), \
+                open(os.path.join(APP_DATA_ROOT, "containers.csv")) as cf, \
+                open(os.path.join(APP_DATA_ROOT, "samples.csv")) as sf:
+            c = Dataset().load(cf.read())
+            self.cr.import_data(c, raise_errors=True)
+            s = Dataset().load(sf.read())
+            self.sr.import_data(s, raise_errors=True)
 
     def test_skip_rows(self):
         ds = get_ds()
@@ -50,25 +69,39 @@ class ResourcesTestCase(TestCase):
             self.assertEqual(len(Container.objects.all()), 4)
 
     def test_sample_import(self):
-        with reversion.create_revision(manage_manually=True), \
-                open(os.path.join(APP_DATA_ROOT, "containers.csv")) as cf, \
-                open(os.path.join(APP_DATA_ROOT, "samples.csv")) as sf:
-            c = Dataset().load(cf.read())
-            self.cr.import_data(c, raise_errors=True)
-            s = Dataset().load(sf.read())
-            self.sr.import_data(s, raise_errors=True)
+        with reversion.create_revision(manage_manually=True):
+            self.load_samples()
             self.assertEqual(len(Sample.objects.all()), 2)
 
     def test_sample_extraction_import(self):
         with reversion.create_revision(manage_manually=True), \
-                open(os.path.join(APP_DATA_ROOT, "containers.csv")) as cf, \
-                open(os.path.join(APP_DATA_ROOT, "samples.csv")) as sf, \
                 open(os.path.join(APP_DATA_ROOT, "extractions.csv")) as ef:
-            c = Dataset().load(cf.read())
-            self.cr.import_data(c, raise_errors=True)
-            s = Dataset().load(sf.read())
-            self.sr.import_data(s, raise_errors=True)
+            self.load_samples()
+
             e = Dataset().load(ef.read())
             self.er.import_data(e, raise_errors=True)
+
             self.assertEqual(len(Sample.objects.all()), 4)
             self.assertEqual(len(ExtractedSample.objects.all()), 2)
+
+    def test_sample_update(self):
+        with reversion.create_revision(manage_manually=True), \
+                open(os.path.join(APP_DATA_ROOT, "sample_update.csv")) as uf:
+            self.load_samples()
+            u = Dataset().load(uf.read())
+            self.ur.import_data(u, raise_errors=True)
+
+            s = Sample.objects.get(container_id="tube001")
+            self.assertEqual(s.concentration, Decimal("0.001"))
+
+    def test_container_move(self):
+        with reversion.create_revision(manage_manually=True), \
+                open(os.path.join(APP_DATA_ROOT, "containers.csv")) as cf, \
+                open(os.path.join(APP_DATA_ROOT, "container_move.csv")) as mf:
+            c = Dataset().load(cf.read())
+            self.cr.import_data(c, raise_errors=True)
+            m = Dataset().load(mf.read())
+            self.mr.import_data(m, raise_errors=True)
+
+            ci = Container.objects.get(barcode="tube001")
+            self.assertEqual(ci.coordinates, "D05")
