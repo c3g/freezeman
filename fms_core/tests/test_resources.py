1ae68c654ae3c373f9260d71ba29c44debfb4e2f
David Lougheed <david.lougheed@gmail.com>
2020-05-06 14:17:26 -0400

    Add basic tests for container/sample resources

--- fms_core/tests/test_resources.py
+++ fms_core/tests/test_resources.py
@@ -1,6 +1,11 @@
+import os
+import reversion
 from django.test import TestCase
+from pathlib import Path
 from tablib import Dataset
-from ..resources import skip_rows
+
+from ..models import Container, Sample
+from ..resources import skip_rows, ContainerResource, SampleResource
 
 
 def get_ds():
@@ -19,7 +24,14 @@ CSV_1 = """#,good 1,good 2,good 3
 """
 
 
+APP_DATA_ROOT = Path(__file__).parent.parent / "example_data" / "csv"
+
+
 class ResourcesTestCase(TestCase):
+    def setUp(self) -> None:
+        self.cr = ContainerResource()
+        self.sr = SampleResource()
+
     def test_skip_rows(self):
         ds = get_ds()
         old_csv = ds.export("csv")
@@ -29,3 +41,19 @@ class ResourcesTestCase(TestCase):
 
         skip_rows(ds, num_rows=2, col_skip=1)
         self.assertEqual(ds.export("csv").replace("\r", ""), CSV_1)
+
+    def test_container_import(self):
+        with reversion.create_revision(manage_manually=True), open(os.path.join(APP_DATA_ROOT, "containers.csv")) as cf:
+            ds = Dataset().load(cf.read())
+            self.cr.import_data(ds, raise_errors=True)
+            self.assertEqual(len(Container.objects.all()), 4)
+
+    def test_sample_import(self):
+        with reversion.create_revision(manage_manually=True), \
+                open(os.path.join(APP_DATA_ROOT, "containers.csv")) as cf, \
+                open(os.path.join(APP_DATA_ROOT, "samples.csv")) as sf:
+            c = Dataset().load(cf.read())
+            self.cr.import_data(c, raise_errors=True)
+            s = Dataset().load(sf.read())
+            self.sr.import_data(s, raise_errors=True)
+            self.assertEqual(len(Sample.objects.all()), 2)
