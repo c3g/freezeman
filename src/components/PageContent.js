18d69488446a7b146c4cb194e5ee0c3344553981
Rom Grk <romgrk.cc@gmail.com>
2020-06-15 21:19:44 -0400

    PageContent: add loading prop

--- src/components/PageContent.js
+++ src/components/PageContent.js
@@ -1,16 +1,25 @@
 import React from "react";
 
+import {Spin} from "antd";
+import "antd/es/spin/style/css";
+
 const defaultStyle = {
   flex: "1",
   padding: "16px 24px 24px 24px",
   overflow: "auto",
 };
 
-const PageContent = ({children, style}) =>
+const PageContent = ({children, style, loading}) =>
   <div
     style={style ? {...defaultStyle, ...style} : defaultStyle}
   >
-    {children}
+    {loading ?
+      <Spin>
+        {children}
+      </Spin>
+      :
+      children
+    }
   </div>;
 
 export default PageContent;
