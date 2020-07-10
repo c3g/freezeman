24db08bacf4406797c09ee220731a015aaf41616
David Lougheed <david.lougheed@gmail.com>
2020-07-10 09:45:12 -0400

    Fix resources error in test

--- fms_core/resources.py
+++ fms_core/resources.py
@@ -352,6 +352,7 @@ class ExtractionResource(GenericResource):
 
     class Meta:
         model = Sample
+        import_id_fields = ()
         fields = (
             'biospecimen_type',
             'volume_used',
