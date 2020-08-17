b82c741f6d525b48d1a0ba5de888d798d1fa7b47
Rom Grk <romgrk.cc@gmail.com>
2020-08-17 14:24:22 -0400

    samples list: label containers by name

--- src/components/samples/SamplesListContent.js
+++ src/components/samples/SamplesListContent.js
@@ -10,60 +10,10 @@ import {SampleDepletion} from "./SampleDepletion";
 import {list, listTemplateActions} from "../../modules/samples/actions";
 import {actionsToButtonList} from "../../utils/templateActions";
 
-const TABLE_COLUMNS = [
-  {
-    title: "Type",
-    dataIndex: "biospecimen_type",
-    width: 80,
-  },
-  {
-    title: "Name",
-    dataIndex: "name",
-    render: (name, sample) => <Link to={`/samples/${sample.id}`}>{name}</Link>,
-  },
-  {
-    title: "Alias",
-    dataIndex: "alias",
-  },
-  {
-    title: "Individual",
-    dataIndex: "individual",
-    render: individual => <Link to={`/individuals/${individual}`}>{individual}</Link>,
-  },
-  {
-    title: "Container",
-    dataIndex: "container",
-    // TODO: Render barcode here
-    render: container => <Link to={`/containers/${container}`}>{container}</Link>,
-  },
-  {
-    title: "Coords",
-    dataIndex: "coordinates",
-    width: 70,
-  },
-  {
-    title: "Vol. (µL)",
-    dataIndex: "volume_history",
-    render: vh => parseFloat(vh[vh.length - 1].volume_value).toFixed(3),
-    width: 100,
-  },
-  {
-    title: "Conc. (ng/µL)",
-    dataIndex: "concentration",
-    render: conc => conc === null ? "—" : parseFloat(conc).toFixed(3),
-    width: 115,
-  },
-  {
-    title: "Depleted",
-    dataIndex: "depleted",
-    render: depleted => <SampleDepletion depleted={depleted} />,
-    width: 85,
-  }
-];
-
 const mapStateToProps = state => ({
   samplesByID: state.samples.itemsByID,
   samples: state.samples.items,
+  containersByID: state.containers.itemsByID,
   actions: state.sampleTemplateActions,
   page: state.samples.page,
   totalCount: state.samples.totalCount,
@@ -75,6 +25,7 @@ const actionCreators = {list, listTemplateActions};
 const SamplesListContent = ({
   samples,
   samplesByID,
+  containersByID,
   actions,
   isFetching,
   page,
@@ -86,6 +37,61 @@ const SamplesListContent = ({
     // Must be wrapped; effects cannot return promises
     listTemplateActions();
   }, []);
+
+  const TABLE_COLUMNS = [
+    {
+      title: "Type",
+      dataIndex: "biospecimen_type",
+      width: 80,
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+      render: (name, sample) => <Link to={`/samples/${sample.id}`}>{name}</Link>,
+    },
+    {
+      title: "Alias",
+      dataIndex: "alias",
+    },
+    {
+      title: "Individual",
+      dataIndex: "individual",
+      render: individual => <Link to={`/individuals/${individual}`}>{individual}</Link>,
+    },
+    {
+      title: "Container",
+      dataIndex: "container",
+      render: containerID =>
+        <Link to={`/containers/${containerID}`}>
+          {containersByID[containerID].name}
+        </Link>,
+    },
+    {
+      title: "Coords",
+      dataIndex: "coordinates",
+      width: 70,
+    },
+    {
+      title: "Vol. (µL)",
+      dataIndex: "volume_history",
+      render: vh => parseFloat(vh[vh.length - 1].volume_value).toFixed(3),
+      width: 100,
+    },
+    {
+      title: "Conc. (ng/µL)",
+      dataIndex: "concentration",
+      render: conc => conc === null ? "—" : parseFloat(conc).toFixed(3),
+      width: 115,
+    },
+    {
+      title: "Depleted",
+      dataIndex: "depleted",
+      render: depleted => <SampleDepletion depleted={depleted} />,
+      width: 85,
+    }
+  ];
+
+
   return <>
     <AppPageHeader title="Samples & Extractions" extra={actionsToButtonList("/samples", actions)} />
     <PageContent>
