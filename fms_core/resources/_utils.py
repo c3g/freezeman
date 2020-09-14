6a3442767eea3c6dfe17052e4dc77ef5a6621ff0
Ulysse Fortier-Gauthier <ulysse.fortiergauthier@mcgill.ca>
2020-09-14 09:53:07 -0400

    Modifications to simplify the preview after dry run.

--- fms_core/resources/_utils.py
+++ fms_core/resources/_utils.py
@@ -6,6 +6,7 @@ from ..utils import str_normalize
 __all__ = [
     "get_container_pk",
     "skip_rows",
+    "remove_column_from_results",
 ]
 
 
@@ -28,3 +29,11 @@ def skip_rows(dataset: Dataset, num_rows: int = 0, col_skip: int = 1) -> None:
         if len(vals) == 1 and "" in vals:
             continue
         dataset.append(tuple(str_normalize(c) if isinstance(c, str) else ("" if c is None else c) for c in r))
+
+
+def remove_column_from_results(column_name: str, results):
+    index_column = results.diff_headers.index(column_name)
+    results.diff_headers.remove(column_name)
+    for row in results.rows:
+        row.diff.pop(index_column)
+    return results
