fca3a705488b602d8a9324c57f48ecec2c2f4246
jleblond <jasmine.leblondc@gmail.com>
2020-12-22 17:19:51 -0500

    draft: collapse some filters

--- src/components/samples/SamplesFilters.js
+++ src/components/samples/SamplesFilters.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 import {connect} from "react-redux";
 import {list, setFilter} from "../../modules/samples/actions";
 import FilterGroup from "../filters/FilterGroup";
 import {SAMPLE_FILTERS} from "../filters/descriptions";
-
+import { DownOutlined } from "@ant-design/icons";
 
 const mapStateToProps = state => ({
   filters: state.samples.filters,
@@ -16,16 +16,25 @@ const SamplesFilters = ({
   setFilter,
   list,
 }) => {
+  const [showDetailedFilters, setShowDetailedFilters] = useState(false);
+
   const onChangeFilter = (filter, value) => {
     setFilter(filter.key, value)
     setTimeout(() => {list()}, 500)
   }
 
-  return <FilterGroup
-    descriptions={SAMPLE_FILTERS}
-    values={filters}
-    onChangeFilter={onChangeFilter}
-  />;
+  return <div>
+    <div onClick={() => setShowDetailedFilters(!showDetailedFilters)}> <DownOutlined /> </div>
+    {
+      showDetailedFilters
+        ?     <FilterGroup
+          descriptions={SAMPLE_FILTERS}
+          values={filters}
+          onChangeFilter={onChangeFilter}
+        />
+        : ''
+    }
+  </div>;
 }
 
 export default connect(mapStateToProps, actionCreators)(SamplesFilters);
