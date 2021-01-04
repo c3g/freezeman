9fdfc0d02c6ac1cee24f94cb5a4b30ff26dd2fd4
Rom Grk <romgrk.cc@gmail.com>
2021-01-04 08:44:49 -0500

    fix: ContainersFilters: use debounce function & add clear button

--- src/components/containers/ContainersFilters.js
+++ src/components/containers/ContainersFilters.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, {useCallback} from "react";
 import {connect} from "react-redux";
-import {list, setFilter} from "../../modules/containers/actions";
+import {debounce} from "debounce";
+import {Button} from "antd";
+
+import {list, setFilter, clearFilters} from "../../modules/containers/actions";
 import FilterGroup from "../filters/FilterGroup";
 import {CONTAINER_FILTERS} from "../filters/descriptions";
 
@@ -9,26 +12,32 @@ const mapStateToProps = state => ({
   filters: state.containers.filters,
 });
 
-const actionCreators = {setFilter, list};
+const actionCreators = {setFilter, clearFilters, list};
 
 const ContainersFilters = ({
   filters,
   setFilter,
+  clearFilters,
   list,
 }) => {
+  const listDebounced = useCallback(debounce(list, 250), [list])
   const onChangeFilter = (filter, value) => {
     setFilter(filter.key, value)
-    setTimeout(() => {list()}, 500)
+    listDebounced()
   }
 
-  return <div style={{textAlign: 'center'}}>
-    <FilterGroup
-      descriptions={CONTAINER_FILTERS}
-      values={filters}
-      onChangeFilter={onChangeFilter}
-    />
-  </div>;
-
+  return (
+    <div>
+      <FilterGroup
+        descriptions={CONTAINER_FILTERS}
+        values={filters}
+        onChangeFilter={onChangeFilter}
+      />
+      <Button onClick={clearFilters}>
+        Clear
+      </Button>
+    </div>
+  );
 }
 
 export default connect(mapStateToProps, actionCreators)(ContainersFilters);
