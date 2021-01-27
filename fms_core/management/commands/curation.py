8b7c4695eb84025d5c7df2371a285c3f16752bb8
UlysseFG <ulysse.fortiergauthier@mcgill.ca>
2021-01-27 12:45:32 -0500

    Adresses PR issues

--- fms_core/management/commands/curation.py
+++ fms_core/management/commands/curation.py
@@ -4,6 +4,7 @@ import json
 from os.path import expanduser
 import os
 import time
+import platform
 import datetime
 import logging
 import reversion
@@ -24,6 +25,9 @@ ACTION_ROLLBACK_EXTRACTION = "rollback_extraction"
 HOME = expanduser("~")
 CURATION_PATH = "/curation/"
 LOG_PATH = "log/"
+SERVER_PLATFORM = "Linux"  # Platform for the server
+SERVER_TZ = "America/Montreal"  # Local timezone
+REVERSION_ADMIN_USER = "biobankadmin"
 
 
 class Command(BaseCommand):
@@ -35,7 +39,10 @@ class Command(BaseCommand):
     }
 
     def init_logging(self, log_name, timestamp):
-        filename = HOME + CURATION_PATH + LOG_PATH + timestamp + "_" + log_name + ".log"
+        path = HOME + CURATION_PATH + LOG_PATH
+        if not os.path.exists(path):
+            os.makedirs(path)
+        filename = path + timestamp + "_" + log_name + ".log"
         formatter = logging.Formatter("%(asctime)s || %(levelname)s || %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
         handler = logging.FileHandler(filename, "w+")
         handler.setFormatter(formatter)
@@ -49,45 +56,53 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.stdout.write(self.style.SUCCESS("Started curation."))
+        if platform.system() == SERVER_PLATFORM:
+            os.environ["TZ"] = SERVER_TZ
+            time.tzset()
         # Copy the curation file to log directory with current timestamp matching log file
-        os.environ["TZ"] = "America/Montreal"
-        time.tzset()
-        timestamp = time.strftime("%Y-%m-%d_%H:%M:%S")
-        src_curation_file = HOME + CURATION_PATH + options["params_file"] + ".json"
-        dst_curation_file = HOME + CURATION_PATH + LOG_PATH + timestamp + "_" + options["params_file"] + ".json"
-        copyfile(src_curation_file, dst_curation_file)
-        # Use param file name to name log file
-        log = self.init_logging(options["params_file"], timestamp)
-        # log info identifying the curation
-        log.info("New curation started using identifier [" + options["params_file"] + "].")
-        # Extracting the curations to perform from file
-        with open(src_curation_file) as file:
-            params = json.load(file)
-        error_found = False
-        try:
-            with transaction.atomic():
-                # Create a curation revision to eventually rollback the current curation
-                with reversion.create_revision():
-                    # Launch each individual curation
-                    for curation in params:
-                        self.stdout.write(self.style.SUCCESS('Launching action [' + str(curation["curation_index"]) + '] "%s"' % curation["action"]) + '.')
-                        action = self.curation_switch.get(curation["action"])
-                        if action:
-                            curation_failed = action(curation, log)
-                            if curation_failed:
-                                self.stdout.write(self.style.ERROR("Action [" + str(curation_failed) + "] failed."))
-                                error_found = True
+        timestamp = time.strftime("%Y-%m-%d-%H-%M-%S")
+        src_path = HOME + CURATION_PATH
+        dst_path = HOME + CURATION_PATH + LOG_PATH
+        if not os.path.exists(dst_path):
+            os.makedirs(dst_path)
+        src_curation_file = src_path + options["params_file"] + ".json"
+        dst_curation_file = dst_path + timestamp + "_" + options["params_file"] + ".json"
+        if not os.path.exists(src_curation_file):
+            self.stdout.write(self.style.ERROR("Parameter file absent from the path : " + src_curation_file))
+        else:
+            copyfile(src_curation_file, dst_curation_file)
+            # Use param file name to name log file
+            log = self.init_logging(options["params_file"], timestamp)
+            # log info identifying the curation
+            log.info("New curation started using identifier [" + options["params_file"] + "].")
+            # Extracting the curations to perform from file
+            with open(src_curation_file) as file:
+                params = json.load(file)
+            error_found = False
+            try:
+                with transaction.atomic():
+                    # Create a curation revision to eventually rollback the current curation
+                    with reversion.create_revision():
+                        # Launch each individual curation
+                        for curation in params:
+                            self.stdout.write(self.style.SUCCESS('Launching action [' + str(curation["curation_index"]) + '] "%s"' % curation["action"]) + '.')
+                            action = self.curation_switch.get(curation["action"])
+                            if action:
+                                curation_failed = action(curation, log)
+                                if curation_failed:
+                                    self.stdout.write(self.style.ERROR("Action [" + str(curation_failed) + "] failed."))
+                                    error_found = True
+                                else:
+                                    self.stdout.write(self.style.SUCCESS("Action complete."))
                             else:
-                                self.stdout.write(self.style.SUCCESS("Action complete."))
-                        else:
-                            self.stdout.write(self.style.ERROR("Curation [" + str(curation["action"]) + "] do not exist."))
-                    reversion.set_user(User.objects.get(username="biobankadmin"))  # set biobankadmin (user 1) as user
-                    reversion.set_comment("Manual curation performed by Administrators.")
-                if error_found:
-                    raise IntegrityError
-                else:
-                    self.stdout.write(self.style.SUCCESS("Completed curation."))
-        except IntegrityError:
-            log.info("Curation operation transaction rolled back.")
+                                self.stdout.write(self.style.ERROR("Curation [" + str(curation["action"]) + "] do not exist."))
+                        reversion.set_user(User.objects.get(username=REVERSION_ADMIN_USER))  # set admin user as creator for revision
+                        reversion.set_comment("Manual curation performed by Administrators.")
+                    if error_found:
+                        raise IntegrityError
+                    else:
+                        self.stdout.write(self.style.SUCCESS("Completed curation."))
+            except IntegrityError:
+                log.info("Curation operation transaction rolled back.")
 
 
