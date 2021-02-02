167bf2de23be9038b62bd0003a16f27efd66a063
jleblond <jasmine.leblondc@gmail.com>
2021-02-02 10:19:17 -0500

    Display filters descriptions as strings

--- src/components/filters/FiltersDescriptions.js
+++ src/components/filters/FiltersDescriptions.js
@@ -5,12 +5,15 @@ const FiltersDescription = ({
   filters
 }) => {
   //TODO: display description label instead of the description key in <strong> {key}: </strong>
+
+  const appliedFilters = Object.keys(filters).filter(key => filters[key]?.value)
+
   return (
     <>
-      {Object.keys(filters).map(key => (
+      {appliedFilters.map(key => (
         <div>
           <strong>{key}: </strong>
-          {filters[key]?.value}
+          { [].concat(filters[key].value).join(', ') }
         </div>
       ))}
     </>
