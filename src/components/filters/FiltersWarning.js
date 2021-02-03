1abad591db66538a14adafbde55a2931e85e521d
Rom Grk <romgrk.cc@gmail.com>
2021-02-03 11:59:31 -0500

    feat: improve styling of tooltip trigger

--- src/components/filters/FiltersWarning.js
+++ src/components/filters/FiltersWarning.js
@@ -2,6 +2,7 @@ import React from "react";
 import {Tooltip, Typography} from "antd";
 import "antd/es/typography/style/css";
 const {Text} = Typography
+import {QuestionCircleOutlined} from "@ant-design/icons";
 
 import FiltersInfos from "./FiltersInfos";
 
@@ -14,13 +15,11 @@ export default function FiltersWarning({ nFilters, filters, description }) {
       <span style={{marginTop: 12}}>
         <Text type="warning"
           style={{
-            textDecoration: 'underline',
-            textDecorationStyle: 'dotted',
             marginLeft: 8,
             marginRight: 8,
           }}
         >
-          {nFilters} filter{nFilters > 1 ? 's' : ''} applied
+          {nFilters} filter{nFilters > 1 ? 's' : ''} applied <QuestionCircleOutlined />
         </Text>
       </span>
     </Tooltip>
