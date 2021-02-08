5c7636171c3f23c8f971f32546f62b6f4b430801
jleblond <jasmine.leblondc@gmail.com>
2021-02-08 10:57:52 -0500

    Handle Filters info values by Filter type

--- src/components/filters/FiltersInfos.js
+++ src/components/filters/FiltersInfos.js
@@ -7,12 +7,35 @@ const FiltersInfos = ({
 }) => {
   const appliedFilters = Object.keys(filters).filter(key => filters[key]?.value)
 
+  const getValue = (key) => {
+    const filterValue = filters[key].value
+
+    switch(description[key].type) {
+      case 'SELECT':
+        const valuesArray = [].concat(filterValue)
+        const labels = valuesArray.map((val) => {
+          const option = description[key].options.find(option => option.value === val)
+          return option.label
+        })
+        return labels.join(', ')
+        break;
+      case 'RANGE':
+        if (filterValue.min || filterValue.max)
+          return `${filterValue.min} to ${filterValue.max} `
+        else
+          throw new Error('MIN and MAX values not defined for Filter range')
+        break;
+      default:
+        return [].concat(filterValue).join(', ')
+    }
+  }
+
   return (
     <>
       {appliedFilters.map(key => (
         <div>
           <strong>{description[key].label}: </strong>
-          { [].concat(filters[key].value).join(', ') }
+          { getValue(key) }
         </div>
       ))}
     </>
