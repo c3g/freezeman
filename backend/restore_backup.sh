b2f09d5321006df8ab1d0b834df56186aac0cbd0
jleblond <jasmine.leblondc@gmail.com>
2021-03-15 12:04:34 -0400

    Script for backup restore

---
+++ backend/restore_backup.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -euf
+set -o pipefail
+
+################
+# 0. Variables #
+################
+
+# NOTE:
+# This assumes that `psql` has access without prompt to the `postgres` or
+# PG_DATABASE database. Variables PG_DATABASE, PG_USER or PG_PASSWORD may
+# need to be configured outside this script.
+
+export PG_DATABASE=${PG_DATABASE:-postgres}
+
+# Our database name
+export fms_database=fms
+
+# pgsql backup file to restore
+export backup_file=${1}
+
+
+#####################
+# 1. Setup database #
+#####################
+
+# Drop it & recreate it
+psql $PG_DATABASE -c "DROP DATABASE IF EXISTS $fms_database WITH (FORCE)"
+psql $PG_DATABASE -c "CREATE DATABASE $fms_database"
+psql -d $PG_DATABASE -f $backup_file
+
+# Manually enable fzy as it's not in the migrations
+psql $fms_database -c 'CREATE EXTENSION fzy;'
+
+
+##########################
+# 2. Setup django server #
+##########################
+
+# Set the DB name for django
+export PG_DATABASE=$fms_database
+
+# Apply migrations, create superuser & run the server
+echo "Applying migrations"
+python manage.py migrate
