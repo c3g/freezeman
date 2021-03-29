58a1600ae0b1f74170f201853a59c531c0a5b026
jleblond <jasmine.leblondc@gmail.com>
2021-03-29 17:24:42 -0400

    Use crum lib to get current user in Models

--- backend/fms_core/models/tracked_model.py
+++ backend/fms_core/models/tracked_model.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from crum import get_current_user
 
 ADMIN_USER = "biobankadmin"
+
 
 class TrackedModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, help_text="Date the instance was created.")
@@ -14,17 +16,25 @@ class TrackedModel(models.Model):
     class Meta:
         abstract = True
 
-    def clean(self):
+    def save(self, *args, **kwargs):
+        user = get_current_user()
+        if user and not user.pk:
+            user = User.objects.get(username=ADMIN_USER)
         # if the instance has not been saved to the DB yet
         if not self.id:
             # initialize the user that create the object.
-            self.created_by = request.user if request.user.is_authenticated else User.objects.get(username=ADMIN_USER)
+            self.created_by = user
         # Set modified by user each time we save
-        self.updated_by = request.user if request.user.is_authenticated else User.objects.get(username=ADMIN_USER)
+        from nose.tools import set_trace;set_trace();
+        self.updated_by = user
+        super().save(*args, **kwargs)
 
     def delete(self, using, keep_parents):
         # Set modified by user
-        self.updated_by = request.user if request.user.is_authenticated else User.objects.get(username=ADMIN_USER)
+        user = get_current_user()
+        if user and not user.pk:
+            user = User.objects.get(username=ADMIN_USER)
+        self.updated_by = user
         # Record the instance as deleted
         self.deleted = True
         # Save the instance to create a version
