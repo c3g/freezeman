5396b954b0b25c348a308927feadc4017261fda6
UlysseFG <ulysse.fortiergauthier@mcgill.ca>
2021-04-01 12:29:07 -0400

    link moved on processes list page. cypress test changed and cypress tests split.

---
+++ frontend/cypress/integration/containers.spec.js
@@ -0,0 +1,54 @@
+/// <reference types="cypress" />
+
+// Fixtures:
+//  - credentials.json
+//  - Container_creation_v0.4.xlsx
+//  - Container_move_v0.4.xlsx
+//  - Container_rename_v0.1.xlsx
+
+// Helpers
+// stored in .../cypress/support/commands.js
+// cy.getCredentials
+// cy.login
+// cy.navigateTo
+// cy.submitForm
+
+export const containersTests = () => {
+  context('contrainers section tests', () => {
+
+    beforeEach(() => {
+      cy.visit('http://localhost:9000/sign-in')
+      cy.getCredentials().then(cy.login)
+    })
+
+    context('Containers', () => {
+      const singleContainerBarcode = 'test-container-add';
+      it('creates single container', () => {
+        cy.navigateTo('Container', 'Add')
+        const comment = 'This is a comment.'
+        cy.get('#name').type(singleContainerBarcode)
+        cy.get('#kind').click()
+        cy.get('.ant-select-dropdown .ant-select-item-option').first().click()
+        cy.get('#barcode').type(singleContainerBarcode)
+        cy.get('#comment').type(comment)
+        cy.submitForm()
+        cy.get('body').should('contain', `Container ${name}`) // Details title
+      })
+  
+      it('visit container detail page', () => {
+         cy.navigateTo('Containers')
+         cy.get('.ant-table-cell').contains(singleContainerBarcode).click()
+         cy.get('body').should('contain', `Container ${singleContainerBarcode}`)
+      })
+  
+      it('creates multiple containers (template import)', () => {
+        cy.navigateTo('Container', 'Add Containers')
+        cy.get('input[type=file]').attachFile('Container_creation_v0.4.xlsx')
+        cy.submitForm()
+        cy.get('.ant-alert-success').should('contain', 'Template submitted')
+        cy.get('button').contains('Go Back').click()
+        // cy.get('body').should('contain', '1-10 of 15 items')
+      })
+    })
+  })
+}
---
+++ frontend/cypress/integration/samples.spec.js
@@ -0,0 +1,43 @@
+/// <reference types="cypress" />
+
+// Fixtures:
+//  - credentials.json
+//  - Sample_submission_v0.11.xlsx
+//  - Sample_submission_v0.9.1_AB.xlsx
+//  - Sample_update_v0.5.xlsx
+
+// Helpers
+// stored in .../cypress/support/commands.js
+// cy.getCredentials
+// cy.login
+// cy.navigateTo
+// cy.submitForm
+
+export const samplesTests = () => {
+  context('samples section tests', () => {
+
+    beforeEach(() => {
+      cy.visit('http://localhost:9000/sign-in')
+      cy.getCredentials().then(cy.login)
+    })
+
+    context('Samples', () => {
+      it('creates samples (template import)', () => {
+        cy.navigateTo('Samples', 'Add Samples')
+        cy.get('input[type=file]').attachFile('Sample_submission_v0.9.1_AB.xlsx')
+        cy.submitForm()
+        cy.get('.ant-alert-success').should('contain', 'Template submitted')
+        cy.get('button').contains('Go Back').click()
+        // cy.get('body').should('contain', '1-8 of 8 items')
+      })
+
+      it('visits sample detail page', () => {
+        // Sample from template import
+        const sampleName = 'Sample_DNA1'
+        cy.navigateTo('Samples')
+        cy.get('.ant-table').contains(sampleName).click()
+        cy.get('body').should('contain', `Sample ${sampleName}`)
+      })
+    })
+  })
+}
