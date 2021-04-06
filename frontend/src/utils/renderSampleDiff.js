165c72211b1ce7189ae305d1cd4f0738ec288bbd
jleblond <jasmine.leblondc@gmail.com>
2021-04-06 11:54:27 -0400

    Change user id fields with username

--- frontend/src/utils/renderSampleDiff.js
+++ frontend/src/utils/renderSampleDiff.js
@@ -12,7 +12,7 @@ const arrowStyle = {
   marginRight: 8,
 }
 
-export default function renderSampleDiff(oldVersion, newVersion) {
+export default function renderSampleDiff(oldVersion, newVersion, usersByID) {
   if (!oldVersion)
     return null;
 
@@ -21,11 +21,10 @@ export default function renderSampleDiff(oldVersion, newVersion) {
   if (deltas === undefined)
     return null;
 
-  delete deltas.update_comment;
 
   const items = Object.entries(deltas).map(([key, delta]) => {
     if (Array.isArray(delta))
-      return renderArrayDelta(key, delta, oldVersion, newVersion);
+      return renderArrayDelta(key, delta, oldVersion, newVersion, usersByID);
 
     return renderUnknownDelta(key, delta, oldVersion, newVersion);
   });
@@ -48,44 +47,48 @@ function renderUnknownDelta(name, delta, oldVersion, newVersion) {
 }
 
 
-function renderArrayDelta(name, delta, oldVersion, newVersion) {
+function renderArrayDelta(key, delta, oldVersion, newVersion, usersByID) {
 
   // ADDED: delta = [ newValue ]
   if (delta.length === 1)
     return (
-      <div key={name}>
-        <code>{name}:</code>{' '}
-        <Tag color="green" className='diff__added'>{renderDeltaValue(delta[0])}</Tag>
+      <div key={key}>
+        <code>{key}:</code>{' '}
+        <Tag color="green" className='diff__added'>{renderDeltaValue(delta[0], key, usersByID)}</Tag>
       </div>
     );
 
   /// MODIFIED: delta = [ oldValue, newValue ]
   if (delta.length === 2)
     return (
-      <div key={name}>
-        <code>{name}:</code>{' '}
-        <Tag color="red" style={removedStyle}>{renderDeltaValue(delta[0])}</Tag>
+      <div key={key}>
+        <code>{key}:</code>{' '}
+        <Tag color="red" style={removedStyle}>{renderDeltaValue(delta[0], key, usersByID)}</Tag>
         <SwapRightOutlined style={arrowStyle} />
-        <Tag color="green" className='diff__added'>{renderDeltaValue(delta[1])}</Tag>
+        <Tag color="green" className='diff__added'>{renderDeltaValue(delta[1], key, usersByID)}</Tag>
       </div>
     );
 
   /// DELETED: delta = [ oldValue, 0, 0 ]
   if (delta.length === 3 && delta[1] === 0 && delta[2] === 0)
     return (
-      <div key={name}>
+      <div key={key}>
         <Tag color="red" style={removedStyle}>
-          <code>{name}:</code> {renderDeltaValue(delta[0])}
+          <code>{key}:</code> {renderDeltaValue(delta[0], key, usersByID)}
         </Tag>
       </div>
     );
 
   // Invalid
-  return renderUnknownDelta(name, delta, oldVersion, newVersion)
+  return renderUnknownDelta(key, delta, oldVersion, newVersion)
 }
 
-function renderDeltaValue(value) {
+function renderDeltaValue(value, key, usersByID) {
   if (value === null) return "null";
   if (value === undefined) return "undefined";
+
+  if (["created_by", "updated_by"].includes(key))
+    return usersByID[value]?.username
+
   return value.toString();
 }
