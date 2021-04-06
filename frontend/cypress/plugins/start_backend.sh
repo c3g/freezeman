322f075ac0418a846a5a0e92d3f548019a4d6bfe
UlysseFG <ulysse.fortiergauthier@mcgill.ca>
2021-04-06 11:00:23 -0400

    removed cypress plugins scripts. Added the all.spec to the scripts to prevent running sub specs.

--- frontend/cypress/plugins/start_backend.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Activate virtual env
-. ../env/bin/activate
-# Activate the test DB as PG current DB
-export PG_DATABASE=fms_test
-# Start Backend
-python3 ../backend/manage.py runserver
