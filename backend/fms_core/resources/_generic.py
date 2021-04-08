0267dda95cc985accab7653e267e6c90d2f75ca3
UlysseFG <ulysse.fortiergauthier@mcgill.ca>
2021-04-08 17:06:13 -0400

    interrupt  processing when to many errors are raised.

--- backend/fms_core/resources/_generic.py
+++ backend/fms_core/resources/_generic.py
@@ -7,6 +7,8 @@ from reversion.models import Version
 class GenericResource(resources.ModelResource):
     clean_model_instances = True
     skip_unchanged = True
+    ERROR_CUTOFF = 100
+    errorCount = 0
 
     def save_instance(self, instance, using_transactions=True, dry_run=False):
         if dry_run:
@@ -21,3 +23,14 @@ class GenericResource(resources.ModelResource):
         if not dry_run:
             versions = Version.objects.get_for_object(instance)
             reversion.set_comment("Updated from template." if len(versions) >= 1 else "Imported from template.")
+
+    def after_import_row(self, row, row_result, row_number=None, **kwargs):
+        self.errorCount += (len(row_result.errors[0]) + len(row_result.validation_error)
+
+    def import_row(self, row, instance_loader, using_transactions=True, dry_run=False, raise_errors=False, **kwargs):
+        if self.errorCount < ERROR_CUTOFF:
+            super().import_row(row, instance_loader, using_transactions, dry_run, raise_errors, **kwargs)
+
+    def after_import(self, dataset, result, using_transactions, dry_run, **kwargs):
+        if self.errorCount >= ERROR_CUTOFF:
+            raise Exception("Too many errors. Interrupted processing.")
