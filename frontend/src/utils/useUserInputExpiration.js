c7dc8de8d73e4b4a8613d65be38efb1542f88eb7
jleblond <jasmine.leblondc@gmail.com>
2021-04-09 12:03:01 -0400

    Changes

--- frontend/src/utils/useUserInputExpiration.js
+++ frontend/src/utils/useUserInputExpiration.js
@@ -4,6 +4,12 @@
 
 import { useEffect } from 'react';
 
+/*
+ * Runs a callback after a delay without user interaction
+ * @param {Function} expirationFunction - The function to run at expiration
+ * @param {number} expirationDelay - The delay in milliseconds
+ * @example
+ */
 export default function useUserInputExpiration(expirationFunction, expirationDelay) {
     let timer;
 
@@ -19,7 +25,6 @@ export default function useUserInputExpiration(expirationFunction, expirationDel
 
     useEffect(() => {
         const events = [
-            'mousedown',
             'click',
             'pageshow',
             'keypress'
@@ -34,10 +39,10 @@ export default function useUserInputExpiration(expirationFunction, expirationDel
             if (timer)
                 clearTimeout(timer);
 
-            for(let i in events){
+            for (let i in events) {
                 window.removeEventListener(events[i], resetTimer);
             }
         }
-    },[]);
+    }, []);
 
 }
