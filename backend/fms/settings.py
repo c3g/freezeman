8599e0f13ee5f2636dbe6fd2d7b025b37144e673
jleblond <jleblond@users.noreply.github.com>
2021-04-15 14:12:04 -0400

    Add backend settings for JWT

--- backend/fms/settings.py
+++ backend/fms/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from datetime import timedelta
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -210,3 +211,31 @@ LOGGING = {
 
 SESSION_EXPIRE_AT_BROWSER_CLOSE = False
 SESSION_COOKIE_AGE = 60 * 60  # 1 hour
+
+SIMPLE_JWT = {
+'ACCESS_TOKEN_LIFETIME': timedelta(seconds=20),
+'REFRESH_TOKEN_LIFETIME': timedelta(seconds=20),
+'ROTATE_REFRESH_TOKENS': True,
+'BLACKLIST_AFTER_ROTATION': True,
+
+'ALGORITHM': 'HS256',
+'SIGNING_KEY': SECRET_KEY,
+'VERIFYING_KEY': None,
+'AUDIENCE': None,
+'ISSUER': None,
+
+'AUTH_HEADER_TYPES': ('Bearer',),
+'USER_ID_FIELD': 'id',
+'USER_ID_CLAIM': 'user_id',
+
+'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+'TOKEN_TYPE_CLAIM': 'token_type',
+
+'JTI_CLAIM': 'jti',
+'TOKEN_USER_CLASS': 'rest_framework_simplejwt.models.TokenUser',
+
+# Note: in the front-end, data is automatically fetched every 30 seconds..
+# 'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+# 'SLIDING_TOKEN_LIFETIME': timedelta(seconds=20),
+# 'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(seconds=20),
+}
