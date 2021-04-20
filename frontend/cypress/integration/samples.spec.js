e955d6a902fd97abed5989ac0a5a8b86a4996aad
jleblond <jleblond@users.noreply.github.com>
2021-04-20 11:41:20 -0400

    WIP Sample form test

--- frontend/cypress/integration/samples.spec.js
+++ frontend/cypress/integration/samples.spec.js
@@ -22,21 +22,31 @@ export const samplesTests = () => {
     })
 
     context('Samples', () => {
-      it('creates samples (template import)', () => {
-        cy.navigateTo('Samples', 'Add Samples')
-        cy.get('input[type=file]').attachFile('Sample_submission_v0.9.1_AB.xlsx')
+      const singleSampleName = 'test-sample-add';
+      it('creates single sample', () => {
+        cy.navigateTo('Samples', 'Add')
+        cy.get('#name').type(singleSampleName)
+        cy.get('#sample_kind').click()
+        cy.get('.ant-select-dropdown .ant-select-item-option').last().click()
+        // cy.get('#individual').click()
+        // cy.get('.ant-select-selection-search .ant-select-selection-search-input').type('test-individual-name')
+        // cy.get('#container').click()
+        // cy.get('.ant-select-selection-search .ant-select-selection-search-input').type('test-container-add')
+        cy.get('#coordinates').type('B12')
+        cy.get('#volume').type('100')
+        cy.get('#collection_site').type('Site Test')
+        cy.get('#creation_date').click()
+        cy.get('.ant-picker .ant-picker-input').click()
+        cy.get('.ant-picker-body .ant-picker-cell').first().click()
+        cy.get('#comment').type('This is a test')
         cy.submitForm()
-        cy.get('.ant-alert-success').should('contain', 'Template submitted')
-        cy.get('button').contains('Go Back').click()
-        cy.get('body').should('contain', '1-8 of 8 items')
+        cy.get('body').should('contain', `Container ${name}`) // Details title
       })
 
-      it('visits sample detail page', () => {
-        // Sample from template import
-        const sampleName = 'Sample_DNA1'
+      it('visit sample detail page', () => {
         cy.navigateTo('Samples')
-        cy.get('.ant-table').contains(sampleName).click()
-        cy.get('body').should('contain', `Sample ${sampleName}`)
+        cy.get('.ant-table-cell').contains(singleSampleName).click()
+        cy.get('body').should('contain', `Sample ${singleSampleName}`)
       })
     })
   })
