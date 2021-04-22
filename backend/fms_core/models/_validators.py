da4d58d013719b1b1170eb9bd637980f00788382
UlysseFG <ulysse.fortiergauthier@mcgill.ca>
2021-04-22 13:21:53 -0400

    Review of container_rename. changed validators for name and barcode. new migration.

--- backend/fms_core/models/_validators.py
+++ backend/fms_core/models/_validators.py
@@ -6,9 +6,6 @@ __all__ = ["name_validator",
            "container_barcode_validator"]
 
 # Names should only contain a-z, A-Z, 0-9, ., -, _
-# Barcodes can contain any character except "$" which is used internally for renaming of containers.
-# They are capped at 200 characters by the field length inherently - but we limit them to
-# one character less than that, since when renaming containers we need to append a
-# temporary character to prevent integrity errors.
-name_validator = RegexValidator(re.compile(r"^[a-zA-Z0-9.\-_]{1,199}$"))
-container_barcode_validator = RegexValidator(re.compile("^[^\\" + TEMPORARY_RENAME_SUFFIX + "]{1,199}$"))
+# Barcodes can contain any character
+name_validator = RegexValidator(re.compile(r"^[a-zA-Z0-9.\-_]{1,200}$"))
+container_barcode_validator = RegexValidator(re.compile("^[.]{1,200}$"))
