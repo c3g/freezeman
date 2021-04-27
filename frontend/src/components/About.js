2bbadad7cf7572c563720bd724db4ce9ef1e0151
jleblond <jleblond@users.noreply.github.com>
2021-04-27 17:40:05 -0400

    Add About page

---
+++ frontend/src/components/About.js
@@ -0,0 +1,36 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import {connect} from "react-redux";
+
+const mapStateToProps = state => ({
+  info: state.base.info,
+});
+
+const actionCreators = {};
+
+const About = ({info}) => {
+    const version = info.version;
+    const repository = info.repository;
+    const commit = info.commit;
+    return <>
+        <div>FreezeMan</div>
+        {(version)?
+            <div>Version {version}</div>
+        : ""}
+        <div>Released under GNU LGPL version 3 © C3G, McGill University</div>
+        {(repository)?
+            <div>
+               <a target='_blank' href={repository}>{repository} </a>
+            </div>
+        : ""}
+
+        {(repository && commit && info.env != 'PROD' )?
+          <div>
+              <a target='_blank' href={`${repository}/commit/${commit.hash_full}`}>{commit.small_hash} </a>
+              ({commit.date})
+          </div>
+        : ""}
+    </>;
+}
+
+export default connect(mapStateToProps, actionCreators)(About);
