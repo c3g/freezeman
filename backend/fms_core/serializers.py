e0011c7ed91a7d8f4a5fe4a502110d044e93579b
jleblond <jleblond@users.noreply.github.com>
2021-05-04 14:19:09 -0400

    Add ProcessSample to Sample endpoint

--- backend/fms_core/serializers.py
+++ backend/fms_core/serializers.py
@@ -84,6 +84,7 @@ class ProcessSampleExportSerializer(serializers.ModelSerializer):
 
 class SampleSerializer(serializers.ModelSerializer):
     extracted_from = serializers.SerializerMethodField()
+    process_sample = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 
     class Meta:
         model = Sample
