c41884c39be83a85b8311a0d659db16994dae348
UlysseFG <ulysse.fortiergauthier@mcgill.ca>
2021-05-10 16:37:45 -0400

    completed modifications to fied_value update script

--- backend/fms_core/management/commands/_field_value_update.py
+++ backend/fms_core/management/commands/_field_value_update.py
@@ -3,14 +3,13 @@ import reversion
 import json
 import logging
 from reversion.models import Version
-from django.contrib.auth.models import User
 from fms_core.models import Sample, Container, Individual, Protocol, SampleKind, SampleLineage, Process, ProcessSample
 
 # Parameters required for this curation
 ACTION = "action"                     # = field_value_update
 CURATION_INDEX = "curation_index"     # Number indicating the order in which this action was performed during the curation.
 ENTITY_MODEL = "entity_model"         # The name of the model for the target entity.
-ENTITY_DICT_ID = "entity_identifier"  # A dictionary that contains the fields required to uniquely identify the targeted entity.
+ENTITY_DICT_ID = "entity_identifier"  # An array of dictionary that contains the fields required to uniquely identify the targeted entity.
 FIELD_NAME = "field_name"             # The name of the field that need to be updated.
 VALUE_OLD = "value_old"               # The old value of the entity's field (used for validation). Optional, validation skipped if empty.
 VALUE_NEW = "value_new"               # The new value of the entity's field.
@@ -30,9 +29,6 @@ USER_ID = "requester_user_id"         # The user id of the person requesting the
 # ENTITY_DICT_ID is an array to allow an identical change to be performed on multiple entities. If the changes are different,
 # add more field_value_actions to the curation.
 
-# Other constants
-REVERSION_ADMIN_USER = "biobankadmin"
-
 def field_value_update(params, log):
     log.info("Action [" + str(params[CURATION_INDEX]) + "] Field Value Update started.")
     log.info("Targeted model : " + str(params[ENTITY_MODEL]))
@@ -40,7 +36,7 @@ def field_value_update(params, log):
     log.info("Field to update : " + str(params[FIELD_NAME]))
     log.info("Old value : " + str(params.get(VALUE_OLD)))
     log.info("New value : " + str(params[VALUE_NEW]))
-    log.info("Old value : " + str(params.get(USER_ID)))
+    log.info("Requester id : " + str(params.get(USER_ID)))
 
     # initialize the curation
     curation_code = params.get(CURATION_INDEX, "Invalid index")
@@ -50,11 +46,12 @@ def field_value_update(params, log):
     field = params[FIELD_NAME]
     old_value = params.get(VALUE_OLD)
     new_value = params[VALUE_NEW]
-    user_id = params.get(USER_ID, User.objects.get(username=REVERSION_ADMIN_USER))
+    user_id = params.get(USER_ID)
     
 
     try:
         entity_model = apps.get_model("fms_core", model)
+        count_updates = 0
         for id in id_array:
             try:
                 entity = entity_model.objects.get(**id)
@@ -62,8 +59,12 @@ def field_value_update(params, log):
                     db_old_value = getattr(entity, field)
                     if old_value and old_value == db_old_value:
                         setattr(entity, field, new_value)
-                        setattr(entity, "updated_by_id", user_id) # Record manually the user_id since the request may originate from a user
-                        entity.save()
+                        log.info(f"Updated model [{model}] id [{id} field [{field}] old value [{old_value}] new value [{new_value}].")
+                        if user_id:
+                            entity.save(requester_id=user_id) # Save using th id of the requester
+                        else:
+                            entity.save() # Save using the default admin user
+                        count_updates += 1
                     else:
                         log.error(f"Content of field [{field}] do not match the old_value expected [{old_value}].")
                         error_found = True    
@@ -81,4 +82,5 @@ def field_value_update(params, log):
         error_found = True
     if not error_found:
         curation_code = None
+        log.info(f"Updated [{count_updates}] field values.")
     return curation_code
