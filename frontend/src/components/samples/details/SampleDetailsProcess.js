b5b599ba7eddf912cdb24f516e71341e3494e2a9
jleblond <jleblond@users.noreply.github.com>
2021-05-18 10:00:33 -0400

    Bugfix: Add Empty ant component

--- frontend/src/components/samples/details/SampleDetailsProcess.js
+++ frontend/src/components/samples/details/SampleDetailsProcess.js
@@ -2,7 +2,7 @@ import React from "react";
 import {connect} from "react-redux";
 import {Link} from "react-router-dom";
 
-import {Table, Tag} from "antd";
+import {Table, Tag, Empty} from "antd";
 
 
 const mapStateToProps = state => ({
