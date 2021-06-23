cabdf632cbe11cae15c9adf030ed0f4a5dc2d344
jleblond <jasmine.leblondc@gmail.com>
2021-06-23 17:20:23 -0400

    Fixes for Experiment Run Template

--- backend/fms_core/resources/experiment_run.py
+++ backend/fms_core/resources/experiment_run.py
@@ -13,19 +13,19 @@ __all__ = ["ExperimentRunResource"]
 
 
 class ExperimentRunResource(GenericResource):
-    container_barcode = Field(column_name='Experiment Container Barcode')
+    container_barcode = Field(column_name='Experiment Container Barcode',
+                              widget=ForeignKeyWidget(Container, field='barcode'))
     container_kind = Field(column_name='Experiment Container Kind')
-    instrument_name = Field(column_name='Instrument Name')
+    instrument_name = Field(column_name='Instrument Name',
+                              widget=ForeignKeyWidget(Instrument, field='name'))
     start_date = Field(attribute='start_date', column_name='Experiment Start Date', widget=DateWidget())
 
     class Meta:
         model = ExperimentRun
         import_id_fields = ()
         fields = (
-            'start_date'
         )
         excluded = (
-            'experiment_type',
             'container',
             'instrument'
         )
@@ -38,15 +38,20 @@ class ExperimentRunResource(GenericResource):
     def import_obj(self, obj, data, dry_run):
         errors = {}
 
+        try:
+            super().import_obj(obj, data, dry_run)
+        except ValidationError as e:
+            errors = e.update_error_dict(errors).copy()
+
         # Getting experiment_type
         try:
             obj.experiment_type = ExperimentType.objects.get(workflow=self.experiment_type_name)
-        except ExperimentType.DoesNotExist:
+        except Exception as e:
             errors["experiment_type"] = ValidationError([f"No experiment type with workflow {self.experiment_type_name} could be found."], code="invalid")
-
 
         # Getting container
         container_info =  dict(
+            name=get_normalized_str(data, "Experiment Container Barcode"),
             barcode=get_normalized_str(data, "Experiment Container Barcode"),
             kind=get_normalized_str(data, "Experiment Container Kind"),
         )
@@ -56,19 +61,19 @@ class ExperimentRunResource(GenericResource):
                 defaults={'comment': f"Automatically generated via experiment run template import on "
                         f"{datetime.utcnow().isoformat()}Z"},
             )
-        except Container.DoesNotExist:
+        except Exception as e:
             errors["container"] = ValidationError([f"Could not create experiment container. Barcode and kind are existing and do not match. "], code="invalid")
 
         # Getting instrument
         instrument_name = get_normalized_str(data, "Instrument Name")
         try:
             obj.instrument = Instrument.objects.get(name=instrument_name)
-        except Instrument.DoesNotExist:
+        except Exception as e:
             errors["instrument"] = ValidationError([f"No instrument named {instrument_name} could be found."], code="invalid")
-
 
         if errors:
             raise ValidationError(errors)
+
 
     def import_field(self, field, obj, data, is_m2m=False):
         super().import_field(field, obj, data, is_m2m)
