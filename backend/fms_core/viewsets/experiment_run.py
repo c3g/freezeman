f84973127da621fbd2cbab4627ba17182aaba654
jleblond <jasmine.leblondc@gmail.com>
2021-07-05 16:30:46 -0400

    Readd ExperimentRun viewset template and resource

--- backend/fms_core/viewsets/experiment_run.py
+++ backend/fms_core/viewsets/experiment_run.py
@@ -5,10 +5,22 @@ from ._utils import TemplateActionsMixin
 
 from fms_core.models import ExperimentRun
 from fms_core.serializers import ExperimentRunSerializer
+from fms_core.resources import ExperimentRunResource
+from fms_core.template_paths import EXPERIMENT_INFINIUM_TEMPLATE
 
 
 class ExperimentRunViewSet(viewsets.ModelViewSet, TemplateActionsMixin):
     queryset = ExperimentRun.objects.select_related("experiment_type", "container", "instrument")
     serializer_class = ExperimentRunSerializer
     pagination_class = None
+
     permission_classes = [IsAuthenticated]
+
+    template_action_list = [
+        {
+            "name": "Experiment Run",
+            "description": "Upload the provided template with experiment run information.",
+            "template": EXPERIMENT_INFINIUM_TEMPLATE,
+            "resource": ExperimentRunResource,
+        },
+    ]
