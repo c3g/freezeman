74a50f7d737258e59bc7d91139779eab88497e19
Sebastian Ballesteros <tatanballesteros@hotmail.com>
2021-07-19 18:30:57 -0400

    Added unit tests

--- frontend/cypress/integration/all.spec.js
+++ frontend/cypress/integration/all.spec.js
@@ -4,11 +4,12 @@ import { containersTests } from "./containers.spec"
 import { individualsTests } from "./individuals.spec"
 import { samplesTests } from "./samples.spec"
 import { protocolsTests } from "./protocols.spec"
+import { paginationTests } from "./pagination.spec"
 
 // Tests
 context('All tests', () => {
   containersTests()
   individualsTests()
   samplesTests()
-  protocolsTests()
+  paginationTests()
 })
