6be7bee93751249e0641f37acaa48664ba800fd8
Sebastian Ballesteros <tatanballesteros@hotmail.com>
2021-07-26 15:48:48 -0400

    Refactored test to submit more than 1 experiment

--- frontend/cypress/integration/experiments.spec.js
+++ frontend/cypress/integration/experiments.spec.js
@@ -21,28 +21,44 @@ export const experimentsTests = () => {
     })
 
     context('Experiments', () => {
+      const experimentBarcode1 = 'CONTAINERFOREXPMULTIPLE7';
+      const experimentBarcode2 = 'CONTAINERFOREXPMULTIPLE8';
+
       it('creates experiments (template import)', () => {
         cy.navigateTo('Experiments')
-        cy.wait(1000)
+        cy.wait(5000)
         cy.get('button').contains('Add Experiments').click()
         cy.get('input[type=file]').attachFile('Experiment_submission_v3_3_0_F_A_1.xlsx')
+        cy.wait(5000)
         cy.submitForm()
+        cy.wait(5000)
         cy.get('.ant-alert-success').should('contain', 'Template submitted')
         cy.get('button').contains('Go Back').click()
-        cy.get('body').should('contain', '1-1 of 1 items')
-      })
+        cy.get('body').should('contain', experimentBarcode1)
+        cy.get('body').should('contain', experimentBarcode2)
+      });
 
-      it('visits experiment detail page', () => {
-        // Sample from template import
-        const experimentBarcode = 'CONTAINERFOREXPMULTIPLE'
+      it('visits experiment detail page and checks the existence of the first experiment', () => {
         cy.navigateTo('Experiments')
-        cy.wait(2000)
-        cy.get('td').within(($tr) => {
-          cy.contains('Experiment')
-          .click()
-        })
-        cy.get('body').should('contain', experimentBarcode)
-      })
-    })
+        cy.wait(5000)
+        cy.get('td.ant-table-cell').contains(experimentBarcode1)
+          .parent().parent().should('have.class', 'ant-table-row')
+          .within(() => {
+            cy.get('td').contains('Experiment').click()
+        });
+        cy.get('body').should('contain', experimentBarcode1)
+      });
+
+      it('visits experiment detail page and checks the existence of the second experiment', () => {
+        cy.navigateTo('Experiments')
+        cy.wait(5000)
+        cy.get('td.ant-table-cell').contains(experimentBarcode2)
+          .parent().parent().should('have.class', 'ant-table-row')
+          .within(() => {
+            cy.get('td').contains('Experiment').click()
+        });
+        cy.get('body').should('contain', experimentBarcode2)
+      });
+    });
   })
 }
