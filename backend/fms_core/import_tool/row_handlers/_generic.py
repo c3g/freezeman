eeb9d460b7eaffb25d2503d725c0f30a04dc7633
jleblond <jleblond@users.noreply.github.com>
2021-08-19 10:33:47 -0400

    Clean RowHandler

--- backend/fms_core/import_tool/row_handlers/_generic.py
+++ backend/fms_core/import_tool/row_handlers/_generic.py
@@ -3,7 +3,7 @@ from django.core.exceptions import ValidationError
 '''
     RowHandler objects
     
-    attributes (input): 
+    process_row (input): 
         row data obtained from the Importer objects.
     get_result (output): 
         A row result dictionary containing errors, validation error, warnings, and row data 
@@ -14,15 +14,16 @@ class GenericRowHandler():
         self.row_object = None
         self.errors = {}
 
+
     def process_row(self, **kwargs):
         pass
+
 
     def get_result(self):
         print('ROW_HANDLER get_result errors', self.errors)
         return {'errors': [],
                 'validation_error': ValidationError(self.errors),
                 'warnings': [],
-                #'import_type': 'new',
                 }
 
     def has_errors(self):
