c979717a3d8da9a16bd2053ff85d756443fb4d07
Sebastian Ballesteros <tatanballesteros@hotmail.com>
2021-08-19 16:00:56 -0400

    Fixed indentation and made methods reusable

--- backend/fms_core/filters.py
+++ backend/fms_core/filters.py
@@ -5,61 +5,50 @@ from .models import Container, Sample, Individual
 import django_filters
 
 from .viewsets._constants import (
- _container_filterset_fields,
- _sample_filterset_fields,
- _individual_filterset_fields,
- _sample_minimal_filterset_fields,
+    _container_filterset_fields,
+    _sample_filterset_fields,
+    _individual_filterset_fields,
+    _sample_minimal_filterset_fields,
 )
 
 from .viewsets._utils import _prefix_keys
 
-class ContainerFilter(django_filters.FilterSet):
- barcode = django_filters.CharFilter(field_name="barcode", method="batch_barcode_filter")
- name = django_filters.CharFilter(field_name="name", method="batch_name_filter")
+class GenericFilter(django_filters.FilterSet):
+    def batch_barcode_filter(self, queryset, name, value):
+        query = Q()
+        for v in value.split(" "):
+            query |= Q(barcode__contains=v)
+        return queryset.filter(query)
 
- class Meta:
-     model = Container
-     fields = {
-         **_container_filterset_fields,
-         **_prefix_keys("location__", _container_filterset_fields),
-         **_prefix_keys("samples__", _sample_minimal_filterset_fields),
-     }
+    def batch_name_filter(self, queryset, name, value):
+        query = Q()
+        for v in value.split(" "):
+            query |= Q(name__contains=v)
+        return queryset.filter(query)
 
- def batch_barcode_filter(self, queryset, name, value):
-     query = Q()
-     for barcode in value.split(" "):
-         query |= Q(barcode__contains=barcode)
-     return queryset.filter(query)
 
- def batch_name_filter(self, queryset, name, value):
-     query = Q()
-     for name in value.split(" "):
-         query |= Q(name__contains=name)
-     return queryset.filter(query)
+class ContainerFilter(GenericFilter):
+    barcode = django_filters.CharFilter(field_name="barcode", method="batch_barcode_filter")
+    name = django_filters.CharFilter(field_name="name", method="batch_name_filter")
 
-class SampleFilter(django_filters.FilterSet):
- barcode = django_filters.CharFilter(field_name="barcode", method="batch_name_filter")
- name = django_filters.CharFilter(field_name="name", method="batch_name_filter")
+class Meta:
+    model = Container
+    fields = {
+        **_container_filterset_fields,
+        **_prefix_keys("location__", _container_filterset_fields),
+        **_prefix_keys("samples__", _sample_minimal_filterset_fields),
+    }
 
- class Meta:
-     model = Sample
-     fields = _sample_filterset_fields
+class SampleFilter(GenericFilter):
+    name = django_filters.CharFilter(field_name="name", method="batch_name_filter")
 
- def batch_name_filter(self, queryset, name, value):
-     query = Q()
-     for name in value.split(" "):
-         query |= Q(name__contains=name)
-     return queryset.filter(query)
+    class Meta:
+        model = Sample
+        fields = _sample_filterset_fields
 
 class IndividualFilter(django_filters.FilterSet):
- name = django_filters.CharFilter(field_name="name", method="batch_name_filter")
+    name = django_filters.CharFilter(field_name="name", method="batch_name_filter")
 
- class Meta:
-     model = Individual
-     fields = _individual_filterset_fields
-
- def batch_name_filter(self, queryset, name, value):
-     query = Q()
-     for name in value.split(" "):
-         query |= Q(name__contains=name)
-     return queryset.filter(query)
+class Meta:
+    model = Individual
+    fields = _individual_filterset_fields
