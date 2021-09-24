71a2c158028bee8cac4911765204d56044de30e5
jleblond <jleblond@users.noreply.github.com>
2021-09-24 13:03:49 -0400

    SampleUpdate Template Import (WIP2)

---
+++ backend/fms_core/template_importer/row_handlers/sample_update/sample.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+
+from fms_core.template_importer.row_handlers._generic import GenericRowHandler
+
+from fms_core.services.sample import get_sample_from_container
+from fms_core.services.process_measurement import create_process_measurement
+
+class SampleRowHandler(GenericRowHandler):
+    def __init__(self):
+        super().__init__()
+
+
+    def process_row_inner(self, sample, sample_updated, process_measurement):
+        print('start sample row handler')
+
+        comment = f"Automatically generated via Sample update Template on {datetime.utcnow().isoformat()}Z"
+
+        sample_to_update, self.errors['sample'], self.warnings['sample'] = get_sample_from_container(
+            barcode=sample['container']['barcode'],
+            coordinates=sample['coordinates'],
+        )
+
+        if sample_to_update:
+            #TODO: edit sample_to_update
+
+            _, self.errors['process_measurement'], self.warnings['process_measurement'] = \
+                create_process_measurement(
+                    process=process_measurement['process'],
+                    source_sample=sample_to_update,
+                    execution_date=process_measurement['execution_date'],
+                    volume_used=process_measurement['volume_used'],
+                    comment=process_measurement['comment'],
+                )
+
+
+        print('sample_update/sample row handler ERRORS: ', self.errors)
