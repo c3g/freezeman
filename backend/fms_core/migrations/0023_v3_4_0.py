127b65e356dacc1073c7e2554fa6acd606473d68
jleblond <jasmine.leblondc@gmail.com>
2021-09-29 15:39:11 -0400

    Add created_by and updated_by for new project and samplebyproject in migration

--- backend/fms_core/migrations/0023_v3_4_0.py
+++ backend/fms_core/migrations/0023_v3_4_0.py
@@ -12,6 +12,8 @@ def initialize_projects_data(apps, schema_editor):
     Project = apps.get_model("fms_core", "Project")
     SampleByProject = apps.get_model("fms_core", "SampleByProject")
 
+    admin_user = User.objects.get(username=ADMIN_USERNAME)
+    admin_user_id = admin_user.id
 
     with reversion.create_revision(manage_manually=True):
         reversion.set_comment("Initialization of projects through curation")
@@ -19,14 +21,14 @@ def initialize_projects_data(apps, schema_editor):
 
         projects_by_cohort_name = {}
         for (project_name, cohort_name) in [('Epocrates', 'EPOCRATES'), ('BQC19', 'BQC'), ('INSPQ_COVID', 'INSPQ_COVID'), ('Timmins', 'Timmins')]:
-            p = Project.objects.create(name=project_name, status="Open")
+            p = Project.objects.create(name=project_name, status="Open", created_by_id=admin_user_id, updated_by_id=admin_user_id)
             reversion.add_to_revision(p)
             projects_by_cohort_name[cohort_name] = p
 
 
         for sample in Sample.objects.filter(individual__cohort__in=["EPOCRATES", 'BQC', 'INSPQ_COVID', 'Timmins']).select_related('individual'):
             cohort = sample.individual.cohort
-            sp = SampleByProject.objects.create(sample=sample, project=projects_by_cohort_name[cohort])
+            sp = SampleByProject.objects.create(sample=sample, project=projects_by_cohort_name[cohort], created_by_id=admin_user_id, updated_by_id=admin_user_id)
             reversion.add_to_revision(sp)
 
 
