e09cf2da427a1fc98c51d8a44bf46e2c50d70fc9
jleblond <jleblond@users.noreply.github.com>
2021-09-30 15:55:27 -0400

    Migration Raw SQL WIP

--- backend/fms_core/migrations/0023_v3_4_0.py
+++ backend/fms_core/migrations/0023_v3_4_0.py
@@ -8,9 +8,7 @@ import datetime
 ADMIN_USERNAME='biobankadmin'
 
 def initialize_projects_data(apps, schema_editor):
-    Sample = apps.get_model("fms_core", "Sample")
     Project = apps.get_model("fms_core", "Project")
-    SampleByProject = apps.get_model("fms_core", "SampleByProject")
 
     admin_user = User.objects.get(username=ADMIN_USERNAME)
     admin_user_id = admin_user.id
@@ -19,17 +17,9 @@ def initialize_projects_data(apps, schema_editor):
         reversion.set_comment("Initialization of projects through curation")
         reversion.set_user(User.objects.get(username=ADMIN_USERNAME))
 
-        projects_by_cohort_name = {}
         for (project_name, cohort_name) in [('Epocrates', 'EPOCRATES'), ('BQC19', 'BQC'), ('INSPQ_COVID', 'INSPQ_COVID'), ('Timmins', 'Timmins')]:
             p = Project.objects.create(name=project_name, status="Open", created_by_id=admin_user_id, updated_by_id=admin_user_id)
             reversion.add_to_revision(p)
-            projects_by_cohort_name[cohort_name] = p
-
-
-        for sample in Sample.objects.filter(individual__cohort__in=["EPOCRATES", 'BQC', 'INSPQ_COVID', 'Timmins']).select_related('individual'):
-            cohort = sample.individual.cohort
-            sp = SampleByProject.objects.create(sample=sample, project=projects_by_cohort_name[cohort], created_by_id=admin_user_id, updated_by_id=admin_user_id)
-            reversion.add_to_revision(sp)
 
 
 class Migration(migrations.Migration):
@@ -43,5 +33,21 @@ class Migration(migrations.Migration):
         migrations.RunPython(
             initialize_projects_data,
             reverse_code=migrations.RunPython.noop,
+        ),
+        migrations.RunSQL(
+            "INSERT INTO fms_core_samplebyproject (sample_id, project_id, created_at, updated_at, created_by_id, updated_by_id, deleted)"
+                "SELECT sample.id, project.id, project.created_at, project.updated_at, project.created_by_id, project.updated_by_id, false"
+                "FROM fms_core_sample sample"
+                    "JOIN fms_core_individual individual ON sample.individual_id = individual.id"
+                    "JOIN fms_core_project project"
+                        " ON CASE"
+                        "WHEN individual.cohort = 'EPOCRATES' AND project.name = 'Epocrates' THEN 1"
+                        "WHEN individual.cohort = 'BQC' AND project.name = 'BQC19' THEN 1"
+                        "WHEN individual.cohort = 'INSPQ_COVID' AND project.name = 'CoVSeQ' THEN 1"
+                        "WHEN individual.cohort = 'Timmins' AND project.name = 'Timmins' THEN 1"
+                        "ELSE 0"
+                        "END=1"
+                "WHERE individual.cohort IN ('EPOCRATES', 'BQC', 'INSPQ_COVID', 'Timmins');",
+            migrations.RunSQL.noop
         ),
     ]
