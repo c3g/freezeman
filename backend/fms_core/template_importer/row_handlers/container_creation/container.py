2fb567e408e393bdb5673c85b0da54cc5a755281
UlysseFG <44278044+UlysseFG@users.noreply.github.com>
2021-10-05 10:04:08 -0400

    Importer container creation (#147)
    * Initial file creation
    
    * First pass at Container creation Importer
    
    * Initial file creation
    
    * First pass at Container creation Importer
    
    * Small fix for container creation.

---
+++ backend/fms_core/template_importer/row_handlers/container_creation/container.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+
+from fms_core.template_importer.row_handlers._generic import GenericRowHandler
+
+from fms_core.services.container import get_container, create_container
+
+class ContainerRowHandler(GenericRowHandler):
+    def __init__(self):
+        super().__init__()
+
+    def process_row_inner(self, container, parent_container):
+
+        comment = f"Automatically generated via Container creation Template on {datetime.utcnow().isoformat()}Z"
+
+        # Container related section
+
+        parent_container_obj = None
+        if parent_container['barcode']:
+            parent_container_obj, self.errors['parent_container'], self.warnings['parent_container'] = get_container(barcode=parent_container['barcode'])
+
+        container_obj, self.errors['container'], self.warnings['container'] = create_container(barcode=container['barcode'],
+                                                                                               kind=container['kind'],
+                                                                                               name=container['name'],
+                                                                                               container_parent=parent_container_obj,
+                                                                                               coordinates=container['coordinates'],
+                                                                                               creation_comment=comment,
+                                                                                              )
