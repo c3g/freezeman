7cc47078c3d3829dbb5d741561046b3c09ae6a2f
jleblond <jleblond@users.noreply.github.com>
2021-10-07 15:04:20 -0400

    T.I. bases for Importer Unit Tests

---
+++ backend/fms_core/tests/test_template_importers/_utils.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+import reversion
+
+APP_DATA_ROOT = Path(__file__).parent.parent / "valid_templates"
+TEST_DATA_ROOT = Path(__file__).parent.parent / "invalid_templates"
+
+def load_template(importer, file):
+    with reversion.create_revision():
+        try:
+            result = importer.import_template(file=file, format='xlsx', dry_run=True)
+        except Exception as e:
+            print(e)
+            result = {
+                'valid': False,
+                'base_errors': [{
+                    "error": str(e),
+                }],
+            }
+
+        reversion.set_comment("Test Template Importer")
+
+    return result
+
