8fde421e3ee721b697e0fdef2acd684a72d5aa32
Sebastian Ballesteros <tatanballesteros@hotmail.com>
2021-10-14 12:48:22 -0400

    Added tests for ProjectLinkSample

--- backend/fms_core/services/project.py
+++ backend/fms_core/services/project.py
@@ -1,4 +1,6 @@
 from fms_core.models import Project
+from django.core.exceptions import ValidationError
+import datetime
 
 def get_project(name=None):
     project = None
@@ -12,5 +14,34 @@ def get_project(name=None):
             errors.append(f"Could not find Project with name {name}")
     else:
         errors.append(f"Name is required to get a project.")
+
+    return (project, errors, warnings)
+
+def create_project(name=None, principal_investigator=None, requestor_name=None,
+                   requestor_email=None, status=None, target_end_date=None, comment=None):
+    project = None
+    errors = []
+    warnings = []
+
+    project_data = dict(
+        name=name,
+        principal_investigator=principal_investigator,
+        requestor_name=requestor_name,
+        requestor_email=requestor_email,
+        status=status,
+        target_end_date=target_end_date,
+        comment=(comment or (f"Automatically generated on {datetime.utcnow().isoformat()}Z")),
+        # Optional attributes
+        **(dict(principal_investigator=principal_investigator) if principal_investigator is not None else dict()),
+        **(dict(requestor_name=requestor_name) if requestor_name is not None else dict()),
+        **(dict(requestor_email=requestor_email) if requestor_email is not None else dict()),
+        **(dict(status=status) if status is not None else dict()),
+        **(dict(target_end_date=target_end_date) if target_end_date is not None else dict()),
+    )
+
+    try:
+        project = Project.objects.create(**project_data)
+    except ValidationError as e:
+        errors.append(str(e))
 
     return (project, errors, warnings)
---
+++ backend/fms_core/tests/test_template_importers/project_link_samples.py
@@ -0,0 +1,64 @@
+from django.test import TestCase
+from django.contrib.contenttypes.models import ContentType
+import datetime
+
+from fms_core.template_importer.importers import ProjectLinkSamples
+from fms_core.tests.test_template_importers._utils import load_template, APP_DATA_ROOT
+
+from fms_core.models import Sample, Individual, SampleKind, SampleByProject
+
+from fms_core.services.container import create_container
+from fms_core.services.individual import get_or_create_individual
+from fms_core.services.sample import create_sample
+from fms_core.services.project import create_project
+
+
+
+class ProjectLinkSamplesTestCase(TestCase):
+    def setUp(self) -> None:
+        self.importer = ProjectLinkSamples()
+        self.file = APP_DATA_ROOT / "Project_link_samples_vtest.xlsx"
+        ContentType.objects.clear_cache()
+
+        #Projects for Link
+        self.project1_name = 'ProjectTest1'
+        self.project2_name = 'ProjectTest2'
+
+        #Samples for Link
+        self.sample1_name = 'SampleTestForLink1'
+        self.sample2_name = 'SampleTestForLink2'
+        self.prefill_data()
+
+    def prefill_data(self):
+        sample_kind, _ = SampleKind.objects.get_or_create(name='DNA')
+
+        (container1, errors, warnings) = create_container(barcode='CONTAINER4PROJECTLINKSAMPLES1', kind='Tube', name='Container4ProjectLinkSamples1')
+        (container2, errors, warnings) = create_container(barcode='CONTAINER4PROJECTLINKSAMPLES2', kind='Tube',name='Container4ProjectLinkSamples2')
+
+        (individual, errors, warnings) = get_or_create_individual(name='Individual4ProjectLinkSamples', taxon='Homo sapiens')
+
+        self.sample1, _, _ = create_sample(name=self.sample1_name, volume=100, concentration=25, collection_site='TestCaseSite',
+                      creation_date=datetime.datetime(2021, 1, 15, 0, 0),
+                      container=container1, individual=individual, sample_kind=sample_kind)
+
+        self.sample2, _, _ = create_sample(name=self.sample2_name, volume=80, concentration=20, collection_site='TestCaseSite',
+                      creation_date=datetime.datetime(2021, 1, 15, 0, 0),
+                      container=container2, individual=individual, sample_kind=sample_kind)
+
+        self.project1, _, _ = create_project(name=self.project1_name)
+        self.project2, _, _ = create_project(name=self.project2_name)
+
+    def test_import(self):
+        # Basic test for all templates - checks that template is valid
+        result = load_template(importer=self.importer, file=self.file)
+        self.assertEqual(result['valid'], True)
+
+        #Custom tests for each template
+        self.assertTrue(SampleByProject.objects.count(), 3)
+        self.assertTrue(SampleByProject.object.get(sample=self.sample1, project=self.project1).exists())
+        self.assertTrue(SampleByProject.object.get(sample=self.sample2, project=self.project1).exists())
+        self.assertTrue(SampleByProject.object.get(sample=self.sample2, project=self.project2).exists())
+        self.assertFalse(SampleByProject.object.get(sample=self.sample2, project=self.project2).exists())
+
+
+
