67a0cc97edafc8187eb9644e2005fb3d20262dde
jleblond <jleblond@users.noreply.github.com>
2021-10-19 11:14:26 -0400

    Correct sample update TI volume/delta_volume

--- backend/fms_core/template_importer/importers/sample_update.py
+++ backend/fms_core/template_importer/importers/sample_update.py
@@ -3,6 +3,8 @@ from fms_core.models import Process, Protocol
 from ._generic import GenericImporter
 from fms_core.template_importer.row_handlers.sample_update import SampleRowHandler
 
+from .._utils import float_to_decimal_and_none
+
 class SampleUpdateImporter(GenericImporter):
     SHEETS_INFO = [
         {'name': 'SampleUpdate', 'header_row_nb': 5},
@@ -28,19 +30,19 @@ class SampleUpdateImporter(GenericImporter):
                 'container': {'barcode': row_data['Container Barcode']}
             }
             sample_updated = {
-                'volume': row_data['New Volume (uL)'],
-                'concentration': row_data['New Conc. (ng/uL)'],
+                'volume': float_to_decimal_and_none(row_data['New Volume (uL)']),
+                'concentration': float_to_decimal_and_none(row_data['New Conc. (ng/uL)']),
                 'depleted': row_data['Depleted'],
             }
 
             process_measurement = {
                 'process': self.preloaded_data['process'],
-                'volume_used': row_data['Delta Volume (uL)'],
                 'execution_date': row_data['Update Date'],
                 'comment': row_data['Update Comment'],
             }
 
             sample_update_kwargs = dict(
+                delta_volume=float_to_decimal_and_none(row_data['Delta Volume (uL)']),
                 sample=sample,
                 sample_updated=sample_updated,
                 process_measurement=process_measurement,
--- backend/fms_core/template_importer/row_handlers/sample_update/sample.py
+++ backend/fms_core/template_importer/row_handlers/sample_update/sample.py
@@ -10,10 +10,8 @@ class SampleRowHandler(GenericRowHandler):
         super().__init__()
 
 
-    def process_row_inner(self, sample, sample_updated, process_measurement):
+    def process_row_inner(self, delta_volume, sample, sample_updated, process_measurement):
         print('start sample row handler')
-
-        comment = f"Automatically generated via Sample update Template on {datetime.utcnow().isoformat()}Z"
 
         sample_to_update, self.errors['sample'], self.warnings['sample'] = get_sample_from_container(
             barcode=sample['container']['barcode'],
@@ -21,29 +19,38 @@ class SampleRowHandler(GenericRowHandler):
         )
 
         if sample_to_update:
+            depleted = None
             if sample_updated['depleted'] == 'YES':
                 depleted = True
             elif sample_updated['depleted'] == 'NO':
                 depleted = False
-            else:
-                depleted = None
+
+            volume_used = None
+            new_volume = None
+            if sample_updated['volume']:
+                volume_used = sample_to_update.volume - sample_updated['volume']
+                new_volume = sample_updated['volume']
+                if delta_volume and delta_volume != volume_used:
+                    self.errors['update_volume'] = f"Delta volume and New volume are both present and do not match."
+            elif delta_volume:
+                volume_used = delta_volume
+                new_volume = sample_to_update.volume - delta_volume
 
             _, self.errors['sample_update'], self.warnings['sample_update'] = update_sample(
                 sample_to_update=sample_to_update,
-                volume=sample_updated['volume'],
+                volume=new_volume,
                 concentration=sample_updated['concentration'],
                 depleted=depleted,
             )
 
             print('sample update sample row handler', sample_to_update.__dict__)
 
-
             _, self.errors['process_measurement'], self.warnings['process_measurement'] = \
                 create_process_measurement(
                     process=process_measurement['process'],
                     source_sample=sample_to_update,
                     execution_date=process_measurement['execution_date'],
-                    volume_used=process_measurement['volume_used'],
+                    volume_used=volume_used,
                     comment=process_measurement['comment'],
                 )
 
