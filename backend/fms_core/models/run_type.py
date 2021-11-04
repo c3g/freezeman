a234d03db8e0b277bda6dc031831c449f04e8716
UlysseFG <ulysse.fortiergauthier@mcgill.ca>
2021-11-04 15:35:50 -0400

    replaced dict by query

--- backend/fms_core/models/run_type.py
+++ backend/fms_core/models/run_type.py
@@ -12,20 +12,6 @@ from ._utils import add_error as _add_error
 
 __all__ = ["RunType"]
 
-PROTOCOLS_BY_RUN_TYPE_NAME = {
-    'Infinium Global Screening Array-24':
-        { 'Illumina Infinium Preparation': [ 'Infinium: Amplification',
-                                             'Infinium: Fragmentation',
-                                             'Infinium: Precipitation',
-                                             'Infinium: Hybridization',
-                                             'Infinium: Wash Beadchip',
-                                             'Infinium: Extend and Stain',
-                                             'Infinium: Scan Preparation'
-                                             ]
-        }
-
-}
-
 @reversion.register()
 class RunType(TrackedModel):
     name = models.CharField(unique=True, max_length=STANDARD_NAME_FIELD_LENGTH,
@@ -53,15 +39,6 @@ class RunType(TrackedModel):
         self.full_clean()
         super().save(*args, **kwargs)  # Save the object
 
-    # REPLACE and move to procotol
     @property
     def get_protocols_dict(self):
-        protocols_dict = {}
-
-        exp_type_protocols = PROTOCOLS_BY_RUN_TYPE_NAME[self.name]
-        for protocol_name in exp_type_protocols.keys():
-            p = Protocol.objects.get(name=protocol_name)
-            subprotocol_names = exp_type_protocols[protocol_name]
-            protocols_dict[p] = list(Protocol.objects.filter(name__in=subprotocol_names))
-
-        return protocols_dict
+        return {self.protocol: list(self.protocol.child_of.all())}
