8e8da01455996deddeb723631a84abd700248165
Sebastian Ballesteros <tatanballesteros@hotmail.com>
2021-12-01 11:53:27 -0500

    Added Unit Test for SampleQC

---
+++ backend/fms_core/tests/test_template_importers/sample_qc_test.py
@@ -0,0 +1,86 @@
+from django.test import TestCase
+from django.contrib.contenttypes.models import ContentType
+import datetime
+
+from fms_core.template_importer.importers import SampleQCImporter
+from fms_core.tests.test_template_importers._utils import load_template, APP_DATA_ROOT
+
+from fms_core.models import Sample, SampleKind, ProcessMeasurement, PropertyType, PropertyValue
+
+from fms_core.services.container import create_container
+from fms_core.services.individual import get_or_create_individual
+from fms_core.services.sample import create_full_sample
+
+
+class SampleQCTestCase(TestCase):
+    def setUp(self) -> None:
+        self.importer = SampleQCImporter()
+        self.file = APP_DATA_ROOT / "Sample_QC_vtest.xlsx"
+        ContentType.objects.clear_cache()
+
+        self.sample_name = 'SampleTestQC'
+        self.sample_new_volume = 98
+        self.process_volume_used = 2
+        self.sample_new_concentration = 20
+        self.sample_new_depleted = False
+        self.update_date = datetime.datetime(2021, 12, 21, 0, 0)
+        self.quality_flag = True
+        self.quantity_flag = True
+        self.process_ql_instrument = 'Agarose Gel'
+        self.process_qt_instrument = 'PicoGreen'
+        self.prefill_data()
+
+
+    def prefill_data(self):
+        sample_kind, _ = SampleKind.objects.get_or_create(name='DNA')
+
+        (container, errors, warnings) = create_container(barcode='CONTAINER4SAMPLEQC', kind='Tube', name='Container4SampleQC')
+
+        (individual, errors, warnings) = get_or_create_individual(name='Individual4SampleQC', taxon='Homo sapiens')
+
+        create_full_sample(name=self.sample_name, volume=100, concentration=25, collection_site='TestCaseSite',
+                           creation_date=datetime.datetime(2021, 1, 15, 0, 0),
+                           container=container, individual=individual, sample_kind=sample_kind)
+
+
+    def test_import(self):
+        # Basic test for all templates - checks that template is valid
+        result = load_template(importer=self.importer, file=self.file)
+        self.assertEqual(result['valid'], True)
+
+        # Custom tests for each template
+        # Sample information tests
+        sample = Sample.objects.get(name=self.sample_name)
+        self.assertEqual(sample.volume, self.sample_new_volume)
+        self.assertEqual(sample.concentration, self.sample_new_concentration)
+        self.assertEqual(sample.depleted, self.sample_new_depleted)
+
+        # Process measurement tests
+        self.assertTrue(ProcessMeasurement.objects.get(source_sample=sample,
+                                            execution_date=self.update_date
+                                            ))
+        pm = ProcessMeasurement.objects.get(source_sample=sample,
+                                            execution_date=self.update_date
+                                            )
+        self.assertEqual(pm.volume_used, self.process_volume_used)
+
+        self.assertEqual(pm.process.protocol.name, 'Sample Quality Control')
+
+        # Derived sample flag tests
+        derived_sample = sample.derived_sample_not_pool
+        self.assertEqual(derived_sample.quality_flag, self.quality_flag)
+        self.assertEqual(derived_sample.quantity_flag, self.quantity_flag)
+
+        # Property Values tests
+        pt_1 = PropertyType.objects.get(name='Quantitation Instrument', object_id=pm.process.protocol.id)
+        p_1 = PropertyValue.objects.get(property_type_id=pt_1, object_id=pm.id)
+
+        pt_2 = PropertyType.objects.get(name='Electrophoresis Instrument', object_id=pm.process.protocol.id)
+        p_2 = PropertyValue.objects.get(property_type_id=pt_2, object_id=pm.id)
+
+        self.assertEqual(p_1.value, self.process_qt_instrument)
+        self.assertEqual(p_2.value, self.process_ql_instrument)
+
+
+
+
