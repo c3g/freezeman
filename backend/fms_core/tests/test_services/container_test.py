c4fb602d834e126c48fd5256683e1e1e56e4fa77
UlysseFG <ulysse.fortiergauthier@mcgill.ca>
2021-12-06 17:26:20 -0500

    Slow start

---
+++ backend/fms_core/tests/test_services/container_test.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+from fms_core.services import container
+
+class ContainerServicesTestCase(TestCase):
+    def setUp(self) -> None:
+        pass
+
+    def test_get_container(self):
+        pass
