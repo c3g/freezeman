c833eebb5ee5e59f1cf9c3e1cc05c0399b035008
UlysseFG <ulysse.fortiergauthier@mcgill.ca>
2022-01-28 16:30:36 -0500

    small fix to volume update

--- backend/fms_core/template_importer/row_handlers/sample_selection_qpcr/sample.py
+++ backend/fms_core/template_importer/row_handlers/sample_selection_qpcr/sample.py
@@ -6,6 +6,8 @@ from fms_core.services.sample import get_sample_from_container, update_sample
 from fms_core.services.process_measurement import create_process_measurement
 from fms_core.services.property_value import create_process_measurement_properties
 
+from fms_core.utils import check_truth_like
+
 class SampleSelectionQPCRRowHandler(GenericRowHandler):
     def __init__(self):
         super().__init__()
@@ -16,36 +18,29 @@ class SampleSelectionQPCRRowHandler(GenericRowHandler):
             coordinates=sample['coordinates'],
         )
 
-        if sample_obj:
-            # Update sample if Depleted
-            depleted = None
-            if sample['depleted'] == 'YES':
-                depleted = True
-            elif sample['depleted'] == 'NO':
-                depleted = False
-
-            if (process_measurement['volume_used']):
-                new_volume = sample_obj.volume - process_measurement['volume_used']
+        if sample_obj:         
+            if not process_measurement['volume_used']:
+                self.errors['volume used'] = 'Volume used is required.'
             else:
-                self.errors['volume'] = 'Volume used is required.'
+                process_measurement_obj, self.errors['process_measurement'], self.warnings['process_measurement'] = \
+                    create_process_measurement(
+                        process=process_measurement['process'],
+                        source_sample=sample_obj,
+                        execution_date=process_measurement['execution_date'],
+                        volume_used=process_measurement['volume_used'],
+                        comment=process_measurement['comment'],
+                    )
 
-            
-            process_measurement_obj, self.errors['process_measurement'], self.warnings['process_measurement'] = \
-                create_process_measurement(
-                    process=process_measurement['process'],
-                    source_sample=sample_obj,
-                    execution_date=process_measurement['execution_date'],
-                    volume_used=process_measurement['volume_used'],
-                    comment=process_measurement['comment'],
-                )
+                # Create process measurement's properties
+                if process_measurement_obj:
+                    properties_obj, self.errors['properties'], self.warnings['properties'] = create_process_measurement_properties(
+                        process_measurement_properties,
+                        process_measurement_obj)
 
-            # Create process measurement's properties
-            if process_measurement_obj:
-                properties_obj, self.errors['properties'], self.warnings['properties'] = create_process_measurement_properties(
-                    process_measurement_properties,
-                    process_measurement_obj)
-
-            if sample['depleted']:
-                _, self.errors['sample_update'], self.warnings['sample_update'] = \
-                    update_sample(sample_to_update=sample_obj, volume=new_volume, depleted=depleted)
+                    # Update sample with Depleted and volume new values
+                    depleted = check_truth_like(sample['depleted']) if check_truth_like(sample['depleted']) else None
+                    new_volume = sample_obj.volume - process_measurement['volume_used']
+                    
+                    _, self.errors['sample_update'], self.warnings['sample_update'] = \
+                        update_sample(sample_to_update=sample_obj, volume=new_volume, depleted=depleted)
 
