3181ea53c0f78feddc7166c8b87d115e0a3c0c84
ufgauthi <ulysse.fortiergauthier@mcgill.ca>
2022-02-23 12:33:28 -0500

    Changes to validate form input.

--- backend/fms_core/viewsets/index.py
+++ backend/fms_core/viewsets/index.py
@@ -6,7 +6,7 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from fms_core.services.index import validate_indices
-from fms_core.models import Index, IndexSet
+from fms_core.models import Index, IndexSet, InstrumentType
 from fms_core.serializers import IndexSerializer, IndexExportSerializer, IndexSetSerializer
 from fms_core.template_importer.importers import IndexCreationImporter
 from fms_core.templates import INDEX_CREATION_TEMPLATE
@@ -15,6 +15,8 @@ from ._utils import TemplateActionsMixin, _list_keys
 from ._constants import _index_filterset_fields
 
 from fms_core.filters import IndexFilter
+
+from collections import defaultdict
 
 
 class IndexViewSet(viewsets.ModelViewSet, TemplateActionsMixin):
@@ -50,13 +52,40 @@ class IndexViewSet(viewsets.ModelViewSet, TemplateActionsMixin):
 
     @action(detail=False, methods=["get"])
     def validate(self, _request):
-        indices_ids = _request.GET.get("indices", "").split(",")
-        instrument_type_id = _request.GET.get("instrument_type", -1) # defaults to an invalid id
-        length_5prime = _request.GET.get("5prime_length", 0)
-        length_3prime = _request.GET.get("3prime_length", 0)
+        form_errors = defaultdict(list)
+        indices = []
+        errors = []
+        warnings = []
+        results = {}
+        
+        indices_ids = [int(id) for id in _request.GET.get("indices", "").split(",")]
+        for index_id in indices_ids:
+            try:
+                indices.append(Index.objects.get(id=index_id))
+            except Index.DoesNotExist:
+                form_errors["indices"].append(f"Index with id {index_id} does not exist.")
+                
+        instrument_type_id = int(_request.GET.get("instrument_type", -1)) # defaults to an invalid id
+        try:
+            instrument_type = InstrumentType.objects.get(id=instrument_type_id)
+        except InstrumentType.DoesNotExist:
+            form_errors["instrument_type"].append(f"Instrument type with id {instrument_type_id} does not exist.")
+        
+        length_5prime = int(_request.GET.get("5prime_length", 0))
+        if length_5prime < 0:
+            form_errors["length_5prime"].append(f"Validation length for index at 5 prime end cannot be negative.")
+        length_3prime = int(_request.GET.get("3prime_length", 0))
+        if length_3prime < 0:
+            form_errors["length_3prime"].append(f"Validation length for index at 3 prime end cannot be negative.")
+
         threshold = _request.GET.get("threshold", None)
-        results, errors, warnings = validate_indices(indices_ids, instrument_type_id, length_5prime, length_3prime, threshold)
-        data = {"validation_errors": ValidationError(errors),
+        threshold = threshold if threshold is None else int(threshold)
+        if threshold < 0:
+            form_errors["threshold"].append(f"Distance threshold cannot be negative.")
+        if not form_errors:
+            results, errors, warnings = validate_indices(indices, instrument_type, length_5prime, length_3prime, threshold)
+        data = {"form_errors": form_errors,
+                "validation_errors": ValidationError(errors),
                 "warnings": warnings,
                 "results": results}
         return Response(data)
