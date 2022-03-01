1c2bfcfc7ab559a10023786f41ab71082dbc18d9
UlysseFG <ulysse.fortiergauthier@mcgill.ca>
2022-03-01 17:06:26 -0500

    variable renaming and pool expansion message

--- frontend/src/components/DropdownListItems.js
+++ frontend/src/components/DropdownListItems.js
@@ -1,24 +1,24 @@
 import React from "react";
-import {Button, Dropdown, Menu} from "antd";
+import {Dropdown, Menu} from "antd";
 import {CloseOutlined, LineOutlined, MenuOutlined} from "@ant-design/icons";
 
-const DropdownListItems = ({ListItems}) => {
+const DropdownListItems = ({listItems}) => {
   const menuListItems = (
     <Menu>
-      {ListItems ? ListItems.map((item, i) => <Menu.Item key={i.toString()}>{item}</Menu.Item>) : <Menu.Item/>}
+      {listItems ? listItems.map((item, i) => <Menu.Item key={i.toString()}>{item}</Menu.Item>) : <Menu.Item/>}
     </Menu>
   );
 
   return (
-    ListItems && ListItems.length > 1 ? 
+    listItems && listItems.length > 1 ? 
     <Dropdown overlay={menuListItems} placement="bottomRight">
       <div>
-        <MenuOutlined /> {ListItems && "Expand pool..."}
+        <MenuOutlined /> {listItems && "Expand pool..."}
       </div>
     </Dropdown>
     :
     <div>
-      {ListItems ? <LineOutlined /> : <CloseOutlined />} {ListItems && ListItems[0]}
+      {listItems.length > 0 ? <LineOutlined /> : <CloseOutlined />} {listItems && listItems[0]}
     </div>
   )
 }
