dc8d64f24d135d27ac3d0b07c8de25d12e0fbcf7
Nafiz Islam <nafiz.islam@mail.mcgill.ca>
2022-05-19 14:03:58 -0400

    reformat/refactor SampleDetailsLineage.js

--- frontend/src/components/samples/details/SampleDetailsLineage.js
+++ frontend/src/components/samples/details/SampleDetailsLineage.js
@@ -23,9 +23,35 @@ const SampleDetailsLineage = ({
   const [graphData, setGraphData] = useState({ nodes: [], links: [] })
   const [pairToProcess, setPairToProcess] = useState({})
 
-  // TODO automatically adjust width and height
-  const [width, setWidth] = useState(1280)
-  const [height, setHeight] = useState(720)
+  const nodeSize = { width: 10, height: 10 }
+
+  const dagreConfig = { rankdir: "TB", ranksep: 150, nodesep: 150, marginx: 50, marginy: 50 }
+
+  const graphConfig = {
+    width: 720,
+    height: 1280,
+    staticGraphWithDragAndDrop: true,
+    maxZoom: 12,
+    minZoom: 0.05,
+    panAndZoom: true,
+    node: {
+      color: "#d3d3d3",
+      fontColor: "black",
+      fontSize: 14,
+      renderLabel: true,
+      labelProperty: node => `${node.label}`,
+      labelPosition: "bottom",
+    },
+    link: {
+      color: "lightgray",
+      fontColor: "black",
+      fontSize: 14,
+      strokeWidth: 5,
+      type: "STRAIGHT",
+      renderLabel: true,
+      labelProperty: link => `${link.label}`
+    },
+  }
 
   useEffect(() => {
     if (sample?.id !== undefined) {
@@ -33,8 +59,8 @@ const SampleDetailsLineage = ({
         graph = graph.data
 
         const g = new dagre.graphlib.Graph({ directed: true })
-          .setGraph({ rankdir: "LR", ranksep: 150, nodesep: 150, marginx: 50, marginy: 50 })
-          .setDefaultEdgeLabel(function () { return {}; });
+                           .setGraph(dagreConfig)
+                           .setDefaultEdgeLabel(function () { return {}; });
 
         graph.nodes.forEach((curr_sample) => {
           let color = "black"
@@ -45,27 +71,24 @@ const SampleDetailsLineage = ({
           g.setNode(curr_sample.id.toString(), {
             id: curr_sample.id.toString(),
             label: curr_sample.name,
-            width: 10,
-            height: 10,
+            ...nodeSize,
             color,
             symbolType: curr_sample.id === sample.id ? "star" : "circle",
           })
         })
 
-        graph.edges
-          .filter((process) => process.child_sample !== null)
-          .forEach((process) => {
-            g.setEdge(process.source_sample, process.child_sample)
-          })
+        graph.edges.filter((process) => process.child_sample !== null)
+                   .forEach((process) => {
+                      g.setEdge(process.source_sample, process.child_sample)
+                    })
 
-        const localPairToProcess = graph.edges
-          .filter((process) => process.child_sample !== null)
-          .reduce((prev, p) => {
-            return {
-              ...prev,
-              [`${p.source_sample}:${p.child_sample}`]: p
-            }
-          }, {})
+        const localPairToProcess = graph.edges.filter((process) => process.child_sample !== null)
+                                              .reduce((prev, p) => {
+                                                return {
+                                                  ...prev,
+                                                  [`${p.source_sample}:${p.child_sample}`]: p
+                                                }
+                                              }, {})
         
         setPairToProcess(localPairToProcess)
 
@@ -73,52 +96,26 @@ const SampleDetailsLineage = ({
 
         setGraphData({
           nodes: g.nodes()
-            .map((v) => {
-              return {
-                ...g.node(v),
-                id: v
-              }
-            }),
+                  .map((v) => {
+                    return {
+                      ...g.node(v),
+                      id: v
+                    }
+                  }),
           links: g.edges()
-            .map((e) => {
-              const p = localPairToProcess[`${e.v}:${e.w}`]
-              return {
-                id: p.id.toString(),
-                source: e.v,
-                target: e.w,
-                label: p.protocol_name,
-              }
-            })
+                  .map((e) => {
+                    const p = localPairToProcess[`${e.v}:${e.w}`]
+                    return {
+                      id: p.id.toString(),
+                      source: e.v,
+                      target: e.w,
+                      label: p.protocol_name,
+                    }
+                  })
         })
       })
     }
   }, [sample])
-
-  const graphConfig = {
-    height,
-    width,
-    staticGraphWithDragAndDrop: true,
-    maxZoom: 12,
-    minZoom: 0.05,
-    panAndZoom: true,
-    node: {
-      color: "#d3d3d3",
-      fontColor: "black",
-      fontSize: 12,
-      renderLabel: true,
-      labelProperty: node => `${node.label}`,
-      labelPosition: "top",
-    },
-    link: {
-      color: "lightgray",
-      fontColor: "black",
-      fontSize: 12,
-      strokeWidth: 5,
-      type: "STRAIGHT",
-      renderLabel: true,
-      labelProperty: link => `${link.label}`
-    },
-  }
 
   return (
     <>
