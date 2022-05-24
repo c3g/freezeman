b770890ae6b8df37d8e7220d6bf70e57c622b6c8
UlysseFG <ulysse.fortiergauthier@mcgill.ca>
2022-05-24 18:09:35 -0400

    Fix sample update delta volume.

--- backend/fms_core/template_importer/row_handlers/sample_update/sample.py
+++ backend/fms_core/template_importer/row_handlers/sample_update/sample.py
@@ -28,11 +28,11 @@ class SampleRowHandler(GenericRowHandler):
             if sample_updated['volume']:
                 volume_used = sample_to_update.volume - sample_updated['volume']
                 new_volume = sample_updated['volume']
-                if delta_volume and delta_volume != volume_used:
+                if delta_volume and delta_volume != -volume_used:
                     self.errors['update_volume'] = f"Delta volume and New volume are both present and do not match."
             elif delta_volume:
-                volume_used = delta_volume
-                new_volume = sample_to_update.volume - delta_volume
+                volume_used = -delta_volume
+                new_volume = sample_to_update.volume + delta_volume
 
             _, self.errors['sample_update'], self.warnings['sample_update'] = update_sample(
                 sample_to_update=sample_to_update,
