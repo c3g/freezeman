79cd7fc38af437c33a93bd9442ae32e9a09e2399
Nafiz Islam <nafiz.islam@mail.mcgill.ca>
2022-06-01 11:44:30 -0400

    create NumberFormat utility function

--- frontend/src/utils/functions.js
+++ frontend/src/utils/functions.js
@@ -1,1 +1,6 @@
 export const constVal = x => () => x;
+
+export function NumberFormat(number, afterDecimal = 0, round = true) {
+    const regex = RegExp(`(^[0-9]+\.[0-9]{0,${afterDecimal}})|(^[0-9]+)`, "g")
+    return number.toFixed(afterDecimal + (round ? 0 : 1)).match(regex)[0]
+}
