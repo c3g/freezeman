d484a956f2a3d2011e4209b44bf6aa586bc65202
nafiz1001 <nafiz.islam1001@gmail.com>
2022-06-10 15:56:59 -0400

    count by biosample

--- backend/fms_core/services/report.py
+++ backend/fms_core/services/report.py
@@ -1,10 +1,4 @@
 from datetime import datetime
-from typing import Any, List
+from typing import Any, Iterable, List
 from django.db.models import Count, F, Q, QuerySet
-from fms_core.models import SampleByProject
-
-def samples_by_project(project, start_date: datetime, end_date: datetime):
-    return SampleByProject.objects.filter(project=project) \
-                                  .annotate(creation_date=F("sample__creation_date")) \
-                                  .filter(creation_date__gte=start_date, creation_date__lte=end_date)
-
+from fms_core.models import Sample, SampleByProject, Biosample, DerivedSample
--- backend/fms_core/viewsets/report.py
+++ backend/fms_core/viewsets/report.py
@@ -1,66 +1,42 @@
 from datetime import datetime, timedelta
+from django.db.models.query import QuerySet
 from django.forms import ValidationError
 from rest_framework.response import Response
 from rest_framework import viewsets
-from fms_core.models import ProcessMeasurement, Project, Sample, SampleByProject
+from fms_core.models import ProcessMeasurement, Project, Sample, SampleByProject, DerivedSample
 import fms_core.services.report as service
 from django.db.models import Count, F, Q
-from django.contrib.postgres.aggregates import ArrayAgg
 from rest_framework.decorators import action
-from ._constants import _project_minimal_filterset_fields
+
+def samples_by_project(project: Project) -> QuerySet[SampleByProject]:
+    return SampleByProject.objects.filter(project=project)
+
+def derived_samples(sbps: QuerySet[SampleByProject]) -> QuerySet[DerivedSample]:
+    ds_ids = sbps.values_list("sample__derived_samples", flat=True)
+    dss = DerivedSample.objects.filter(id__in=ds_ids)
+    return dss
+
+def count_biosamples(dss: QuerySet[DerivedSample]) -> int:
+    return dss.values_list("biosample", flat=True).distinct().count()
 
 class ReportViewSet(viewsets.ViewSet):
     @action(detail=False, methods=["get"])
     def summary(self, request):
         projectId = request.GET.get("project_id")
 
-        project = Project.objects.filter(pk=projectId)
+        project = Project.objects.filter(pk=projectId).first()
 
-        if project.exists():
-            project = project[0]
+        if project:
+
+            sbps = samples_by_project(project)
+
+            dss = derived_samples(sbps)
 
             response = {}
+            response["total"] = count_biosamples(dss)
+            response["extracted"] = count_biosamples(dss.filter(sample_kind__is_extracted=True))
+            response["libraried"] = count_biosamples(dss.filter(~Q(library=None)))
 
-            end_date = datetime.today()
-            start_date = end_date - timedelta(days=300)
-
-            sbps = service.samples_by_project(project, start_date, end_date)
-            sbps = sbps.annotate(
-                child_of_count=Count("child_of"),
-                quality_flag=F("quality_flag"),
-                quantity_flag=F("quantity_flag"),
-            )
-            sbps = sbps.aggregate(
-                          libraries=ArrayAgg("sample__derived_samples__library"),
-                          extractions=ArrayAgg("sample__derived_samples__sample_kind__is_extracted"),
-                          sample_kinds=ArrayAgg("sample__derived_samples__sample_kind__name"),
-                      )
-
-            filters = {}
-
-            for t in filters:
-                response.setdefault(t, {
-                            "total": 0,
-                            "qc": {
-                                "passed": 0,
-                                "failed": 0,
-                            },
-                            "kinds": {}
-                        })
-
-            for sbp in sbps:
-                for t, f in filters.items():
-                    if f(sbp):
-                        response[t]["total"] += 1
-
-                        response[t]["qc"]["passed"] += (sbp.quality_flag == True and sbp.quantity_flag == True)
-                        response[t]["qc"]["failed"] += (sbp.quality_flag == False or sbp.quantity_flag == False)
-
-                        response[t].setdefault("kinds", {})
-                        key = sbp.sample_kind_name.lower()
-                        prev = response[t]["kinds"].setdefault(key, 0)
-                        response[t]["kinds"][key] = prev + 1
-            
             return Response(response)
         else:
-            raise ValidationError([f"Could not find projects with id {projectId}"])
+            raise ValidationError([f"Could not find projects with id '{projectId}'"])
