7e5d7e7dc68849b289a9ad41ec400f84afb2a08f
ufgauthi <ulysse.fortiergauthier@mcgill.ca>
2022-06-13 17:11:35 -0400

    Save the Template on the server and create and entry into importedFile.

--- backend/fms_core/viewsets/_utils.py
+++ backend/fms_core/viewsets/_utils.py
@@ -104,12 +104,11 @@ class TemplateActionsMixin:
             return HttpResponseBadRequest(json.dumps({"detail": action_data}), content_type="application/json")
 
         action_def, file = action_data
-        _, file_format = os.path.splitext(file.name)
 
         importer_instance = action_def["importer"]()
 
         try:
-            result = importer_instance.import_template(file=file, format=file_format, dry_run=True)
+            result = importer_instance.import_template(file=file, dry_run=True, user=request.user)
         except Exception as e:
             result = {
                 'valid': False,
@@ -133,12 +132,11 @@ class TemplateActionsMixin:
             return HttpResponseBadRequest(json.dumps({"detail": action_data}), content_type="application/json")
 
         action_def, file = action_data
-        _, file_format = os.path.splitext(file.name)
 
         importer_instance = action_def["importer"]()
 
         try:
-            importer_instance.import_template(file=file, format=file_format, dry_run=False)
+            importer_instance.import_template(file=file, dry_run=False, user=request.user)
             if not importer_instance.is_valid:
                 return HttpResponseBadRequest(json.dumps({"detail": "Template errors encountered in submission"}),
                                               content_type="application/json")
