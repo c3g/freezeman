a53d330208e912a90226da0cf8f4deaf305c82aa
nafiz1001 <nafiz.islam1001@gmail.com>
2022-06-23 14:12:59 -0400

    handle multiple files and optional args

--- backend/fms_core/viewsets/dataset.py
+++ backend/fms_core/viewsets/dataset.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 import json
-from typing import List
+from typing import Dict, List
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import action
@@ -12,18 +12,20 @@ import fms_core.services.dataset as service
 class DatasetViewSet(viewsets.ViewSet):
     def create(self, request, *args, **kwargs):
         data = request.data
+        options = data["options"]
+        run_processing_metrics = data["run_processing_metrics"]
 
         datasets = []
         dataset_files = []
         errors = []
         warnings = []
 
-        datasets, errors, warnings = create_datasets(data)
+        datasets, errors, warnings = create_datasets(run_processing_metrics)
         for dataset in datasets:
             if errors:
                 break
             else:
-                new_dataset_files, new_errors, new_warnings = create_dataset_files(data, dataset)
+                new_dataset_files, new_errors, new_warnings = create_dataset_files(options, run_processing_metrics, dataset)
                 errors.extend(new_errors)
                 warnings.extend(new_warnings)
                 dataset_files.extend(new_dataset_files)
@@ -48,57 +50,57 @@ class DatasetViewSet(viewsets.ViewSet):
                 "warnings": warnings,
             })
 
-def create_datasets(data):
+def create_datasets(run_processing_metrics: Dict):
     datasets = []
     errors = []
     warnings = []
+    rpm = run_processing_metrics
 
-    for rv in data["run_validation"]:
+    for rv in rpm["run_validation"]:
         dataset_args = {}
 
         dataset_args["project_name"] = rv["project"]
-        dataset_args["run_name"] = data["run"]
-        dataset_args["lane"] = data["lane"]
+        dataset_args["run_name"] = rpm["run"]
+        dataset_args["lane"] = rpm["lane"]
         dataset, errors, warnings = service.create_dataset(**dataset_args)
         datasets.append(dataset)
     
     return (datasets, errors, warnings)
 
-def create_dataset_files(data, dataset: Dataset):
+def create_dataset_files(options: Dict, run_processing_metrics: Dict, dataset: Dataset):
     dataset_files = []
     errors = []
     warnings = []
+    rpm = run_processing_metrics
 
     rv = None
-    for rv in data["run_validation"]:
-        if rv["project"] == dataset.project_name and data["run"] == dataset.run_name:
+    for rv in rpm["run_validation"]:
+        if rv["project"] == dataset.project_name and rpm["run"] == dataset.run_name:
             break
     if rv is None:
         errors.append("Could not find run validation for dataset")
 
-
     if errors:
         return (dataset_files, errors, warnings)
 
-    dataset_file_args = {}
-    for readset in data["readsets"].values():
+    for readset in rpm["readsets"].values():
         # TODO: actually find the closest matching sample_name
-        # TODO: create dataset_file for each file path
         if readset["sample_name"] in rv["sample"]:
-            dataset_file_args["dataset"] = dataset
-            dataset_file_args["file_path"] = readset["fastq_1"]
-            dataset_file_args["completion_date"] = datetime.now()
-            dataset_file_args["validation_date"] = datetime.now()
-            dataset_file_args["sample_name"] = readset["sample_name"]
+            for key in readset:
+                if key not in ["sample_name", "barcodes"]:
+                    dataset_file_args = {}
+                    dataset_file_args["dataset"] = dataset
+                    dataset_file_args["file_path"] = readset[key]
+                    dataset_file_args["completion_date"] = options.get("completion_date", None)
+                    dataset_file_args["validation_date"] = options.get("validation_date", None)
+                    dataset_file_args["sample_name"] = readset["sample_name"]
     
-    if dataset_file_args:
-        dataset_file, newerrors, newwarnings = service.create_dataset_file(**dataset_file_args)
-        errors.extend(newerrors)
-        warnings.extend(newwarnings)
+                    dataset_file, newerrors, newwarnings = service.create_dataset_file(**dataset_file_args)
+                    errors.extend(newerrors)
+                    warnings.extend(newwarnings)
 
-        if not errors:
-            dataset_files.append(dataset_file)
-    else:
-        errors.append("Could not find readset for dataset")
+                    if not newerrors:
+                        dataset_files.append(dataset_file)
+                        
 
-    return ([] if errors else dataset_files, errors, warnings)
+    return (dataset_files, errors, warnings)
