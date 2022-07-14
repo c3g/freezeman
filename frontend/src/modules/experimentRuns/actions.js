ce584414740e3b3fdc0e7aa87a8f2fef2ac3ffdf
Nafiz Islam <nafiz.islam1001@gmail.com>
2022-07-14 10:57:29 -0400

    fix propertyValuesByID?

--- frontend/src/modules/experimentRuns/actions.js
+++ frontend/src/modules/experimentRuns/actions.js
@@ -123,26 +123,27 @@ export const listProcessProperties = (id) => async (dispatch, getState) => {
     if (getState().propertyValues.isFetching)
         return;
 
-    const { itemsByID, propertyValuesByID } = getState().processes    
+    const { itemsByID: processesByID } = getState().processes    
+    const { itemsByID: propertyValuesByID } = getState().propertyValues
     
     return await (
         Promise.resolve()
     ).then(async () => {
-        const isLoaded = id in itemsByID;
+        const isLoaded = id in processesByID;
         if (!isLoaded) {
             return await dispatch(listProcesses({ id__in: id }))
         }
     }).then(async () => {
-        const process = itemsByID[id];
-        const childrenProcessesAreLoaded = process?.children_processes?.every(process => process in itemsByID)
+        const process = processesByID[id];
+        const childrenProcessesAreLoaded = process?.children_processes?.every(process => process in processesByID)
         
         if (!childrenProcessesAreLoaded) {
             return await dispatch(listProcesses({ id__in: process.children_processes.join() }))
         }
     }).then(async () => {
-        const process = itemsByID[id];
+        const process = processesByID[id];
         const propertiesAreLoaded = process?.children_properties?.every(property => property in propertyValuesByID)
-        const childrenPropertiesAreLoaded = process?.children_processes?.every(process => itemsByID[process]?.children_properties?.every(property => property in propertyValuesByID))
+        const childrenPropertiesAreLoaded = process?.children_processes?.every(process => processesByID[process]?.children_properties?.every(property => property in propertyValuesByID))
         const allPropertiesAreLoaded = propertiesAreLoaded && childrenPropertiesAreLoaded
 
         if (!allPropertiesAreLoaded) {
