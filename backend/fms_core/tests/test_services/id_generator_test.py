0b4c377c3134fb93fff0c2445bfb6da65b807271
UlysseFG <ulysse.fortiergauthier@mcgill.ca>
2022-07-19 15:58:08 -0400

    Basic for id_generator

---
+++ backend/fms_core/tests/test_services/id_generator_test.py
@@ -0,0 +1,22 @@
+from django.test import TestCase
+
+from fms_core.services.id_generator import get_unique_id
+
+class IndividualServicesTestCase(TestCase):
+
+    def test_get_unique_id(self):
+        """
+          Test assume 2 ids were generated during the model tests
+        """
+        THREE = 3
+        FOUR = 4
+        FIVE = 5
+
+        three = get_unique_id()
+        self.assertEqual(three, THREE)
+
+        four = get_unique_id()
+        self.assertEqual(four, FOUR)
+
+        five = get_unique_id()
+        self.assertEqual(five, FIVE)
