d8f4915b73a78545b65aa3a190b79c26d6c6b43b
ufgauthi <ulysse.fortiergauthier@mcgill.ca>
2022-07-20 16:55:14 -0400

    Prevent user lockups by correcting existing date greater than current date.

--- backend/fms_core/migrations/0040_v3_10_0.py
+++ backend/fms_core/migrations/0040_v3_10_0.py
@@ -1,5 +1,6 @@
 from django.conf import settings
-from django.db import migrations, models
+from django.db import migrations
+from datetime import date
 from django.contrib.auth.models import User
 import reversion
 
@@ -43,6 +44,37 @@ def add_property_types_for_normalization(apps, schema_editor):
                 reversion.add_to_revision(pt)
 
 
+def fix_future_dates(apps, schema_editor):
+    """
+    Set the creation_date and execution_date to the current date in the cases
+    where the dates are set to a date after the current date. This is to prevent
+    historical data to conflict with a new validation added with release v3.10.0.
+
+    Args:
+        apps: apps class handle
+        schema_editor: ignore
+    """
+    Sample = apps.get_model("fms_core", "Sample")
+    ProcessMeasurement = apps.get_model("fms_core", "ProcessMeasurement")
+
+    with reversion.create_revision(manage_manually=True):
+        admin_user = User.objects.get(username=ADMIN_USERNAME)
+        today = date.today()
+
+        reversion.set_comment(f"Change dates in the future to pass validation (set to current date {today}).")
+        reversion.set_user(admin_user)
+
+        for future_sample in Sample.objects.all().filter(creation_date__gt=today):
+            future_sample.creation_date=today
+            future_sample.save()
+            reversion.add_to_revision(future_sample)
+
+        for future_pm in ProcessMeasurement.objects.all().filter(execution_date__gt=today):
+            future_pm.execution_date=today
+            future_pm.save()
+            reversion.add_to_revision(future_pm)
+
+
 class Migration(migrations.Migration):
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
@@ -52,6 +84,10 @@ class Migration(migrations.Migration):
     operations = [
         migrations.RunPython(
             add_property_types_for_normalization,
+            reverse_code=migrations.RunPython.noop,
+        ),
+        migrations.RunPython(
+            fix_future_dates,
             reverse_code=migrations.RunPython.noop,
         )
     ]
