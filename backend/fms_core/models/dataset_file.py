452ae1f8367e701b63f2826374f9c44da850443e
Nafiz Islam <nafiz.islam@mail.mcgill.ca>
2022-07-27 09:29:16 -0400

    correct dataset file comments

--- backend/fms_core/models/dataset_file.py
+++ backend/fms_core/models/dataset_file.py
@@ -13,7 +13,7 @@ class DatasetFile(TrackedModel):
     """ Class to store information about the files associated with their dataset for data deliveries. """
 
     dataset = models.ForeignKey(Dataset, on_delete=models.PROTECT, help_text="The dataset of the file", related_name="files")
-    file_path = models.CharField(max_length=STANDARD_FILE_PATH_LENGTH, help_text="File path to the dataset")
+    file_path = models.CharField(max_length=STANDARD_FILE_PATH_LENGTH, help_text="Path to the dataset file")
 
     # derived_sample = models.ForeignKey(DerivedSample, null=True, blank=True, on_delete=models.PROTECT, help_text="The derived sample that correspond to the dataset")
     sample_name = models.CharField(max_length=STANDARD_NAME_FIELD_LENGTH, help_text="The sample that corresponds with this file")
