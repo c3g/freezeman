9d6bb42b7cdfed40d106d2d723504659494bf4fe
Nafiz Islam <nafiz.islam1001@gmail.com>
2022-08-02 13:48:48 -0400

    document useToken

--- frontend/src/hooks/useToken.js
+++ frontend/src/hooks/useToken.js
@@ -2,6 +2,13 @@ import { useState } from "react";
 import { useSelector } from "react-redux";
 import { withToken } from "../utils/api";
 
+/**
+ * 
+ * @param {any} initialState 
+ * @param {(...any) => any} apiFn 
+ * @param {any[]} args 
+ * @returns the current state and a dispatcher that takes a function that takes a response of a successful request.
+ */
 export const useToken = (initialState, apiFn, args) => {
     const [state, setState] = useState(initialState)
     const token = useSelector((state) => state.auth.tokens.access)
