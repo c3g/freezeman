b9a356ddfd18a440209c80002b1add33809a8f7b
Chris Kostiw <104078019+ckostiw@users.noreply.github.com>
2022-08-04 15:22:25 -0400

    Fix corrupt imported template downloads RM#1199 (#360)
    * Fix corrupt imported template downloads
    
    * Init the mimetypes module
    
    * Force the xlsx mime type

--- backend/fms_core/viewsets/imported_file.py
+++ backend/fms_core/viewsets/imported_file.py
@@ -1,6 +1,5 @@
 import os
 import json
-import mimetypes
 
 from rest_framework import viewsets
 from rest_framework.decorators import action
@@ -40,12 +39,15 @@ class ImportedFileViewSet(viewsets.ModelViewSet):
         queryset = self.get_queryset().filter(id=pk)
         filename = queryset.first().filename
         file_path = os.path.join(settings.TEMPLATE_UPLOAD_PATH, filename)
-        mime_type_guess = mimetypes.guess_type(file_path)
         
         try:
             with open(file_path, "rb") as file:
                 response = HttpResponse(content=file)
-                response["Content-Type"] = mime_type_guess[0]
+                response["Content-Encoding"] = 'identity'
+                # NOTE: Currently, the frontend mangles template files by converting them to unicode text
+                # if the Content-Type is not set to the following value. 
+                # TODO: Remove this note once the frontend has been fixed.
+                response["Content-Type"] = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
                 response["Content-Disposition"] = "attachment; filename=" + filename
         except Exception as err:
             print(err)
