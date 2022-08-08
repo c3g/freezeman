14dcfaebd695c68886f284a4ab862626313457e0
ufgauthi <ulysse.fortiergauthier@mcgill.ca>
2022-08-08 17:32:37 -0400

    Simplification and production of files.

--- backend/fms_core/template_importer/row_handlers/normalization_planning/normalization_planning.py
+++ backend/fms_core/template_importer/row_handlers/normalization_planning/normalization_planning.py
@@ -9,6 +9,7 @@ from fms_core.services.property_value import create_process_measurement_properti
 
 from fms_core.utils import convert_concentration_from_nm_to_ngbyul
 
+import decimal
 
 class NormalizationPlanningRowHandler(GenericRowHandler):
     """
@@ -38,17 +39,22 @@ class NormalizationPlanningRowHandler(GenericRowHandler):
                 measurements['na_quantity'] is not None]) != 1:
             self.errors['concentration'] = 'Only one option must be specified out  of the following: NA quantity, conc. ng/uL or conc. nM'
 
-        if measurements['concentration_ngul']:
-            concentration_nguL = measurements['concentration_ngul']
-        elif measurements['concentration_nM']:
-            concentration_nM = measurements['concentration_nM']
-        elif measurements['na_quantity']:
-            #compute concentration in ngul
-            concentration_nguL = measurements['na_quantity'] / measurements['Final Volume']
-
         source_sample_obj, self.errors['sample'], self.warnings['sample'] = get_sample_from_container(
             barcode=source_sample['container']['barcode'],
             coordinates=source_sample['coordinates'])
+
+        if measurements['concentration_ngul']:
+            concentration_nguL = measurements['concentration_ngul']
+            na_qty = decimal.Decimal(measurements['Final Volume']) * decimal.Decimal(concentration_nguL)
+        elif measurements['concentration_nM']:
+            concentration_nM = measurements['concentration_nM']
+            na_qty = decimal.Decimal(measurements['Final Volume']) * decimal.Decimal(convert_concentration_from_nm_to_ngbyul(measurements['concentration_nM'],
+                                                                                                                             source_sample_obj.library.molecular_weight_approx,
+                                                                                                                             source_sample_obj.library.library_size))
+        elif measurements['na_quantity']:
+            #compute concentration in ngul
+            concentration_nguL = measurements['na_quantity'] / measurements['Final Volume']
+            na_qty = decimal.Decimal(measurements['na_quantity'])
 
         destination_container_dict = destination_sample['container']
 
@@ -58,6 +64,8 @@ class NormalizationPlanningRowHandler(GenericRowHandler):
                 barcode=parent_barcode)
         else:
             container_parent_obj = None
+
+        volume_used = na_qty / source_sample_obj.concentration
 
         if source_sample_obj and (container_parent_obj or not parent_barcode) and "concentration" not in self.errors.keys():
             self.row_object = {
@@ -75,7 +83,7 @@ class NormalizationPlanningRowHandler(GenericRowHandler):
                 'Destination Parent Container Barcode': destination_container_dict['barcode'],
                 'Destination Parent Container Coord': destination_container_dict['coordinates'],
                 'Source Depleted': '',
-                'Volume Used (uL)': '',
+                'Volume Used (uL)': str(volume_used),
                 'Volume (uL)': measurements['volume'],
                 'Conc. (ng/uL)': measurements['concentration_ngul'] if concentration_nguL else '',
                 'Conc. (nM)': measurements['concentration_nM'] if concentration_nM else '',
