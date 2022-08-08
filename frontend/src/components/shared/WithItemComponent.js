85ef7179af2eb04c6429c18f3f91e6d469d24de5
Nafiz Islam <nafiz.islam1001@gmail.com>
2022-08-08 15:41:01 -0400

    correct WithItemComponent

--- frontend/src/components/shared/WithItemComponent.js
+++ frontend/src/components/shared/WithItemComponent.js
@@ -2,8 +2,12 @@ import React from "react"
 import { useItem } from "../../hooks/useItem"
 
 export const WithItemComponent = (withItem) => (itemsByID, id, fn, defaultValue = null, render = (item) => <>{item}</>) => {
-    const item = useItem(withItem, itemsByID, id, fn, null)
-    return render(item ?? defaultValue)
+    const Container = ({}) => {
+        const item = useItem(withItem, itemsByID, id, fn, null)
+        return render(item ?? defaultValue)
+    }
+
+    return <Container />
 }
 
 export default WithItemComponent
