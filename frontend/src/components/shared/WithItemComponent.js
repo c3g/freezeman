e0f5ef8d9220282cfa0cbc127ba3dc4155c39e4b
Nafiz Islam <nafiz.islam1001@gmail.com>
2022-08-09 09:26:38 -0400

    use defaultValue

--- frontend/src/components/shared/WithItemComponent.js
+++ frontend/src/components/shared/WithItemComponent.js
@@ -3,8 +3,8 @@ import { useItem } from "../../hooks/useItem"
 
 export const WithItemComponent = (withItem) => (itemsByID, id, fn, defaultValue = null, render = (item) => <>{item}</>) => {
     const Container = ({}) => {
-        const item = useItem(withItem, itemsByID, id, fn, null)
-        return render(item ?? defaultValue)
+        const item = useItem(withItem, itemsByID, id, fn, defaultValue)
+        return render(item)
     }
 
     return <Container />
