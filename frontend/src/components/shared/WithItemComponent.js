3a1843614f51d5a667373fcacccd6ae4ae6e2695
Nafiz Islam <nafiz.islam1001@gmail.com>
2022-08-09 10:42:50 -0400

    export more functions in WithItemComponent

--- frontend/src/components/shared/WithItemComponent.js
+++ frontend/src/components/shared/WithItemComponent.js
@@ -1,5 +1,17 @@
 import React from "react"
 import { useItem } from "../../hooks/useItem"
+import { 
+    withContainer,
+    withSample,
+    withIndividual,
+    withUser,
+    withProcessMeasurement,
+    withProject,
+    withSequence,
+    withIndex,
+    withLibrary,
+    withTaxon
+} from "../../utils/withItem"
 
 /**
  * 
@@ -8,7 +20,7 @@ import { useItem } from "../../hooks/useItem"
  * A function that looks like a function created by `createWithItem` but it returns a React component
  * containing the value returned by `withItem`.
  */
-export const WithItemComponent = (withItem) => (itemsByID, id, fn, defaultValue = null) => {
+const WithItemComponent = (withItem) => (itemsByID, id, fn, defaultValue = null) => {
     const Container = ({}) => {
         const item = useItem(withItem, itemsByID, id, fn, defaultValue)
         return <>{item}</>
@@ -17,4 +29,26 @@ export const WithItemComponent = (withItem) => (itemsByID, id, fn, defaultValue 
     return <Container />
 }
 
-export default WithItemComponent
+export const withContainerComponent = WithItemComponent(withContainer)
+export const withSampleComponent = WithItemComponent(withSample)
+export const withIndividualComponent = WithItemComponent(withIndividual)
+export const withUserComponent = WithItemComponent(withUser)
+export const withProcessMeasurementComponent = WithItemComponent(withProcessMeasurement)
+export const withProjectComponent = WithItemComponent(withProject)
+export const withSequenceComponent = WithItemComponent(withSequence)
+export const withIndexComponent = WithItemComponent(withIndex)
+export const withLibraryComponent = WithItemComponent(withLibrary)
+export const withTaxonComponent = WithItemComponent(withTaxon)
+
+export default {
+    withContainerComponent,
+    withSampleComponent,
+    withIndividualComponent,
+    withUserComponent,
+    withProcessMeasurement,
+    withProjectComponent,
+    withSequenceComponent,
+    withIndexComponent,
+    withLibraryComponent,
+    withTaxonComponent,
+}
