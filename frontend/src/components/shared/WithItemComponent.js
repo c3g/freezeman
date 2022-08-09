da0fc27cc4d7cca45150fccf863773209738257f
Nafiz Islam <nafiz.islam1001@gmail.com>
2022-08-09 10:06:09 -0400

    document functions

--- frontend/src/components/shared/WithItemComponent.js
+++ frontend/src/components/shared/WithItemComponent.js
@@ -1,10 +1,17 @@
 import React from "react"
 import { useItem } from "../../hooks/useItem"
 
-export const WithItemComponent = (withItem) => (itemsByID, id, fn, defaultValue = null, render = (item) => <>{item}</>) => {
+/**
+ * 
+ * @param {(any, any, any, any) => any} withItem a function created by `createWithItem`
+ * @returns
+ * A function that looks like a function created by `createWithItem` but it returns a React component
+ * containing the value returned by `withItem`.
+ */
+export const WithItemComponent = (withItem) => (itemsByID, id, fn, defaultValue = null) => {
     const Container = ({}) => {
         const item = useItem(withItem, itemsByID, id, fn, defaultValue)
-        return render(item)
+        return <>{item}</>
     }
 
     return <Container />
--- frontend/src/hooks/useItem.js
+++ frontend/src/hooks/useItem.js
@@ -1,5 +1,15 @@
 import { useEffect, useState } from "react"
 
+/**
+ * A hook that wraps any functions created with `createWithItem`
+ * and ensures that the function is called inside the `useEffect` hook.
+ * @param {(any, any, any, any) => any} withItem a function created by `createWithItem`
+ * @param {object} itemsByID `itemsByID` argument of `withItem`
+ * @param {any} id `id` argument of `withItem`
+ * @param {(any) => any} fn `fn` argument of `withItem`
+ * @param {any} defaultValue `defaultValue` argument of `withItem`
+ * @returns the value returned by calling `withItem`
+ */
 export const useItem = (withItem, itemsByID, id, fn, defaultValue = null) => {
     const [value, setValue] = useState(defaultValue)
 
