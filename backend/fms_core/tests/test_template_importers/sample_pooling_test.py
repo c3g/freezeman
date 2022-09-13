00ba99ec04092ceb0caf5d81fd22b0ec6677ec61
UlysseFG <ulysse.fortiergauthier@mcgill.ca>
2022-09-13 11:05:31 -0400

    Some more testing content...

--- backend/fms_core/tests/test_template_importers/sample_pooling_test.py
+++ backend/fms_core/tests/test_template_importers/sample_pooling_test.py
@@ -6,7 +6,7 @@ from fms_core.template_importer.importers import SamplePoolingImporter
 from fms_core.tests.test_template_importers._utils import load_template, APP_DATA_ROOT
 from fms_core.tests.constants import create_sample_container, create_individual, create_fullsample
 
-from fms_core.models import SampleKind, LibraryType, Platform, Index, ProcessMeasurement, PropertyType, PropertyValue
+from fms_core.models import SampleKind, Protocol, Process, LibraryType, Platform, Index, ProcessMeasurement, PropertyType, PropertyValue
 from fms_core.models._constants import DOUBLE_STRANDED, SINGLE_STRANDED
 
 
@@ -26,9 +26,10 @@ class LibraryPreparationTestCase(TestCase):
 
         self.DNA_sample_kind, _ = SampleKind.objects.get_or_create(name='DNA')
         self.RNA_sample_kind, _ = SampleKind.objects.get_or_create(name="RNA")
+        self.protocol_pooling = Protocol.objects.get(name="Sample pooling")
 
         self.plate_source_name_and_barcode = "POOLFROMPLATE1"
-        self.source_sample_initial_volume = 200     
+        self.source_sample_initial_volume = 200
 
         self.prefill_data()
 
@@ -60,23 +61,27 @@ class LibraryPreparationTestCase(TestCase):
                                collection_site='PoolingTestSite', creation_date=datetime.datetime(2022, 8, 12, 0, 0), sample_kind=self.DNA_sample_kind,
                                container=container, coordinates="A01", individual=create_individual("Bobino"))
         self.source_sample_1.library = library_1
+        self.source_sample_1.save()
                     
         self.source_sample_2, _, _ = \
             create_full_sample(name="SOURCESAMPLE2POOL1", alias="SOURCESAMPLE2POOL1", volume=self.source_sample_initial_volume, concentration=25,
                                collection_site='PoolingTestSite', creation_date=datetime.datetime(2022, 8, 12, 0, 0), sample_kind=self.DNA_sample_kind,
                                container=container, coordinates="A02", individual=create_individual("Bobinette"))
         self.source_sample_2.library = library_2
+        self.source_sample_2.save()
 
         self.source_sample_3, _, _ = \
             create_full_sample(name="SOURCESAMPLE3POOL1", alias="SOURCESAMPLE3POOL1", volume=self.source_sample_initial_volume, concentration=25,
                                collection_site='PoolingTestSite', creation_date=datetime.datetime(2022, 8, 12, 0, 0), sample_kind=self.DNA_sample_kind,
                                container=container, coordinates="A03", individual=create_individual("Bobinouche"))
         self.source_sample_3.library = library_3
+        self.source_sample_3.save()
 
         self.source_sample_4, _, _ = \
             create_full_sample(name="SOURCESAMPLE1POOL2", alias="SOURCESAMPLE1POOL2", volume=self.source_sample_initial_volume, concentration=25,
                                collection_site='PoolingTestSite', creation_date=datetime.datetime(2022, 8, 12, 0, 0), sample_kind=self.RNA_sample_kind,
                                container=container, coordinates="A04", individual=create_individual("Bobinouille"))
+
         self.source_sample_5, _, _ = \
             create_full_sample(name="SOURCESAMPLE2POOL2", alias="SOURCESAMPLE2POOL2", volume=self.source_sample_initial_volume, concentration=25,
                                collection_site='PoolingTestSite', creation_date=datetime.datetime(2022, 8, 12, 0, 0), sample_kind=self.RNA_sample_kind,
@@ -88,5 +93,37 @@ class LibraryPreparationTestCase(TestCase):
         result = load_template(importer=self.importer, file=self.file)
         self.assertEqual(result['valid'], True)
 
+        self.assertEqual(Process.objects.filter(protocol=self.protocol_pooling).count(), 2)
+        self.assertEqual(ProcessMeasurement.objects.filter(process__protocol=self.protocol_pooling).count(), 5)
+
+        pm1 = ProcessMeasurement.objects.get(source_sample=self.source_sample_1, process__protocol=self.protocol_pooling)
+        self.assertEqual(pm1.volume_used, 200)
+        self.assertEqual(pm1.execution_date, datetime.strptime("2022-05-10", "%Y-%m-%d").date())
+        self.assertEqual(self.source_sample_1.volume, self.source_sample_initial_volume-200)
+        self.assertTrue(self.source_sample_1.depleted)
+
+        pm2 = ProcessMeasurement.objects.get(source_sample=self.source_sample_2, process__protocol=self.protocol_pooling)
+        self.assertEqual(pm2.volume_used, 50)
+        self.assertEqual(pm2.execution_date, datetime.strptime("2022-05-10", "%Y-%m-%d").date())
+        self.assertEqual(self.source_sample_2.volume, self.source_sample_initial_volume-50)
+        self.assertFalse(self.source_sample_2.depleted)
+
+        pm3 = ProcessMeasurement.objects.get(source_sample=self.source_sample_3, process__protocol=self.protocol_pooling)
+        self.assertEqual(pm3.volume_used, 25)
+        self.assertEqual(pm3.execution_date, datetime.strptime("2022-05-10", "%Y-%m-%d").date())
+        self.assertEqual(self.source_sample_3.volume, self.source_sample_initial_volume-25)
+        self.assertFalse(self.source_sample_3.depleted)
+
+        pm4 = ProcessMeasurement.objects.get(source_sample=self.source_sample_4, process__protocol=self.protocol_pooling)
+        self.assertEqual(pm4.volume_used, 100)
+        self.assertEqual(pm4.execution_date, datetime.strptime("2022-05-12", "%Y-%m-%d").date())
+        self.assertEqual(self.source_sample_4.volume, self.source_sample_initial_volume-100)
+        self.assertFalse(self.source_sample_4.depleted)
+
+        pm5 = ProcessMeasurement.objects.get(source_sample=self.source_sample_5, process__protocol=self.protocol_pooling)
+        self.assertEqual(pm5.volume_used, 100)
+        self.assertEqual(pm5.execution_date, datetime.strptime("2022-05-12", "%Y-%m-%d").date())
+        self.assertEqual(self.source_sample_5.volume, self.source_sample_initial_volume-100)
+        self.assertFalse(self.source_sample_5.depleted)
         
 
