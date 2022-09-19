1a18940383b5e50250c5178abf20f7d26992d6b2
UlysseFG <ulysse.fortiergauthier@mcgill.ca>
2022-09-19 08:15:46 -0400

    Migrate projects to FK on derived sample.

--- backend/fms_core/models/derived_sample.py
+++ backend/fms_core/models/derived_sample.py
@@ -27,7 +27,8 @@ class DerivedSample(TrackedModel):
                                       help_text="Can only be specified if the sample kind is DNA or RNA (i.e. is an extracted sample kind).")
     library = models.OneToOneField(Library, null=True, blank=True, on_delete=models.PROTECT, related_name="derived_sample",
                                    help_text="Library associated to this Derived Sample.")
-    projects= models.ManyToManyField("Project", blank=True, through="DerivedSampleByProject", related_name="derived_samples")
+    project= models.ForeignKey("Project", null=True, blank=True, on_delete=models.PROTECT, related_name="derived_samples",
+                               help_text="Project linked to the derived sample.")
 
 
     @property
