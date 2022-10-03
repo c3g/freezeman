db2fab3b1936384678789b284150f073cf501d3b
Chris Kostiw <christopher.kostiw@mcgill.ca>
2022-10-03 16:28:28 -0400

    Replace serializers with flat structure

--- backend/fms_core/viewsets/sample_pooled.py
+++ backend/fms_core/viewsets/sample_pooled.py
@@ -1,76 +1,66 @@
 from rest_framework import viewsets, serializers
-from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.exceptions import APIException
-from fms_core.models import Biosample, DerivedSample, DerivedBySample, Index, Library, LibraryType, Project, Sample, SampleKind
+from fms_core.models import DerivedBySample, Sample
 from fms_core.filters import PooledSampleFilter
 
-# Custom serializers
-# These serializers serialize the data we want to receive in pool samples.
-
-EXCLUDE_BOOKKEEPING = ['created_at', 'updated_at', 'created_by', 'updated_by', 'deleted']
-
-class IndexSerializer(serializers.ModelSerializer):
-        index_set = serializers.CharField(read_only=True, source="index_set.name")
-        index_structure = serializers.CharField(read_only=True, source="index_structure.name")
-        class Meta:
-            model = Index
-            exclude = EXCLUDE_BOOKKEEPING
-
-class SampleKindSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SampleKind
-        fields = ['name', 'is_extracted']
-
-class BiosampleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Biosample
-        exclude = EXCLUDE_BOOKKEEPING
-
-class LibrarySerializer(serializers.ModelSerializer):
-    library_type = serializers.CharField(read_only=True, source='library_type.name')
-    platform = serializers.CharField(read_only=True, source='platform.name')
-    index = IndexSerializer(read_only=True, required=False)
-    class Meta:
-        model = Library
-        exclude = EXCLUDE_BOOKKEEPING
-        depth = 1
-
-class ProjectSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Project
-        fields = ['name', 'id']
-
-class DerivedSampleSerializer(serializers.ModelSerializer):
-    library = LibrarySerializer(required=False)
-    biosample = BiosampleSerializer(required=False)
-    tissue_source = SampleKindSerializer(required=False)
-    sample_kind=SampleKindSerializer(required=False)
-    project = ProjectSerializer(required=False)
-    
-    class Meta:
-        model = DerivedSample
-        exclude = EXCLUDE_BOOKKEEPING
-        depth = 1
         
-
 class PooledSampleSerializer(serializers.Serializer):
     ''' Serializes a DerivedBySample object, representing a pooled sample. 
         The result is a nested data structure containing all of the information about the
         pooled sample needed by the frontend for display.
     '''
-    volume_ratio = serializers.DecimalField(max_digits=20, decimal_places=3, read_only=True)
-    derived_sample = DerivedSampleSerializer()
     # Since DerivedBySample doesn't have its own id field, we use the derived_sample id
     # as a top level id in the returned data structure. The UX needs this for 'objectsById' stuff.
     id = serializers.IntegerField(read_only = True, source='derived_sample.id')
-    parent_sample_name=serializers.SerializerMethodField()
-    parent_sample_id = serializers.SerializerMethodField()
+
+    volume_ratio = serializers.DecimalField(max_digits=20, decimal_places=3, read_only=True)
+
+    # Associated project info
+    project_id = serializers.IntegerField(read_only=True, source='derived_sample.project.id')
+    project_name = serializers.CharField(read_only=True, source='derived_sample.project.name')
+    
+    # Sample info
+    alias = serializers.CharField(read_only=True, source='derived_sample.biosample.alias')
+    collection_site = serializers.CharField(read_only=True, source='derived_sample.biosample.collection_site')
+    experimental_group = serializers.JSONField(read_only=True, source='derived_sample.experimental_group')
+    individual_id = serializers.CharField(read_only=True, source='derived_sample.biosample.individual.id')
+    individual_name = serializers.CharField(read_only=True, source='derived_sample.biosample.individual_name')
+    parent_sample_id = serializers.SerializerMethodField(read_only=True)
+    parent_sample_name = serializers.SerializerMethodField(read_only=True)
+    sample_kind = serializers.CharField(read_only=True, source='derived_sample.sample_kind.name')
+
+    # Library info
+    index = serializers.CharField(read_only=True, source='derived_sample.library.index.name')
+    index_id = serializers.CharField(read_only=True, source='derived_sample.library.index.id')
+    index_set = serializers.CharField(read_only=True, source='derived_sample.library.index.index_set.name')
+    library_size = serializers.DecimalField(read_only=True, max_digits=20, decimal_places=0, source='derived_sample.library.size')
+    library_type = serializers.CharField(read_only=True, source='derived_sample.library.library_type')
+    platform = serializers.CharField(read_only=True, source='derived_sample.library.platform')
+    strandedness = serializers.CharField(read_only=True, source='derived_sample.library.strandedness')
+
     class Meta:
         model = DerivedBySample
-        exclude = EXCLUDE_BOOKKEEPING + ['sample']
+        fields = [
+            'alias', 
+            'collection_site',
+            'experimental_group',
+            'id', 
+            'index_id',
+            'index_set',
+            'index',
+            'individual_id',
+            'individual_name',
+            'library_size',
+            'library_type', 
+            'parent_sample_id', 
+            'parent_sample_name', 
+            'platform',
+            'project_id', 
+            'project_name', 
+            'sample_kind'
+            'strandedness',
+            'volume_ratio', 
+            ]
 
     # Finds the id of the parent sample from which this pooled sample was derived. For example, if this
     # pool member is from a library then it returns the id of the library sample. 
--- frontend/src/modules/pooledSamples/reducers.js
+++ frontend/src/modules/pooledSamples/reducers.js
@@ -1,11 +1,9 @@
 import {merge, set} from "object-path-immutable"
-import {map} from "rambda"
 
 import {indexByID} from "../../utils/objects"
 import mergeArray from "../../utils/mergeArray"
 
-import POOLED_SAMPLES from "./actions";
-import { emailRules } from "../../constants";
+import POOLED_SAMPLES from "./actions"
 
 
 const EMPTY_STATE = {
@@ -32,7 +30,7 @@ export const pooledSamples = (
                 fixedFilters: set(state.fixedFilters, [action.data.name, 'value'], action.data.value)
             }
         case POOLED_SAMPLES.SET_SORT_BY:
-            return { ...state, sortBy: action.data, items: [], totalCount: 0 };
+            return { ...state, sortBy: action.data, items: [], totalCount: 0 }
         case POOLED_SAMPLES.SET_FILTER:
             return {
                 ...state,
@@ -40,7 +38,7 @@ export const pooledSamples = (
                 items: [],
                 totalCount: 0,
                 page: set(state.page, ['offset'], 0),
-            };
+            }
         case POOLED_SAMPLES.SET_FILTER_OPTION:
             return {
                 ...state,
@@ -63,17 +61,17 @@ export const pooledSamples = (
             }
 
         case POOLED_SAMPLES.LIST_TABLE.REQUEST:
-            return { ...state, isFetching: true, };
+            return { ...state, isFetching: true, }
         case POOLED_SAMPLES.LIST_TABLE.RECEIVE: {
-            const totalCount = action.data.count;
+            const totalCount = action.data.count
             //If filter was changed we get a new list with a different count
-            const hasChanged = state.totalCount !== action.data.count;
-            const currentItems = hasChanged ? [] : state.items;
+            const hasChanged = state.totalCount !== action.data.count
+            const currentItems = hasChanged ? [] : state.items
             const results = action.data.results.map(preprocess)
             //New filtered items
             const newItems = action.data.results.map(r => r.id)
             const items = mergeArray(currentItems, action.meta.offset, newItems)
-            const itemsByID = merge(state.itemsByID, [], indexByID(results));
+            const itemsByID = merge(state.itemsByID, [], indexByID(results))
             return {
               ...state,
               itemsByID,
@@ -81,7 +79,7 @@ export const pooledSamples = (
               totalCount,
               isFetching: false,
               error: undefined
-            };
+            }
         }
         case POOLED_SAMPLES.LIST_TABLE.ERROR:
             return { ...state, isFetching: false, error: action.error, }
@@ -90,88 +88,12 @@ export const pooledSamples = (
             return EMPTY_STATE
 
         default:
-            return state;
+            return state
     }
 }
 
 function preprocess(pooledSample) {
     pooledSample.isFetching = false
     pooledSample.isLoaded = true
-
-    // Flatten the data structure for the UX
-   return flattenSample(pooledSample)
 }
 
-/**
- * Convert the nested data structure returned by the pool sample api to a flat
- * list of properties for the UX (column/filter mapping requires a flat structure)
- * @param {*} pooledSample 
- * @returns 
- */
-function flattenSample(pooledSample) {
-    return {
-        id: pooledSample.id,                                        // The id of the pooled derived sample
-        parent_sample_name: pooledSample.parent_sample_name,        // The name of the sample that was added to this pool
-        parent_sample_id: pooledSample.parent_sample_id,            // The id of the sample that was added to this pool
-        biosample_id: pooledSample.biosample?.id,                   // The biosample attached to the sample that was added to this pool
-        alias: pooledSample.derived_sample.biosample?.alias,
-        project_name: pooledSample.derived_sample.project?.name,   
-        project_id: pooledSample.derived_sample.project?.id,
-        volume_ratio: pooledSample.volume_ratio,
-
-        // library fields
-        library_type: pooledSample.derived_sample.library?.library_type,
-        library_size: pooledSample.derived_sample.library?.library_size,
-        strandedness: pooledSample.derived_sample.library?.strandedness,
-        platform: pooledSample.derived_sample.library?.platform,
-        index_set: pooledSample.derived_sample.library?.index?.index_set,
-        index: pooledSample.derived_sample.library?.index?.name,
-        index_id: pooledSample.derived_sample.library?.index?.id,
-    }
-}
-
-/* Example of sample data returned.
- {
-    "volume_ratio": "0.500",
-    "derived_sample": {
-        "id": 392696,
-        "library": {
-            "id": 6,
-            "library_type": "PCR-free",
-            "platform": "ILLUMINA",
-            "index": {
-                "id": 13133,
-                "index_set": "IDT_10nt_UDI_TruSeq_Adapter",
-                "index_structure": "TruSeqHT",
-                "name": "IDT_10nt_UDI_i7_002-IDT_10nt_UDI_i5_002",
-                "sequences_3prime": [
-                    7094
-                ],
-                "sequences_5prime": [
-                    7093
-                ]
-            },
-            "library_size": "100",
-            "strandedness": "Double stranded"
-        },
-        "biosample": {
-            "id": 296342,
-            "alias": null,
-            "collection_site": "MUHC",
-            "individual": 189385
-        },
-        "tissue_source": {
-            "name": "BLOOD",
-            "is_extracted": false
-        },
-        "sample_kind": {
-            "name": "DNA",
-            "is_extracted": true
-        },
-        "experimental_group": []
-    },
-    "id": 392696,
-    "sample_name": "CK-SAMPLE-2",
-    "sample_id": 727302
-}
-*/
