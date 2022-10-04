82f81a62346bdece6f094cf1a4a0a0efc1e878e6
UlysseFG <ulysse.fortiergauthier@mcgill.ca>
2022-10-04 10:58:54 -0400

    Removed unused constant.

--- backend/fms_core/models/sample.py
+++ backend/fms_core/models/sample.py
@@ -22,7 +22,7 @@ from .derived_sample import DerivedSample
 from .derived_by_sample import DerivedBySample
 from .biosample import Biosample
 
-from ._constants import STANDARD_NAME_FIELD_LENGTH, POOL_KIND_NAME
+from ._constants import STANDARD_NAME_FIELD_LENGTH
 from ._utils import add_error as _add_error
 from ._validators import name_validator
 
