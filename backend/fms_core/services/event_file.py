a6f119dbb2b9b0e3b1092af3401c868842f1818e
Chris Kostiw <christopher.kostiw@mcgill.ca>
2022-11-17 16:36:47 -0500

    library, individual

--- backend/fms_core/services/event_file.py
+++ backend/fms_core/services/event_file.py
@@ -1,18 +1,16 @@
-from decimal import Decimal
 from typing import cast, List, TextIO, Union
 from dataclasses import asdict, dataclass
 from io import StringIO
 import json
 from fms_core.models import (
     Biosample, 
-    Container, 
     DerivedSample, 
     DerivedBySample,
     ExperimentRun, 
+    Index,
+    Individual,
     Instrument, 
-    InstrumentType, 
-    Process, 
-    ProcessMeasurement, 
+    Library,
     Project,
     Sample, 
 )
@@ -37,6 +35,21 @@ class EventSample:
 
     fms_pool_volume_ratio: Union[float, None] = None
 
+    # individual fields
+    fms_expected_sex: Union[str, None] = None
+    ncbi_taxon_id: Union[int, None] = None
+    fms_taxon_name: Union[str, None] = None
+    
+    # library fields
+    fms_platform_name: Union[str, None] = None
+    fms_library_type: Union[str, None] = None
+    fms_library_size: Union[float, None] = None
+    fms_index_id: FMS_Id = None
+    fms_index_name: Union[str, None] = None
+    fms_index_sequence_3_prime: Union[List[str], None] = None
+    fms_index_sequence_5_prime: Union[List[str], None] = None
+    
+
 @dataclass
 class EventManifest:
     # Manifest version (1.0.0 to start) 
@@ -45,6 +58,7 @@ class EventManifest:
     # Experiment run name and id
     run_name: str
     fms_run_id: int
+    fms_run_start_date: Union[str, None]
 
     # Flowcell / container for experiment
     fms_container_id: int
@@ -73,14 +87,19 @@ def _generate_event_manifest(experiment_run: ExperimentRun) -> EventManifest:
 
     instrument: Instrument = experiment_run.instrument
 
+    start_date = None
+    if experiment_run.start_date is not None:
+        start_date = experiment_run.start_date.strftime("%Y-%m-%d")
 
     manifest : EventManifest = EventManifest(
         version=EVENT_FILE_VERSION,
         run_name=experiment_run.name,
         fms_run_id=experiment_run.id,
+        fms_run_start_date= start_date,
         fms_container_id=experiment_run.container.id,
         fms_container_barcode=experiment_run.container.barcode,
-        instrument_id=instrument.id,
+        # TODO Merge master and then use instrument.serial_id for instrument_id
+        instrument_id='TODO: serial_id',
         fms_instrument_type=instrument.type.type,
         samples=[]
     )
@@ -139,6 +158,30 @@ def _generate_sample(experiment_run: ExperimentRun, sample: Sample, derived_samp
     row.hercules_project_name = project.external_name
 
     row.fms_container_coordinates = sample.coordinates
+
+    # INDIVIDUAL
+    individual: Individual = biosample.individual
+    if individual is not None:
+        row.fms_expected_sex = individual.sex
+
+        if individual.taxon is not None:
+            row.ncbi_taxon_id = individual.taxon.ncbi_id
+            row.fms_taxon_name = individual.taxon.name
+
+    # LIBRARY
+    library: Library = derived_sample.library
+    if library is not None:
+        index: Index = library.index
+
+        row.fms_platform_name = library.platform.name
+        row.fms_library_type = library.library_type.name
+        row.fms_library_size = float(library.library_size) if library.library_size is not None else None 
+
+        row.fms_index_id = index.id
+        row.fms_index_name = index.name
+
+        row.fms_index_sequence_3_prime = index.list_3prime_sequences
+        row.fms_index_sequence_5_prime = index.list_5prime_sequences
 
     return row
 
