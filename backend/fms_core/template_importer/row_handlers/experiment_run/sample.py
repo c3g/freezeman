fa0d6c9051478fa0e00a8daf158d000798e8a170
ufgauthi <ulysse.fortiergauthier@mcgill.ca>
2022-12-01 15:14:50 -0500

    Prevent crash when sample coordinates are wrong.

--- backend/fms_core/template_importer/row_handlers/experiment_run/sample.py
+++ backend/fms_core/template_importer/row_handlers/experiment_run/sample.py
@@ -12,18 +12,19 @@ class SampleRowHandler(GenericRowHandler):
         # Calling the service creator for Samples in ExperimentRun
         sample, self.errors['container'], self.warnings['container'] = get_sample_from_container(barcode=barcode, coordinates=coordinates)
 
-        self.row_object = sample
+        if sample is not None:
+            self.row_object = sample
 
-        for derived_sample in sample.derived_samples.all():
-            if derived_sample.library is not None and platform != derived_sample.library.platform:
-                self.errors["platform"].append(f"Library {derived_sample.biosample.alias} design ({derived_sample.library.platform.name}) "
-                                             f"does not match the experiment platform ({platform.name}).")
+            for derived_sample in sample.derived_samples.all():
+                if derived_sample.library is not None and platform != derived_sample.library.platform:
+                    self.errors["platform"].append(f"Library {derived_sample.biosample.alias} design ({derived_sample.library.platform.name}) "
+                                                f"does not match the experiment platform ({platform.name}).")
 
-        # Add a warning if the sample has failed qc
-        if any([sample.quality_flag is False, sample.quantity_flag is False]):
-            self.warnings["qc_flags"] = (f"Sample {sample.name} has failed QC.")
+            # Add a warning if the sample has failed qc
+            if any([sample.quality_flag is False, sample.quantity_flag is False]):
+                self.warnings["qc_flags"] = (f"Sample {sample.name} has failed QC.")
 
-        if not volume_used:
-            self.errors['volume_used'] = f"Volume used must be entered"
-        elif sample and volume_used > sample.volume:
-            self.errors['volume_used'] = f"Volume used ({volume_used}) exceeds the current volume of the sample ({sample.volume})"
+            if not volume_used:
+                self.errors['volume_used'] = f"Volume used must be entered"
+            elif sample and volume_used > sample.volume:
+                self.errors['volume_used'] = f"Volume used ({volume_used}) exceeds the current volume of the sample ({sample.volume})"
