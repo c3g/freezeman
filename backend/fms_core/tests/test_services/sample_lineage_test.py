73b218d309f06e2c2a687e8f48a9acb4054c509f
UlysseFG <ulysse.fortiergauthier@mcgill.ca>
2023-05-02 16:59:18 -0400

    Test for get_library_size_for_derived_sample.

--- backend/fms_core/tests/test_services/sample_lineage_test.py
+++ backend/fms_core/tests/test_services/sample_lineage_test.py
@@ -1,10 +1,16 @@
 from django.test import TestCase
+from pathlib import Path
 
 from fms_core.models import Container, SampleKind, ProcessMeasurement, Protocol, Process
 
-from fms_core.services.sample_lineage import create_sample_lineage
-from fms_core.services.sample import create_full_sample
-
+from fms_core.services.sample_lineage import create_sample_lineage, get_library_size_for_derived_sample
+from fms_core.services.sample import create_full_sample, get_sample_from_container
+from fms_core.services.project import create_project
+from fms_core.tests.test_template_importers._utils import load_template
+from fms_core.template_importer.importers import (SampleSubmissionImporter,
+                                                  LibraryPreparationImporter,
+                                                  IndexCreationImporter,
+                                                  LibraryQCImporter)
 
 class SampleLineageServicesTestCase(TestCase):
     def setUp(self) -> None:
@@ -43,6 +49,13 @@ class SampleLineageServicesTestCase(TestCase):
         self.process_measurement = ProcessMeasurement.objects.create(source_sample=self.full_sample_parent, process=self.process,
                                                                      execution_date="2022-01-01", volume_used=5)
 
+    def import_template(self, importer, file):
+        TEMPLATES_DIR = Path(__file__).parent.parent / "service-templates"
+        template_path = TEMPLATES_DIR / file
+        result = load_template(importer=importer, file=template_path)
+        if result['valid'] == False:
+            raise Exception(f'Failed to import template {file}')
+
     def test_create_sample_lineage(self):
         lineage, errors, warnings = create_sample_lineage(self.full_sample_parent, self.full_sample_child, self.process_measurement)
         self.assertEqual(errors, [])
@@ -76,4 +89,17 @@ class SampleLineageServicesTestCase(TestCase):
                                   f"Process measurement is required for sample lineage creation."])
         self.assertEqual(warnings, [])
 
+    def test_get_library_size_for_derived_sample(self):
+        
+        PROJECT_NAME = 'TEST_PROJECT'
+        self.project, _, _ = create_project(name=PROJECT_NAME)
 
+        self.import_template(IndexCreationImporter(), 'Index_creation_v3_7_0.xlsx')
+        self.import_template(SampleSubmissionImporter(), 'Sample_submission_v4_2_0.xlsx')
+        self.import_template(LibraryPreparationImporter(), 'Library_preparation_v4_1_0.xlsx')
+        self.import_template(LibraryQCImporter(), 'Library_QC_v4_1_0.xlsx')
+
+        sample_obj, _, _ = get_sample_from_container(barcode="ER_Container_BC_01",coordinates="B01")
+        library_size = get_library_size_for_derived_sample(sample_obj.derived_sample_not_pool.id)
+
+        self.assertEqual(library_size, 100)
