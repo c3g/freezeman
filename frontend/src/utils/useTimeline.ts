ee4807e50e4372db3256e3cb86c43c4d7b217166
nafiz1001 <nafiz.islam1001@gmail.com>
2023-05-18 18:17:42 +0000

    fix useTimeline

--- frontend/src/utils/useTimeline.ts
+++ frontend/src/utils/useTimeline.ts
@@ -4,17 +4,17 @@ const OFFSET = 350
 
 export default function useTimeline() {
 
-  const [timelineWidth, setTimelineWidth] = useState(0);
+  const [timelineWidth, setTimelineWidth] = useState<number>(0);
   const timelineMarginLeft = -timelineWidth + OFFSET;
-  const timelineRef = useRef(null);
+  const timelineRef = useRef<HTMLElement | null>(null);
 
   useEffect(() => {
     if (timelineRef.current)
       setTimelineWidth(timelineRef.current.clientWidth || window.innerWidth - OFFSET)
-  });
+  }, []);
   useEffect(() => {
     const resizeListener = () => {
-      setTimelineWidth(timelineRef.current.clientWidth)
+      setTimelineWidth((timelineWidth) => timelineRef.current?.clientWidth ?? timelineWidth)
     };
     window.addEventListener('resize', resizeListener);
     return () => window.removeEventListener('resize', resizeListener);
