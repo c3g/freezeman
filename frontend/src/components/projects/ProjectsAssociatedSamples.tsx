8ea0a9dc0f47413c5d99261c90d241cf66671365
nafiz1001 <nafiz.islam1001@gmail.com>
2023-06-27 18:31:13 +0000

    wip ProjectsAssociatedSamples

--- frontend/src/components/projects/ProjectsAssociatedSamples.tsx
+++ frontend/src/components/projects/ProjectsAssociatedSamples.tsx
@@ -1,107 +1,73 @@
-import { Tag } from "antd";
-import React, { useCallback } from "react";
-import { connect } from "react-redux";
-import { Link } from "react-router-dom";
-import FilteredList from "../FilteredList";
-import { listFilter } from "../../modules/samples/actions";
-import { Depletion } from "../Depletion";
-import { SAMPLE_FILTERS } from "../filters/descriptions";
-import { WithIndividualRenderComponent } from "../shared/WithItemRenderComponent";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../../hooks";
-import { selectSampleKindsState, selectSamplesByID } from "../../selectors";
+import { selectSamplesByID, selectSamplesTable } from "../../selectors";
+import SamplesTableActions from '../../modules/samplesTable/actions'
+import { DataID, FilterSetting, createFixedFilter } from "../../models/paged_items";
+import { FILTER_TYPE } from "../../constants";
+import { ObjectWithSample, SAMPLE_COLUMN_DEFINITIONS, SAMPLE_COLUMN_FILTERS, SAMPLE_NEXT_STEP_FILTER_KEYS } from "../shared/WorkflowSamplesTable/SampleTableColumns";
+import PagedItemsTable, { useFilteredColumns } from "../pagedItemsTable/PagedItemsTable"
 
-const getTableColumns = (sampleKinds) => [
-    {
-      title: "Sample Kind",
-      dataIndex: "derived_samples__sample_kind__name",
-      sorter: true,
-      width: 70,
-      options: sampleKinds.items.map(x => ({ label: x.name, value: x.name })), // for getFilterProps
-      render: (_, sample) =>
-        {return <Tag>{sample.sample_kind ? sampleKinds.itemsByID[sample.sample_kind].name : "POOL"}</Tag>}
-    },
-    {
-      title: "Name",
-      dataIndex: "name",
-      sorter: true,
-      width: 170,
-      render: (name, sample) =>
-        <Link to={`/samples/${sample.id}`}>
-          <div>{name}</div>
-          {sample.alias &&
-            <div><small>alias: {sample.alias}</small></div>
-          }
-        </Link>,
-    },
-    {
-      title: "Cohort",
-      dataIndex: "derived_samples__biosample__individual__cohort",
-      sorter: true,
-      width: 130,
-      render: (_, sample) => {
-        const individual = sample.individual
-        return (individual &&
-          <Link to={`/individuals/${individual}`}>
-            <WithIndividualRenderComponent objectID={individual} render={individual => <>{individual.cohort}</>} placeholder={"Loading..."}/>
-          </Link>)
-      }
-    },
-    {
-      title: "Vol. (µL)",
-      dataIndex: "volume",
-      sorter: true,
-      align: "right",
-      className: "table-column-numbers",
-      width: 70,
-    },
-    {
-      title: "Creation Date",
-      dataIndex: "creation_date",
-      sorter: true,
-      width: 115,
-    },
-    {
-      title: "Depleted",
-      dataIndex: "depleted",
-      sorter: true,
-      render: depleted => <Depletion depleted={depleted} />,
-      width: 50,
-    }
-  ];
+const sampleColumns = [
+  SAMPLE_COLUMN_DEFINITIONS.KIND,
+  SAMPLE_COLUMN_DEFINITIONS.NAME,
+  SAMPLE_COLUMN_DEFINITIONS.COHORT,
+  SAMPLE_COLUMN_DEFINITIONS.VOLUME,
+  SAMPLE_COLUMN_DEFINITIONS.CREATION_DATE,
+  SAMPLE_COLUMN_DEFINITIONS.DEPLETED,
+]
 
-const ProjectsAssociatedSamples = ({projectID}) => {
-
-  const filterKey = SAMPLE_FILTERS.derived_samples__project__id.key
-
-  const sampleKinds = useAppSelector(selectSampleKindsState)
-  const page = useAppSelector((state) => state.samples.page)
-  const samplesByID = useAppSelector(selectSamplesByID)
-  const samples = useAppSelector((state) => state.samples.filteredItems)
-  const totalCount = useAppSelector((state) => state.samples.filteredItemsCount)
-  const isFetching = useAppSelector((state) => state.samples.isFetching)
-  
+const ProjectsAssociatedSamples = ({
+  projectID
+}) => {
   const dispatch = useAppDispatch()
-  const listFilterCallback = useCallback((props: any) => {
-    return dispatch(listFilter(props))
+  const { setFilter, setFilterOptions } = SamplesTableActions
+  const projectIDFilter = useMemo(() => {
+    const filterKey = 'derived_samples__project__id'
+    const filter: FilterSetting = createFixedFilter(FILTER_TYPE.INPUT_OBJECT_ID, filterKey, projectID)
+    return filter
+  }, [projectID])
+
+  const samplesTable = useAppSelector(selectSamplesTable)
+  const samplesByID = useAppSelector(selectSamplesByID)
+
+  const columns = useFilteredColumns<ObjectWithSample>(
+    sampleColumns, 
+    SAMPLE_COLUMN_FILTERS, 
+    SAMPLE_NEXT_STEP_FILTER_KEYS, 
+    samplesTable.filters,
+    setFilter,
+    setFilterOptions)
+  
+  const mapSamplesByID = async (ids: DataID[]) => {
+    return ids.reduce((acc, id) => {
+      const sample = samplesByID[id]
+      if (sample) {
+        acc.push({id, sample})
+      }
+      return acc
+    }, [] as ObjectWithSample[])
+  }
+
+  const requestPageCallback = useCallback((pageNumber: number) => {
+		// Create a thunk and dispatch it.
+		const requestAction = (page: number) => async (dispatch) => {
+			dispatch(SamplesTableActions.listPage(page))
+		}
+		dispatch(requestAction(pageNumber))
   }, [dispatch])
   
-  const columns = getTableColumns(sampleKinds)
-
-  return <>
-    <FilteredList
-      description={SAMPLE_FILTERS}
+  return (
+    // Don't render until the sample fixed filter is set, or you will get all of the projects.
+     <PagedItemsTable
+      requestPageCallback={requestPageCallback}
       columns={columns}
-      listFilter={listFilterCallback}
-      items={samples}
-      itemsByID={samplesByID}
-      totalCount={totalCount}
-      filterID={projectID}
-      filterKey={filterKey}
-      rowKey="id"
-      isFetching={isFetching}
-      page={page}
+      fixedFilter={projectIDFilter}
+      getDataObjectsByID={mapSamplesByID}
+      pagedItems={samplesTable}
+      pagedItemsActions={SamplesTableActions}
+      usingFilters={true}
     />
-  </>;
+  )
 }
 
 export default ProjectsAssociatedSamples;
