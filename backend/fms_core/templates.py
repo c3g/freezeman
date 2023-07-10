aa72dd7d896780ded6ff7703d745cbb49d915799
ufgauthi <ulysse.fortiergauthier@mcgill.ca>
2023-07-10 17:11:58 -0400

    Experiment Infinium

--- backend/fms_core/templates.py
+++ backend/fms_core/templates.py
@@ -88,14 +88,16 @@ EXPERIMENT_RUN_TEMPLATE_SHEET_INFO = [
 
 EXPERIMENT_INFINIUM_TEMPLATE = {
   "identity": {"description": "Template to add Infinium experiments",
-               "file": static("submission_templates/Experiment_Infinium_24_v4_1_0.xlsx"),
+               "file": static("submission_templates/Experiment_Infinium_24_v4_4_0.xlsx"),
                "protocol": "Illumina Infinium Preparation"},
   "sheets info": EXPERIMENT_RUN_TEMPLATE_SHEET_INFO,
   # prefill_info : [("Template Sheet Name", "Template Column Header", "Queryset Name", "Sample Model Attribute/Property"), ...]
   "prefill info": [
       ("Samples", "Source Sample Name", "name", "name"),
       ("Samples", "Source Container Barcode", "container__barcode", "container_barcode"),
-      ("Samples", "Source Container Coordinates", "coordinate__name", "coordinates"),],
+      ("Samples", "Source Container Coordinates", "coordinate__name", "coordinates"),
+      ("Samples", "Source Sample Current Volume (uL)", "volume", "volume"),
+  ],
 }
 
 EXPERIMENT_MGI_TEMPLATE = {
