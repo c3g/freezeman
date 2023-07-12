3e182c3737ef7fd09b20ce040f0d4e68d923ae72
nafiz1001 <nafiz.islam1001@gmail.com>
2023-07-12 16:28:29 +0000

    remove SampleSerializer

--- backend/fms_core/viewsets/sample.py
+++ backend/fms_core/viewsets/sample.py
@@ -57,7 +57,6 @@ class SampleViewSet(viewsets.ModelViewSet, TemplateActionsMixin, TemplatePrefill
             .values_list("derived_sample", flat=True)[:1]
         )
     )
-    serializer_class = SampleSerializer
 
     ordering_fields = (
         *_list_keys(_sample_filterset_fields),
