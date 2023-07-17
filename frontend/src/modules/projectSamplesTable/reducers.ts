d63881d72b29731ff991fdcb56ce0419fd657e1a
nafiz1001 <nafiz.islam1001@gmail.com>
2023-07-17 19:22:20 +0000

    SAMPLES_TABLE to PROJECT_SAMPLES_TABLE

--- frontend/src/modules/projectSamplesTable/reducers.ts
+++ frontend/src/modules/projectSamplesTable/reducers.ts
@@ -1,7 +1,7 @@
 import { createPagedItems } from '../../models/paged_items'
 import { createPagedItemsActionTypes, createPagedItemsReducer } from '../../models/paged_items_factory'
 
-export const actionTypes = createPagedItemsActionTypes('SAMPLES_TABLE')
+export const actionTypes = createPagedItemsActionTypes('PROJECT_SAMPLES_TABLE')
 const initialState = createPagedItems()
 
 export const projectSamplesTable = createPagedItemsReducer(actionTypes, initialState)
