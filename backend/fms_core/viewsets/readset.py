bd924193805aee7bccaa6ed1117f31bba72f0c08
nafiz1001 <nafiz.islam1001@gmail.com>
2023-08-02 13:37:09 -0400

    remove filter import

--- backend/fms_core/viewsets/readset.py
+++ backend/fms_core/viewsets/readset.py
@@ -3,7 +3,6 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import action
 
-from fms_core.filters import ReadsetFilter
 from fms_core.models.readset import Readset
 from fms_core.serializers import ReadsetSerializer
 
