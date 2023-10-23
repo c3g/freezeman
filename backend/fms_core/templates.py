fc55e081d26672ea113aa2c9042ed0c33585b42c
Justin Marcelino <jmarcelino@Thunbergia.local>
2023-10-23 14:06:45 -0400

    fixing template dropdowns

--- backend/fms_core/templates.py
+++ backend/fms_core/templates.py
@@ -162,8 +162,14 @@ EXPERIMENT_AXIOM_TEMPLATE = {
     "identity" : {"description": "Template to add Axiom experiments",
                   "file": static("submission_templates/Experiment_run_Axiom_v4_6_0.xlsx"),
                   "protocol": "Axiom Experiment Run"},
-    "sheets info": EXPERIMENT_RUN_TEMPLATE_SHEET_INFO + 
+    "sheets info":
     [{
+          'name': 'Samples',
+          'headers': ['Experiment Name', 'Source Sample Name', 'Source Container Barcode', 'Source Container Coordinates',
+                      'Source Sample Current Volume (uL)', 'Source Sample Volume Used (uL)', 'Experiment Container Coordinates', 'Comment', 'Workflow Action'],
+          'stitch_column': 'Experiment Name',
+          'batch': False,
+      },{
           'name': 'GeneTitanSetup',
           'headers': ['Coord', 'Array Barcode', 'Unique Sample ID','Sample Name', 'ID'],
     }],
