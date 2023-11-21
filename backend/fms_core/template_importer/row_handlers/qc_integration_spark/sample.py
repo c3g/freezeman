d7d44c0c53bbfbe3c2140b0509f3c532101a7a94
UlysseFG <ulysse.fortiergauthier@mcgill.ca>
2023-11-21 13:19:40 -0500

    validate sample is queued for step.

--- backend/fms_core/template_importer/row_handlers/qc_integration_spark/sample.py
+++ backend/fms_core/template_importer/row_handlers/qc_integration_spark/sample.py
@@ -8,7 +8,7 @@ from fms_core.services.process_measurement import create_process_measurement
 from fms_core.services.property_value import create_process_measurement_properties
 from fms_core.services.sample import update_qc_flags
 from fms_core.services.sample_next_step import execute_workflow_action
-from fms_core.models import InstrumentType, Process, Step
+from fms_core.models import InstrumentType, Process, Step, SampleNextStep
 
 INSTRUMENT_PROPERTIES = ['Quantity Instrument']
 QC_PLATFORM = "Quality Control"
@@ -32,45 +32,51 @@ class QCIntegrationSparkRowHandler(GenericRowHandler):
                                                                                                coordinates=sample['coordinates'])
 
         if sample_obj:
-            # Check if sample is not a library or a pool of libraries
-            if sample_obj.is_library:
-                self.errors['source_sample'] = f"Source sample can't be a library or a pool of libraries."
+            # Validate the sample is queued to the workflow step
+            queued_to_workflow_step = SampleNextStep.objects.filter(sample_id=sample_obj.id, step_id=workflow["step"].id).exists()
 
-            # Update the sample's flags with sample information
-            _, self.errors['flags'], self.warnings['flags'] = update_qc_flags(sample=sample_obj,
-                                                                              quantity_flag=sample_information['quantity_flag'])
+            if not queued_to_workflow_step:
+                self.errors['source_sample'].append(f"Sample is not queued to the workflow step {workflow['step'].name}.")
+            else:
+                # Check if sample is not a library or a pool of libraries
+                if sample_obj.is_library:
+                    self.errors['source_sample'] = f"Source sample can't be a library or a pool of libraries."
 
-            process_measurement_obj, self.errors['process_measurement'], self.warnings['process_measurement'] = \
-                create_process_measurement(
-                    process=process_measurement['process'],
-                    source_sample=sample_obj,
-                    execution_date=process_measurement['execution_date'],
-                    volume_used=process_measurement['volume_used'],
-                    comment=process_measurement['comment'],
-                )
+                # Update the sample's flags with sample information
+                _, self.errors['flags'], self.warnings['flags'] = update_qc_flags(sample=sample_obj,
+                                                                                  quantity_flag=sample_information['quantity_flag'])
 
-            # Create process measurement's properties
-            if process_measurement_obj:
-                properties_obj, self.errors['properties'], self.warnings['properties'] = create_process_measurement_properties(
-                    process_measurement_properties,
-                    process_measurement_obj)
+                process_measurement_obj, self.errors['process_measurement'], self.warnings['process_measurement'] = \
+                    create_process_measurement(
+                        process=process_measurement['process'],
+                        source_sample=sample_obj,
+                        execution_date=process_measurement['execution_date'],
+                        volume_used=process_measurement['volume_used'],
+                        comment=process_measurement['comment'],
+                    )
 
-                # Process the workflow action
-                self.errors['workflow'], self.warnings['workflow'] = execute_workflow_action(workflow_action=workflow["step_action"],
-                                                                                             step=workflow["step"],
-                                                                                             current_sample=sample_obj,
-                                                                                             process_measurement=process_measurement_obj)
+                # Create process measurement's properties
+                if process_measurement_obj:
+                    properties_obj, self.errors['properties'], self.warnings['properties'] = create_process_measurement_properties(
+                        process_measurement_properties,
+                        process_measurement_obj)
 
-            if process_measurement_obj and properties_obj:
-                # Validate instruments according to platform
-                for instrument in INSTRUMENT_PROPERTIES:
-                    try:
-                        instrument_type_type = process_measurement_properties[instrument]['value']
-                        instrument_type = InstrumentType.objects.get(type=instrument_type_type)
-                        # Validate platform and type
-                        if instrument_type.platform.name != QC_PLATFORM:
-                            self.errors['instrument_type'] = f'Invalid type: {instrument_type.platform} for instrument: {instrument_type.type}.'
-                    except Exception as e:
-                        self.errors['instrument'] = f'Invalid instrument {instrument_type_type}.'
+                    # Process the workflow action
+                    self.errors['workflow'], self.warnings['workflow'] = execute_workflow_action(workflow_action=workflow["step_action"],
+                                                                                                step=workflow["step"],
+                                                                                                current_sample=sample_obj,
+                                                                                                process_measurement=process_measurement_obj)
+
+                if process_measurement_obj and properties_obj:
+                    # Validate instruments according to platform
+                    for instrument in INSTRUMENT_PROPERTIES:
+                        try:
+                            instrument_type_type = process_measurement_properties[instrument]['value']
+                            instrument_type = InstrumentType.objects.get(type=instrument_type_type)
+                            # Validate platform and type
+                            if instrument_type.platform.name != QC_PLATFORM:
+                                self.errors['instrument_type'] = f'Invalid type: {instrument_type.platform} for instrument: {instrument_type.type}.'
+                        except Exception as e:
+                            self.errors['instrument'] = f'Invalid instrument {instrument_type_type}.'
 
 
