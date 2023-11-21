34e8aa766b84b34d6a42c537289f6b3d9cf0fd0a
UlysseFG <ulysse.fortiergauthier@mcgill.ca>
2023-11-21 17:50:22 -0500

    Addition of test and moved the validation out of row_handler to prevent error spam.

--- backend/fms_core/tests/test_template_importers/qc_integration_spark_test.py
+++ backend/fms_core/tests/test_template_importers/qc_integration_spark_test.py
@@ -6,11 +6,15 @@ from decimal import Decimal
 from fms_core.template_importer.importers import QCIntegrationSparkImporter
 from fms_core.tests.test_template_importers._utils import load_template, APP_DATA_ROOT
 
-from fms_core.models import Sample, Individual, SampleKind, ProcessMeasurement, Taxon, PropertyValue
+from fms_core.models import (Sample, Individual, SampleKind, ProcessMeasurement, Taxon, PropertyValue,
+                             Step, SampleNextStep, SampleNextStepByStudy, Workflow)
 
 from fms_core.services.container import get_or_create_container
 from fms_core.services.individual import get_or_create_individual
 from fms_core.services.sample import create_full_sample
+from fms_core.services.project import create_project
+from fms_core.services.study import create_study
+from fms_core.services.sample_next_step import queue_sample_to_study_workflow
 
 
 class QcIntegrationSparkTestCase(TestCase):
@@ -23,22 +27,32 @@ class QcIntegrationSparkTestCase(TestCase):
 
 
     def prefill_data(self):
-        sample_kind_DNA, _ = SampleKind.objects.get_or_create(name='DNA')
-        taxon = Taxon.objects.get(name='Homo sapiens')
+        sample_kind_DNA, _ = SampleKind.objects.get_or_create(name="DNA")
+        taxon = Taxon.objects.get(name="Homo sapiens")
+        workflow = Workflow.objects.get(name="Axiom Genotyping")
+        project, _, _ = create_project(name="AxiomTestProject")
+        study, _, _ = create_study(project=project,
+                                   workflow=workflow,
+                                   start=1,
+                                   end=7)
 
         samples_info = [
-            {'name': 'sample1Spark', 'volume': 100, 'container_barcode': 'TestPlate01', 'coordinates': 'A01', 'individual_name': 'Individual1Spark'},
-            {'name': 'sample2Spark', 'volume': 90, 'container_barcode': 'TestPlate01', 'coordinates': 'B01', 'individual_name': 'Individual2Spark'},
-            {'name': 'sample3Spark', 'volume': 200, 'container_barcode': 'TestPlate01', 'coordinates': 'C01', 'individual_name': 'Individual3Spark'},
+            {'name': 'sample1Spark', 'volume': 100, 'container_barcode': 'TestPlate01', 'coordinates': 'A01', 'individual_name': 'Individual1Spark', 'project': project},
+            {'name': 'sample2Spark', 'volume': 90, 'container_barcode': 'TestPlate01', 'coordinates': 'B01', 'individual_name': 'Individual2Spark', 'project': project},
+            {'name': 'sample3Spark', 'volume': 200, 'container_barcode': 'TestPlate01', 'coordinates': 'C01', 'individual_name': 'Individual3Spark', 'project': project},
         ]
 
         for info in samples_info:
             container, _, errors, warnings = get_or_create_container(barcode=info['container_barcode'], kind='96-well plate', name=info['container_barcode'])
 
             individual, _, errors, warnings = get_or_create_individual(name=info['individual_name'], taxon=taxon)
-            sample, errors, warnings = create_full_sample(name=info['name'], volume=info['volume'], collection_site='site1',
+            sample, errors, warnings = create_full_sample(name=info['name'], volume=info['volume'], collection_site='site1', project=info['project'],
                                                           creation_date=datetime.datetime(2023, 11, 7, 0, 0), container=container,
                                                           coordinates=info['coordinates'], individual=individual, sample_kind=sample_kind_DNA)
+            # Queue sample to qc integration step
+            queue_sample_to_study_workflow(sample_obj=sample,
+                                           study_obj=study,
+                                           order=5) # order 5 in Axiom Workflow is 
 
     def test_import(self):
         content_type_processmeasurement = ContentType.objects.get_for_model(ProcessMeasurement)
@@ -96,3 +110,14 @@ class QcIntegrationSparkTestCase(TestCase):
         self.assertEqual(pv3_1.value, "Failed")
         self.assertEqual(pv3_2.value, "Spark 10M")
         self.assertIsNone(pv3_3)
+
+    def test_import_samples_not_queued(self):
+        # Importing first time to move the sample to next step
+        result = load_template(importer=self.importer, file=self.file)
+        self.assertEqual(result['valid'], True)
+        # Importing again the same file should fail
+        result = load_template(importer=self.importer, file=self.file)
+        self.assertEqual(result['valid'], False)
+
+        # Check the error message
+        self.assertEqual(result["base_errors"][0]["error"], "File has likely already been submitted. Samples included are not queued to workflow step Quality Control - Integration (Spark).")
