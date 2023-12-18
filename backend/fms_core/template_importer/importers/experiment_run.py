93cfea7a887eba62fc8e4bf1a45d487c13ae6f28
ufgauthi <ulysse.fortiergauthier@mcgill.ca>
2023-12-18 11:40:24 -0500

    Cleanup

--- backend/fms_core/template_importer/importers/experiment_run.py
+++ backend/fms_core/template_importer/importers/experiment_run.py
@@ -46,10 +46,34 @@ class ExperimentRunImporter(GenericImporter):
         except Exception as e:
             self.base_errors.append(f"Property Type could not be found. {e}")
 
+    def validation_by_run_type(self, run_type, sample_input, experiment_input):
+        match run_type.name:
+            case "Axiom":
+                experiment_destination_barcode = defaultdict(set)
+                for sample_row in sample_input:
+                    experiment_destination_barcode[sample_row["sample"]["experiment_name"]].add(sample_row["barcode"])
+                for experiment_row in experiment_input:
+                    if (len(experiment_destination_barcode[experiment_row["experiment_run_name"]]) > 1):
+                        self.base_errors.append(f"Experiment {experiment_row['experiment_run_name']} receives samples from more than one container {experiment_destination_barcode[experiment_row['experiment_run_name']]}.")
+                    elif len(experiment_destination_barcode[experiment_row["experiment_run_name"]]) == 1:
+                        source_container_barcode = list(experiment_destination_barcode[experiment_row["experiment_run_name"]])[0]
+                        container = Container.objects.filter(barcode=source_container_barcode).first()
+                        comment = container.comment
+                        start = comment.find(DESTINATION_CONTAINER_BARCODE_MARKER)
+                        if start > -1:
+                            start = start + len(DESTINATION_CONTAINER_BARCODE_MARKER)
+                            end = comment.find(" ", start)
+                            destination_barcode = comment[start:end]
+                            if destination_barcode != experiment_row["container"]["barcode"]:
+                                self.base_errors.append(f"Experiment Name {experiment_row['experiment_run_name']} : Source container {container.name} was set to be transfered to Experiment Container Barcode {destination_barcode}.")
+                    
+
     def import_template_inner(self):
         samples_sheet = self.sheets['Samples']
         experiments_sheet = self.sheets['Experiments']
         experiments_df = experiments_sheet.dataframe
+        sample_input = []
+        experiment_input = []
 
         # PRELOADING - Set values for global data
         runtype_name = experiments_df.values[RUN_TYPE_INDEX][1]
@@ -64,7 +88,6 @@ class ExperimentRunImporter(GenericImporter):
         """
             SAMPLES SHEET
         """
-        experiment_destination_barcode = defaultdict(set)
         sample_rows_data = defaultdict(list)
         for i, row_data in enumerate(samples_sheet.rows):
             sample = {'experiment_name': str_cast_and_normalize(row_data['Experiment Name']),
@@ -93,11 +116,8 @@ class ExperimentRunImporter(GenericImporter):
             # Set the actual volumed_used in case the load all option was used
             sample["volume_used"] = sample['sample_obj'].volume if sample["volume_used"] == LOAD_ALL else sample["volume_used"]
             sample_rows_data[sample['experiment_name']].append(sample)
-
-            # Build a validation object for Axiom Experiment to verify the Array Barcode matches what was declared when creating folders.
-            # map source container to experiment name
-            if self.preloaded_data['protocol'].name == "Axiom Experiment Preparation":
-                experiment_destination_barcode[sample["experiment_name"]].add(sample_kwargs["barcode"])
+            sample_kwargs["sample"] = sample
+            sample_input.append(sample_kwargs)
 
         """
             EXPERIMENTS SHEET
@@ -129,26 +149,13 @@ class ExperimentRunImporter(GenericImporter):
                 imported_template=self.imported_file
             )
 
-            # Second part of Axiom validation get destination barcode from the source barcode.
-            # Each experiment should have a single container barcode attached
-            if (len(experiment_destination_barcode[experiment_run_kwargs["experiment_run_name"]]) > 1):
-                self.base_errors.append(f"Experiment {experiment_run_kwargs['experiment_run_name']} receives samples from more than one container {experiment_destination_barcode[experiment_run_kwargs['experiment_run_name']]}.")
-            elif len(experiment_destination_barcode[experiment_run_kwargs["experiment_run_name"]]) == 1:
-                source_container_barcode = list(experiment_destination_barcode[experiment_run_kwargs["experiment_run_name"]])[0]
-                container = Container.objects.filter(barcode=source_container_barcode).first()
-                comment = container.comment
-                start = comment.find(DESTINATION_CONTAINER_BARCODE_MARKER)
-                if start > -1:
-                    start = start + len(DESTINATION_CONTAINER_BARCODE_MARKER)
-                    end = comment.find(" ", start)
-                    destination_barcode = comment[start:end]
-                    if destination_barcode != experiment_run_kwargs["container"]["barcode"]:
-                        self.base_errors.append(f"Source container {container.name} was set to be transfered to Axiom Array {destination_barcode}.")
-                    
-
             (result, _) = self.handle_row(
                 row_handler_class=ExperimentRunRowHandler,
                 sheet=experiments_sheet,
                 row_i=row_id,
                 **experiment_run_kwargs,
             )
+
+            experiment_input.append(experiment_run_kwargs)
+      
+        self.validation_by_run_type(self.preloaded_data['run_type'], sample_input, experiment_input)
