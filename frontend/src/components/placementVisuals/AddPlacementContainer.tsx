b3aa82862d7b2c66640ab3e84a811006c34f46a7
Justin Marcelino <jmarcelino@Thunbergia.local>
2024-01-24 10:44:17 -0500

    adding tube error prevention

--- frontend/src/components/placementVisuals/AddPlacementContainer.tsx
+++ frontend/src/components/placementVisuals/AddPlacementContainer.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useState } from "react"
-import { Button, Select, Tabs, Typography } from "antd"
+import { Alert, Button, Form, Select, Tabs } from "antd"
 import { useAppDispatch, useAppSelector } from "../../hooks"
 import { selectCoordinatesByID } from "../../selectors"
 import { PLACED_STRING } from "./PlacementTab"
@@ -8,20 +8,21 @@ import Modal from "antd/lib/modal/Modal"
 import SearchContainer from "../SearchContainer"
 import Input from "antd/lib/input/Input"
 import api from "../../utils/api"
-const { Text } = Typography;
+
 interface AddPlacementContainerProps {
     addDestination: (destinationContainer) => void
 }
+
 //component used to handle the creation of a new destination container
 const AddPlacementContainer = ({ addDestination }: AddPlacementContainerProps) => {
     const [isPopup, setIsPopup] = useState<boolean>(false)
-    //used to hold new container data
-    const [newContainer, setNewContainer] = useState<any>({
-        barcode: '', samples: '', container_kind: ''
-    })
+
     //used to hold loaded container data
     const [loadedContainer, setLoadedContainer] = useState<any>({})
     const [selectedTab, setSelectedTab] = useState<string>('new')
+    const [error, setError] = useState<string | undefined>(undefined)
+    const [form] = Form.useForm()
+
     const coordinates = useAppSelector(selectCoordinatesByID)
 
     const dispatch = useAppDispatch()
@@ -35,44 +36,50 @@ const AddPlacementContainer = ({ addDestination }: AddPlacementContainerProps) =
         loadedSamples.data.results.forEach(sample => {
             newDestination[sample.id] = { id: sample.id, coordinates: (coordinates[sample.coordinate].name), type: PLACED_STRING, name: sample.name, sourceContainer: containerName }
         })
-        setLoadedContainer({ container_name: containerName, samples: copyKeyObject(newDestination) })
+        setLoadedContainer({ container_name: containerName, container_kind: container.data.kind, samples: copyKeyObject(newDestination), })
     }, [coordinates])
 
     //calls addDestination prop with 'New Destination' container
     const handleConfirm = useCallback(() => {
-        const container = selectedTab == 'load' ? loadedContainer : newContainer
-        addDestination(container)
-        setIsPopup(false)
-    }, [newContainer, loadedContainer])
-
-    //handles input change, NOTE: didn't want to use a form since there were only two fields
-    const handleInputChange = useCallback((e, inputName) => {
-        const tempContainer = newContainer
-        const value = e.target ? e.target.value : e
-        switch (inputName) {
-            case 'barcode':
-                tempContainer.container_name = value
-                break
-            case 'container_kind':
-                tempContainer.container_kind = value
-                break
-            default:
-                break;
+        const container = selectedTab == 'load' ? loadedContainer : { ...form.getFieldsValue(), samples: { 6: {id: 6}} }
+        if (container.container_name && container.container_kind && container.container_kind != 'tube') {
+            addDestination(container)
+            setIsPopup(false)
+            form.resetFields()
+        } else {
+            setError("Invalid container kind")
         }
-        setNewContainer({ ...tempContainer })
-    }, [newContainer])
+    }, [loadedContainer, selectedTab])
+
+
     return (
         <>
             <Button onClick={() => setIsPopup(true)}>Add Destination</Button>
             <Modal title="Add Destination" visible={isPopup} onOk={handleConfirm} onCancel={() => setIsPopup(false)}>
                 <div style={{ display: 'flex', flexDirection: 'column' }}>
-
+                    {error ?
+                        <Alert
+                            type='error'
+                            message='Destination Error'
+                            description={error}
+                            closable={true}
+                            showIcon={true}
+                            onClose={() => { setError(undefined) }}
+                        />
+                        : ''}
                     <Tabs defaultActiveKey={'New'} activeKey={selectedTab} onTabClick={(e) => setSelectedTab(e)}>
                         <Tabs.TabPane tab='New Container' key={'new'}>
-                            <Input onChange={(e) => handleInputChange(e, 'barcode')} placeholder="Barcode"></Input>
-                            <Select onChange={(e) => handleInputChange(e, 'container_kind')} placeholder="Container kind" style={{ width: "100%" }} options={[{ value: '96-well plate', label: '96-well plate' },]}></Select>
+                            <Form
+                                form={form}
+                                layout="horizontal">
+                                <Form.Item name={"container_name"}>
+                                    <Input placeholder="Barcode"></Input>
+                                </Form.Item>
+                                <Form.Item name={"container_kind"}>
+                                    <Select placeholder="Container kind" style={{ width: "100%" }} options={[{ value: '96-well plate', label: '96-well plate' }]}></Select>
+                                </Form.Item>
+                            </Form>
                         </Tabs.TabPane>
-                        
                         <Tabs.TabPane tab='Load Container' key={'load'}>
                             <SearchContainer handleOnChange={(value) => handleContainerLoad(value)} />
                         </Tabs.TabPane>
