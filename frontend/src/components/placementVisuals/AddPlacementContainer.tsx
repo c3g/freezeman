9e3dd312d3e0950ae7ed9f3117fa9b2ba3ca8da5
Justin Marcelino <jmarcelino@Thunbergia.local>
2024-01-24 12:20:35 -0500

    removing add destination tab, should fix deselect problem

--- frontend/src/components/placementVisuals/AddPlacementContainer.tsx
+++ frontend/src/components/placementVisuals/AddPlacementContainer.tsx
@@ -3,25 +3,23 @@ import { Alert, Button, Form, Select, Tabs } from "antd"
 import { useAppDispatch, useAppSelector } from "../../hooks"
 import { selectCoordinatesByID } from "../../selectors"
 import { PLACED_STRING } from "./PlacementTab"
-import { copyKeyObject } from "./Placement"
 import Modal from "antd/lib/modal/Modal"
 import SearchContainer from "../SearchContainer"
 import Input from "antd/lib/input/Input"
 import api from "../../utils/api"
 
 interface AddPlacementContainerProps {
-    addDestination: (destinationContainer) => void
+    onConfirm: (destinationContainer) => void
 }
 
 //component used to handle the creation of a new destination container
-const AddPlacementContainer = ({ addDestination }: AddPlacementContainerProps) => {
+const AddPlacementContainer = ({ onConfirm }: AddPlacementContainerProps) => {
     const [isPopup, setIsPopup] = useState<boolean>(false)
 
     //used to hold loaded container data
     const [loadedContainer, setLoadedContainer] = useState<any>({})
-    const [selectedTab, setSelectedTab] = useState<string>('new')
+    const [selectedTab, setSelectedTab] = useState<string>('load')
     const [error, setError] = useState<string | undefined>(undefined)
-    const [form] = Form.useForm()
 
     const coordinates = useAppSelector(selectCoordinatesByID)
 
@@ -36,16 +34,15 @@ const AddPlacementContainer = ({ addDestination }: AddPlacementContainerProps) =
         loadedSamples.data.results.forEach(sample => {
             newDestination[sample.id] = { id: sample.id, coordinates: (coordinates[sample.coordinate].name), type: PLACED_STRING, name: sample.name, sourceContainer: containerName }
         })
-        setLoadedContainer({ container_name: containerName, container_kind: container.data.kind, samples: copyKeyObject(newDestination), })
+        setLoadedContainer({ container_name: containerName, container_kind: container.data.kind, samples: { ...newDestination } })
     }, [coordinates])
 
     //calls addDestination prop with 'New Destination' container
     const handleConfirm = useCallback(() => {
-        const container = selectedTab == 'load' ? loadedContainer : { ...form.getFieldsValue(), samples: { 6: {id: 6}} }
+        const container = loadedContainer
         if (container.container_name && container.container_kind && container.container_kind != 'tube') {
-            addDestination(container)
+            onConfirm(container)
             setIsPopup(false)
-            form.resetFields()
         } else {
             setError("Invalid container kind")
         }
@@ -54,7 +51,7 @@ const AddPlacementContainer = ({ addDestination }: AddPlacementContainerProps) =
 
     return (
         <>
-            <Button onClick={() => setIsPopup(true)}>Add Destination</Button>
+            <Button onClick={() => setIsPopup(true)}>Load Destination</Button>
             <Modal title="Add Destination" visible={isPopup} onOk={handleConfirm} onCancel={() => setIsPopup(false)}>
                 <div style={{ display: 'flex', flexDirection: 'column' }}>
                     {error ?
@@ -68,18 +65,6 @@ const AddPlacementContainer = ({ addDestination }: AddPlacementContainerProps) =
                         />
                         : ''}
                     <Tabs defaultActiveKey={'New'} activeKey={selectedTab} onTabClick={(e) => setSelectedTab(e)}>
-                        <Tabs.TabPane tab='New Container' key={'new'}>
-                            <Form
-                                form={form}
-                                layout="horizontal">
-                                <Form.Item name={"container_name"}>
-                                    <Input placeholder="Barcode"></Input>
-                                </Form.Item>
-                                <Form.Item name={"container_kind"}>
-                                    <Select placeholder="Container kind" style={{ width: "100%" }} options={[{ value: '96-well plate', label: '96-well plate' }]}></Select>
-                                </Form.Item>
-                            </Form>
-                        </Tabs.TabPane>
                         <Tabs.TabPane tab='Load Container' key={'load'}>
                             <SearchContainer handleOnChange={(value) => handleContainerLoad(value)} />
                         </Tabs.TabPane>
