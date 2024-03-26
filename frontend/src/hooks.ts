08f0ca3604838f7337b7ddc294c2cd22e7ff2ff2
Nafiz Islam <nafiz.islam1001@gmail.com>
2024-03-26 13:26:28 -0400

    move useSampleList into hooks.ts

--- frontend/src/hooks.ts
+++ frontend/src/hooks.ts
@@ -1,6 +1,44 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 import type { RootState, AppDispatch } from './store'
+import { FMSId } from './models/fms_api_models'
+import { useEffect, useMemo, useState } from 'react'
+import { fetchLibrariesForSamples, fetchSamples } from './modules/cache/cache'
+import { selectLibrariesByID, selectSamplesByID } from './selectors'
+import { SampleAndLibrary } from './components/WorkflowSamplesTable/ColumnSets'
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch: () => AppDispatch = useDispatch
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+
+export function useSampleList(sampleIDs: FMSId[], offset?: number, size?: number) {
+	const [isFetching, setIsFetching] = useState(false)
+    const endIndex = size !== undefined ? (offset ?? 0) + size : undefined
+
+	useEffect(() => {
+		(async () => {
+			setIsFetching(true)
+			const samples = await fetchSamples(sampleIDs.slice(offset, endIndex))
+			const samplesWithLibraries = samples.filter((s) => s.is_library).map((s) => s.id)
+			await fetchLibrariesForSamples(samplesWithLibraries)
+			setIsFetching(false)
+		})()
+	}, [sampleIDs, endIndex, size])
+
+	const samplesByID = useAppSelector(selectSamplesByID)
+	const librariesByID = useAppSelector(selectLibrariesByID)
+
+	const availableSamples = useMemo(() => sampleIDs.slice(offset, endIndex).reduce((acc, sampleID) => {
+		const sample = samplesByID[sampleID]
+		if (sample) {
+			if (sample.is_library) {
+				const library = librariesByID[sampleID]
+				acc.push({ sample, library })
+			} else {
+				acc.push({ sample })
+			}
+		}
+		return acc
+	}, [] as SampleAndLibrary[]), [sampleIDs, samplesByID, librariesByID, offset, endIndex])
+
+	return [availableSamples, isFetching] as const
+}
