795cf98734f35a9c69847e555b77e4d061a4a999
Nafiz Islam <nafiz.islam1001@gmail.com>
2024-04-03 08:41:40 -0400

    wip

--- frontend/src/modules/placement/reducers.ts
+++ frontend/src/modules/placement/reducers.ts
@@ -1,29 +1,29 @@
-import { PayloadAction, createAction, createReducer, createSlice } from "@reduxjs/toolkit"
+import { PayloadAction, createSlice } from "@reduxjs/toolkit"
 import { Container, Coordinate, Sample } from "../../models/frontend_models"
 import { CoordinateSpec } from "../../models/fms_api_models"
 
 export interface CellIdentifier {
-    parentContainer: Container['id']
+    parentContainerID: Container['id']
     column: number
     row: number
 }
 
-export interface Cell {
-    state?: 'selected' | 'preview' | 'placed'
-    sample?: Sample['id']
-    fromCell?: CellIdentifier
-    toCell?: CellIdentifier
+export type CellState = {
+    state: 'none' | 'selected' | 'preview' | 'placed-out' | 'placed-in'
+    sample: Sample['id'] | null
+    toCell: CellIdentifier | null
+    fromCell: CellIdentifier | null
 }
 
 export interface PlacementContainerState {
-    isSource: boolean
     spec: CoordinateSpec
-    cells: Cell[][]
+    cells: CellState[][]
 }
 
 export interface PlacementState {
     parentContainers: Record<Container['id'], PlacementContainerState | undefined>
     activeSelection: CellIdentifier[]
+    dragStart: CellIdentifier  | null
 }
 
 export interface LoadSamplesAndContainersPayload {
@@ -39,29 +39,40 @@ export interface LoadSamplesAndContainersPayload {
     }[]
 }
 
+export interface ClickCellPayload extends CellIdentifier {}
+
+const initialState: PlacementState = {
+    parentContainers: {},
+    activeSelection: [],
+    dragStart: null
+}
 
 const slice = createSlice({
     name: 'PLACEMENT',
-    initialState: { parentContainers: {} } as PlacementState,
+    initialState,
     reducers: {
         loadSourceSamples(state, action: PayloadAction<LoadSamplesAndContainersPayload>) {
-            const payload = action.payload
-            for (const parentContainer of payload.parentContainers) {
+            const parentContainers = action.payload.parentContainers
+            for (const parentContainer of parentContainers) {
                 // initialize empty cells array
-                const parentContainerState = state.parentContainers[parentContainer.id] ?? {
-                    isSource: parentContainer.isSource,
+                const parentContainerState: NonNullable<PlacementState['parentContainers'][number]> = {
                     cells: [],
-                    spec: parentContainer.spec 
+                    spec: parentContainer.spec
                 }
-                state.parentContainers[parentContainer.id] ??= parentContainerState
+                state.parentContainers[parentContainer.id] = parentContainerState
 
                 // initialize array of cells
                 const rowLength = parentContainer.spec[0]?.length ?? 1
                 if (parentContainerState.cells.length < rowLength) {
                     for (let row = 0; row < rowLength; row++) {
-                        const rowCell: Cell[] = []
+                        const rowCell: CellState[] = []
                         for (let col = 0; col < (parentContainer.spec[1]?.length ?? 1); col++) {
-                            rowCell.push({})
+                            rowCell.push({
+                                state: 'none',
+                                sample: null,
+                                toCell: null,
+                                fromCell: null
+                            })
                         }
                         parentContainerState.cells.push(rowCell)
                     }
@@ -70,10 +81,33 @@ const slice = createSlice({
                 // populate cells
                 const cells = parentContainerState.cells
                 for (const container of parentContainer.containers) {
-                    cells[container.row][container.column].sample = container.sample
+                    cells[container.row][container.column].sample = container.sample ?? null
                 }
             }
         },
+        clickCell(state, action: PayloadAction<ClickCellPayload>) {
+            const { parentContainerID, row, column } = action.payload
+            const parentContainer = state.parentContainers[parentContainerID]
+            if (parentContainer) {
+                const cell = parentContainer.cells[row][column]
+                if (cell.state === 'none'|| cell.state === 'preview') {
+                    if (cell.sample) {
+                        state.activeSelection.push(action.payload)
+                    } else if (state.activeSelection.length > 0) {
+                        cell.state = 'placed-in'
+                        cell.fromCell = action.payload
+                        // should not be undefined
+                        const activeSelection = state.activeSelection.pop() as CellIdentifier
+                        const sourceCell = state.parentContainers[activeSelection.parentContainerID]?.cells[activeSelection.row][activeSelection.column]
+                        if (sourceCell) {
+                            sourceCell.state = 'placed-out'
+                            sourceCell.toCell = action.payload
+                        }
+                    }
+                }
+            }
+            return state
+        }
     }
 })
 
