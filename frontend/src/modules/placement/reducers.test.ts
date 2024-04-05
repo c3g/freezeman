c6e24b7b71bd7651eec210cf3952d255bb74ecb5
Nafiz Islam <nafiz.islam1001@gmail.com>
2024-04-05 16:02:32 -0400

    wip

--- frontend/src/modules/placement/reducers.test.ts
+++ frontend/src/modules/placement/reducers.test.ts
@@ -1,13 +1,7 @@
 import { describe, expect, test } from '@jest/globals';
-import reducer, { loadSamplesAndContainers, clickCell, initialState, PlacementState, PlacementContainerState, LoadSamplesAndContainersPayload, CellIdentifier, CellState, createEmptyCells, MouseOnCellPayload } from './reducers'
+import reducer, { actions, initialState, PlacementState, PlacementContainerState, LoadSamplesAndContainersPayload, CellIdentifier, CellState, createEmptyCells, MouseOnCellPayload, coordinatesToOffsets, offsetsToCoordinates, PlacementType, PlacementDirection, placementDestinationLocations } from './reducers'
+const { loadSamplesAndContainers, clickCell } = actions
 import { CoordinateSpec } from '../../models/fms_api_models';
-
-test('initialize with 0 containers and samples', () => {
-    expect(reducer(
-        initialState,
-        loadSamplesAndContainers({ parentContainers: [] })
-    )).toEqual(initialState)
-})
 
 type LoadParentContainerPayload = LoadSamplesAndContainersPayload['parentContainers'][number]
 
@@ -15,13 +9,13 @@ const srcContainer: LoadParentContainerPayload = {
     name: 'Source Container',
     spec: [['A', 'B', 'C'], ['01', '02', '03']] as CoordinateSpec,
     containers: [{
-        coordinate: 'A01',
+        coordinates: 'A01',
         sample: 1
     }, {
-        coordinate: 'B02',
+        coordinates: 'B02',
         sample: 2
     }, {
-        coordinate: 'C03',
+        coordinates: 'C03',
         sample: 3
     }]
 }
@@ -30,81 +24,242 @@ const dstContainer: LoadParentContainerPayload = {
     name: 'Destination Container',
     spec: [['A', 'B', 'C', 'D'], ['01', '02', '03', '04']] as CoordinateSpec,
     containers: [{
-        coordinate: 'A01',
+        coordinates: 'A01',
         sample: 1
     }, {
-        coordinate: 'B02',
+        coordinates: 'B02',
         sample: 2
     }, {
-        coordinate: 'C03',
+        coordinates: 'C03',
         sample: 3
     }, {
-        coordinate: 'D04',
+        coordinates: 'D04',
         sample: 4
     }]
 }
 
-
-function makeExpectedParentContainerState(parentContainer: LoadParentContainerPayload): PlacementState['parentContainers'] {
-    const { name, spec, containers } = parentContainer
-    return {
-        [name]: {
-            spec,
-            cells: {
-                ...createEmptyCells(spec),
-                ...containers.reduce((cells: PlacementContainerState['cells'], c) => {
-                    cells[c.coordinate] = { state: { status: 'none', sample: c.sample } }
-                    return cells
-                }, {})
+describe('loadSamplesAndContainers', () => {
+    test('initialize with 0 containers and samples', () => {
+        expect(reducer(
+            initialState,
+            loadSamplesAndContainers({ parentContainers: [] })
+        )).toEqual(initialState)
+    })
+    
+    test('initialize with 2 containers containing more than one container', () => {
+        function makeExpectedParentContainerState(parentContainer: LoadParentContainerPayload): PlacementState['parentContainers'] {
+            const { name, spec, containers } = parentContainer
+            return {
+                [name]: {
+                    spec,
+                    cells: {
+                        ...createEmptyCells(spec),
+                        ...containers.reduce((cells: PlacementContainerState['cells'], c) => {
+                            cells[c.coordinates] = { preview: false, selected: false, sample: c.sample }
+                            return cells
+                        }, {})
+                    }
+                }
             }
         }
-    }
-}
-
-test('initialize with 2 containers containing more than one container', () => {
-    const expectedState: PlacementState = {
-        ...initialState,
-        parentContainers: {
-            ...makeExpectedParentContainerState(srcContainer),
-            ...makeExpectedParentContainerState(dstContainer)
+    
+        const expectedState: PlacementState = {
+            ...initialState,
+            parentContainers: {
+                ...makeExpectedParentContainerState(srcContainer),
+                ...makeExpectedParentContainerState(dstContainer)
+            }
         }
-    }
-
-    expect(reducer(
-        initialState,
-        loadSamplesAndContainers({ parentContainers: [srcContainer, dstContainer] })
-    )).toEqual(expectedState)
+    
+        expect(reducer(
+            initialState,
+            loadSamplesAndContainers({ parentContainers: [srcContainer, dstContainer] })
+        )).toEqual(expectedState)
+    })    
 })
 
-describe('select all samples from source, preview them on destination and then place', () => {
+describe('coordinatesToOffsets', () => {
+    type GoodTestCase = { spec: CoordinateSpec, coordinates: string, offsets: number[] }
+    const goodTestCases: GoodTestCase[] = [
+        { spec: dstContainer.spec, coordinates: "A01", offsets: [0, 0] },
+        { spec: dstContainer.spec, coordinates: "B02", offsets: [1, 1] },
+        { spec: dstContainer.spec, coordinates: "C03", offsets: [2, 2] },
+        { spec: dstContainer.spec, coordinates: "D04", offsets: [3, 3] },
+        { spec: dstContainer.spec, coordinates: "D01", offsets: [3, 0] },
+    ]
+    test.each(goodTestCases)('successfully convert coordinates ($coordinates) to index offsets ($offsets)', ({spec, coordinates, offsets}) => {     
+        expect(coordinatesToOffsets(spec, coordinates)).toEqual(offsets)
+    })
+
+    type BadTestCase = { spec: CoordinateSpec, coordinates: string }
+    const badTestCases: BadTestCase[] = [
+        { spec: srcContainer.spec, coordinates: "D04"},
+        { spec: dstContainer.spec, coordinates: "E05"},
+    ]
+    test.each(badTestCases)('throw error when converting invalid coordinates ($coordinates) based on spec', ({spec, coordinates}) => {
+        expect(() => (coordinatesToOffsets(spec, coordinates))).toThrow()
+    })
+})
+
+describe('offsetsToCoordinates', () => {
+    type GoodTestCase = { spec: CoordinateSpec, coordinates: string, offsets: number[] }
+    const goodTestCases: GoodTestCase[] = [
+        { spec: dstContainer.spec, coordinates: "A01", offsets: [0, 0] },
+        { spec: dstContainer.spec, coordinates: "B02", offsets: [1, 1] },
+        { spec: dstContainer.spec, coordinates: "C03", offsets: [2, 2] },
+        { spec: dstContainer.spec, coordinates: "D04", offsets: [3, 3] },
+        { spec: dstContainer.spec, coordinates: "D01", offsets: [3, 0] },
+    ]
+    test.each(goodTestCases)('successfully convert index offsets ($offsets) to coordinates ($coordinates)', ({spec, coordinates, offsets}) => {
+        expect(offsetsToCoordinates(offsets, spec)).toEqual(coordinates)
+    })
+
+    type BadTestCase = { spec: CoordinateSpec, offsets: number[] }
+    const badTestCases: BadTestCase[] = [
+        { spec: dstContainer.spec, offsets: [-1, -1] },
+        { spec: dstContainer.spec, offsets: [4, 4] },
+        { spec: srcContainer.spec, offsets: [3, 3] },
+    ]
+    test.each(badTestCases)('throw error when converting invalid index offsets ($offsets) based on spec', ({spec, offsets}) => {
+        expect(() => offsetsToCoordinates(offsets, spec)).toThrow()
+    })
+})
+
+describe('placementDestinationLocations', () => {
+    const state = reducer(initialState, loadSamplesAndContainers({ parentContainers: [srcContainer, dstContainer] }))
+
+    type GoodTestCase = { state: PlacementState, sources: CellIdentifier[], destination: CellIdentifier, placementType: PlacementType, placementDirection: PlacementDirection, expected: CellIdentifier[] }
+    const goodTestCases: GoodTestCase[] = [
+        {
+            state: state,
+            sources: [
+                { parentContainer: srcContainer.name, coordinates: "A01" },
+                { parentContainer: srcContainer.name, coordinates: "B02" },
+                { parentContainer: srcContainer.name, coordinates: "C03" }
+            ],
+            destination: { parentContainer: dstContainer.name, coordinates: "B02" },
+            placementType: 'group',
+            placementDirection: 'row',
+            expected: [
+                { parentContainer: dstContainer.name, coordinates: "B02" },
+                { parentContainer: dstContainer.name, coordinates: "B03" },
+                { parentContainer: dstContainer.name, coordinates: "B04" }
+            ]
+        },
+        {
+            state: state,
+            sources: [
+                { parentContainer: srcContainer.name, coordinates: "A01" },
+                { parentContainer: srcContainer.name, coordinates: "B02" },
+                { parentContainer: srcContainer.name, coordinates: "C03" }
+            ],
+            destination: { parentContainer: dstContainer.name, coordinates: "B02" },
+            placementType: 'group',
+            placementDirection: 'column',
+            expected: [
+                { parentContainer: dstContainer.name, coordinates: "B02" },
+                { parentContainer: dstContainer.name, coordinates: "C02" },
+                { parentContainer: dstContainer.name, coordinates: "D02" }
+            ]
+        },
+        {
+            state: state,
+            sources: [
+                { parentContainer: srcContainer.name, coordinates: "A01" },
+                { parentContainer: srcContainer.name, coordinates: "B02" },
+                { parentContainer: srcContainer.name, coordinates: "C03" }
+            ],
+            destination: { parentContainer: dstContainer.name, coordinates: "B02" },
+            placementType: 'pattern',
+            placementDirection: 'column',
+            expected: [
+                { parentContainer: dstContainer.name, coordinates: "B02" },
+                { parentContainer: dstContainer.name, coordinates: "C03" },
+                { parentContainer: dstContainer.name, coordinates: "D04" }
+            ]
+        },
+    ]
+    test.each(goodTestCases)('successfully generate destination locations from source samples with type ($placementType) and direction ($placementDirection)', ({ state, sources, destination, placementType, placementDirection, expected }) => {
+        expect(placementDestinationLocations(state, sources, destination, placementType, placementDirection)).toEqual(expected)
+    })
+
+    type BadTestCase = { state: PlacementState, sources: CellIdentifier[], destination: CellIdentifier, placementType: PlacementType, placementDirection: PlacementDirection }
+    const badTastCases: BadTestCase[] = [
+        {
+            state: state,
+            sources: [
+                { parentContainer: srcContainer.name, coordinates: "A01" },
+                { parentContainer: srcContainer.name, coordinates: "B02" },
+                { parentContainer: srcContainer.name, coordinates: "C03" }
+            ],
+            destination: { parentContainer: dstContainer.name, coordinates: "B03" },
+            placementType: 'group',
+            placementDirection: 'row',
+        },
+        {
+            state: state,
+            sources: [
+                { parentContainer: srcContainer.name, coordinates: "A01" },
+                { parentContainer: srcContainer.name, coordinates: "B02" },
+                { parentContainer: srcContainer.name, coordinates: "C03" }
+            ],
+            destination: { parentContainer: dstContainer.name, coordinates: "C02" },
+            placementType: 'group',
+            placementDirection: 'column',
+        },
+        {
+            state: state,
+            sources: [
+                { parentContainer: srcContainer.name, coordinates: "A01" },
+                { parentContainer: srcContainer.name, coordinates: "B02" },
+                { parentContainer: srcContainer.name, coordinates: "C03" }
+            ],
+            destination: { parentContainer: dstContainer.name, coordinates: "C03" },
+            placementType: 'pattern',
+            placementDirection: 'column',
+        },
+    ]
+    test.each(badTastCases)('throw error when there is risk of going out of bounds with type ($placementType) and direction ($placementDirection)', ({ state, sources, destination, placementType, placementDirection }) => {
+        expect(() => placementDestinationLocations(state, sources, destination, placementType, placementDirection)).toThrow()
+    })
+
+})
+
+describe.skip('select all samples from source, preview them on destination and then place', () => {
     let state = reducer(initialState, loadSamplesAndContainers({ parentContainers: [srcContainer, dstContainer] }))
 
-    const placedOutCoords = srcContainer.containers.map((container) => container.coordinate)
+    const placedOutCoords = srcContainer.containers.map((container) => container.coordinates)
 
     test('select all samples from source', () => {
-        state = placedOutCoords.reduce((state, coordinate) => {
+        state = placedOutCoords.reduce((state, coordinates) => {
             return reducer(state, clickCell({
                 parentContainer: srcContainer.name,
-                coordinate
+                coordinates,
+                placementType: 'group',
+                placementDirection: 'row'
             }))
         }, state)
-        expect(placedOutCoords.map((coordinate) => state.parentContainers[srcContainer.name]?.cells[coordinate]?.state)).toMatchObject({ status: 'selected' })
-        expect(state.activeSelections).toHaveLength(3)
+        placedOutCoords.map((coordinates) => state.parentContainers[srcContainer.name]?.cells[coordinates]).forEach((cell) => {
+            expect(cell?.selected).toEqual(true)
+        })
+        expect(state.activeSelections).toHaveLength(placedOutCoords.length)
+        expect(state.error).toBeUndefined()
     })
 
     const placedInCoords = ['D01', 'D02', 'D03']
 
     const dstLocation: MouseOnCellPayload = {
         parentContainer: dstContainer.name,
-        coordinate: 'D01',
+        coordinates: 'D01',
         placementType: 'group',
         placementDirection: 'row'
     }
     test('place a sample in destination from source', () => {
         state = reducer(state, clickCell(dstLocation))
 
-        placedOutCoords.map(())
+        placedOutCoords.map((coordinates) => state.parentContainers[dstContainer.name]?.cells[coordinates])
 
         expect(state.activeSelections).toHaveLength(0)
+        expect(state.error).toBeUndefined()
     })
 })
--- frontend/src/modules/placement/reducers.ts
+++ frontend/src/modules/placement/reducers.ts
@@ -1,4 +1,4 @@
-import { Draft, PayloadAction, createSlice } from "@reduxjs/toolkit"
+import { Draft, PayloadAction, createSlice, original } from "@reduxjs/toolkit"
 import { Container, Sample } from "../../models/frontend_models"
 import { CoordinateSpec } from "../../models/fms_api_models"
 
@@ -13,8 +13,8 @@ export interface CellState {
     preview: boolean
     selected: boolean
     sample: Sample['id'] | null
-    fromCell?: CellIdentifier
-    toCell?: CellIdentifier
+    placedFrom?: CellIdentifier
+    placedAt?: CellIdentifier
 }
 
 export interface PlacementContainerState {
@@ -25,7 +25,7 @@ export interface PlacementContainerState {
 export interface PlacementState {
     parentContainers: Record<ContainerIdentifier, PlacementContainerState | undefined>
     activeSelections: CellIdentifier[]
-    errors?: string
+    error?: string
 }
 
 export interface LoadSamplesAndContainersPayload {
@@ -39,14 +39,16 @@ export interface LoadSamplesAndContainersPayload {
     }[]
 }
 
+export type PlacementType = 'group' | 'pattern'
+export type PlacementDirection = 'row' | 'column'  
 export interface MouseOnCellPayload extends CellIdentifier {
-    placementType?: 'group' | 'pattern'
-    placementDirection?: 'row' | 'column'
+    placementType: PlacementType
+    placementDirection: PlacementDirection
 }
 
 export function createEmptyCells(spec: CoordinateSpec) {
     const cells: PlacementContainerState['cells'] = {}
-    for (const row of spec[0] ?? ['']) {
+    for (const row of spec[0] ?? []) {
         for (const col of spec[1] ?? ['']) {
             cells[row + col] = {
                 sample: null,
@@ -66,46 +68,38 @@ function atLocation(id: CellIdentifier) {
 function getContainerAndCell(state: Draft<PlacementState>, location: CellIdentifier) {
     const container = state.parentContainers[location.parentContainer]
     if (!container) {
-        throw new Error(`Invalid container: "${location.parentContainer}"`)
+        throw new Error(`Container not found: "${location.parentContainer}"`)
     }
     const cell = container.cells[location.coordinates]
     if (!cell) {
         throw new Error(`Invalid coordinate: "${atLocation(location)}"`)
     }
 
-    return [container, cell] as const
+    return { container, cell }
 }
 
 function placeCell(state: Draft<PlacementState>, sourceLocation: CellIdentifier, destinationLocation: CellIdentifier) {
-    const [sourceContainer, sourceCell] = getContainerAndCell(state, sourceLocation)
-    if (!sourceContainer || !sourceCell) {
-        return state
-    }
+    const sourceCell = getContainerAndCell(state, sourceLocation).cell
     if (!sourceCell.sample) {
         throw new Error(`Container at "${atLocation(sourceLocation)}" has no sample`)
     }
-    if (sourceCell.fromCell || sourceCell.toCell) {
-        throw new Error(`Cannot place sample from ${atLocation(sourceLocation)}`)
+    if (sourceCell.placedAt) {
+        throw new Error(`Sample at ${atLocation(sourceLocation)} already placed`)
     }
 
-    const [destinationContainer, destinationCell] = getContainerAndCell(state, destinationLocation)
-    if (!destinationContainer || !destinationCell) {
-        return state
-    }
-    if (destinationCell.sample !== null) {
+    const destinationCell = getContainerAndCell(state, destinationLocation).cell
+    if (destinationCell.sample !== null && !destinationCell.placedAt) {
         throw new Error(`Container at ${atLocation(destinationLocation)} already contains sample`)
     }
-    if (destinationCell.fromCell || destinationCell.toCell) {
-        throw new Error(`Cannot place sample at ${atLocation(destinationLocation)}`)
+    if (destinationCell.sample === null && !destinationCell.placedFrom) {
+        throw new Error(`Sample already placed at ${atLocation(destinationLocation)}`)
     }
 
-    sourceCell.toCell = destinationLocation
-    destinationCell.fromCell = sourceLocation
-
-    return state
+    sourceCell.placedAt = destinationLocation
+    destinationCell.placedFrom = sourceLocation
 }
 
-function coordinatesToOffsets(spec: CoordinateSpec, coordinates: string) {
+export function coordinatesToOffsets(spec: CoordinateSpec, coordinates: string) {
     const offsets: number[] = []
     const originalCoordinates = coordinates
     for (const axis of spec) {
@@ -113,6 +107,9 @@ function coordinatesToOffsets(spec: CoordinateSpec, coordinates: string) {
             throw new Error(`Cannot convert coordinates ${originalCoordinates} with spec ${JSON.stringify(spec)} to offsets`)
         }
         const offset = axis.findIndex((coordinate) => coordinates.startsWith(coordinate))
+        if (offset < 0) {
+            throw new Error(`Cannot convert coordinates ${originalCoordinates} with spec ${JSON.stringify(spec)} to offsets`)
+        }
         offsets.push(offset)
         coordinates = coordinates.slice(axis[offset].length)
     }
@@ -120,44 +117,67 @@ function coordinatesToOffsets(spec: CoordinateSpec, coordinates: string) {
     return offsets
 }
 
-function offsetsToCoordinates(offsets: number[], spec: CoordinateSpec) {
+export function offsetsToCoordinates(offsets: readonly number[], spec: CoordinateSpec) {
     if (spec.length !== offsets.length) {
         throw new Error(`Cannot convert offsets ${JSON.stringify(offsets)} to coordinates with spec ${JSON.stringify(spec)}`)
     }
     
     const coordinates: string[] = []
     for (let i = 0; i < spec.length; i++) {
+        if (offsets[i] < 0) {
+            throw new Error('Numbers in offsets argument cannot be negative')
+        }
+        if (offsets[i] >= spec[i].length) {
+            throw new Error(`Cannot convert offset ${JSON.stringify(offsets)} to coordinates with spec ${JSON.stringify(spec)} at axis ${i}`)
+        }
         coordinates.push(spec[i][offsets[i]])
     }
-    return coordinates.join()
+    return coordinates.join('')
 }
 
-function placementDestinationCoordinates(state: PlacementState, sources: CellIdentifier[], destination: CellIdentifier) {
-    const sourceOffsetsList = sources.map((source) => {
-        const parentContainer = state.parentContainers[source.parentContainer]
-        if (parentContainer) {
-            return coordinatesToOffsets(parentContainer.spec, source.coordinates)
-        } else {
-            throw new Error(`Could not find source container at ${atLocation(source)}`)
-        }
-    })
-
+export function placementDestinationLocations(state: PlacementState, sources: CellIdentifier[], destination: CellIdentifier, placementType: PlacementType, placementDirection: PlacementDirection): CellIdentifier[] {
+    const newOffsetsList: number[][] = []
     const destinationContainer = state.parentContainers[destination.parentContainer]
     if (!destinationContainer) {
         throw new Error(`Could not find destination container at ${atLocation(destination)}`)
     }
     const destinationStartingOffsets = coordinatesToOffsets(destinationContainer.spec, destination.coordinates)
 
-    const newOffsetsList: typeof sourceOffsetsList = []
-    for (const sourceOffsets of sourceOffsetsList) {
-        const newSourceOffsets: typeof sourceOffsets = []
-        for (let index = 0; index < destinationContainer.spec.length; index++) {
-            newSourceOffsets.push(sourceOffsets[index] + destinationStartingOffsets[index])
+    switch (placementType) {
+        case 'pattern': {
+            const sourceContainerNames = new Set(sources.map((s) => s.parentContainer))
+            if (sourceContainerNames.size > 1) {
+                throw new Error('Cannot use pattern placement type with more than one source container')
+            }
+
+            const sourceOffsetsList = sources.map((source) => {
+                const parentContainer = state.parentContainers[source.parentContainer]
+                if (parentContainer) {
+                    return coordinatesToOffsets(parentContainer.spec, source.coordinates)
+                } else {
+                    throw new Error(`Could not find source container at ${atLocation(source)}`)
+                }
+            })
+        
+            for (const sourceOffsets of sourceOffsetsList) {
+                const newSourceOffsets: typeof sourceOffsets = []
+                for (let index = 0; index < destinationContainer.spec.length; index++) {
+                    newSourceOffsets.push(sourceOffsets[index] + destinationStartingOffsets[index])
+                }
+                newOffsetsList.push(newSourceOffsets)
+            }
+            break
         }
-        newOffsetsList.push(newSourceOffsets)
+        case 'group': {
+            for (let index = 0; index < sources.length; index++) {
+                newOffsetsList.push(destinationStartingOffsets.map((offset, axis) =>
+                    offset + (placementDirection === 'row' && axis == 1 ? index : 0) + (placementDirection === 'column' && axis == 0 ? index : 0)
+                ))
+            }        
+        }
     }
 
-    return newOffsetsList.map((offsets) => offsetsToCoordinates(offsets, destinationContainer.spec))
+    return newOffsetsList.map((offsets) => ({ parentContainer: destination.parentContainer, coordinates: offsetsToCoordinates(offsets, destinationContainer.spec) }))
 }
 
 export const initialState: PlacementState = {
@@ -193,15 +213,41 @@ const slice = createSlice({
             return state
         },
         clickCell(state, action: PayloadAction<MouseOnCellPayload>) {
-            const { parentContainer, coordinates: coordinate, placementType = 'group', placementDirection = 'row' } = action.payload
-            
-            const [clickedContainer, clickedCell] = getContainerAndCell(state, { parentContainer, coordinates: coordinate })
-            if (!clickedContainer || !clickedCell) {
-                return state
+            try {
+                const { parentContainer, coordinates: coordinate, placementType = 'group', placementDirection = 'row' } = action.payload
+                
+                const clickedLocation: CellIdentifier = { parentContainer, coordinates: coordinate }
+                const clickedCell = getContainerAndCell(state, clickedLocation).cell
+                if (!clickedCell.placedAt) {
+                    clickedCell.selected = !clickedCell.selected
+                    if (clickedCell.selected) {
+                        state.activeSelections.push(clickedLocation)
+                    } else {
+                        state.activeSelections = state.activeSelections.filter((c) => !(c.coordinates === clickedLocation.coordinates && c.parentContainer === clickedLocation.parentContainer))
+                    }
+                } else if (state.activeSelections.length > 0) {
+                    // relying on placeCell to do error checking
+
+                    const destinationLocations = placementDestinationLocations(state, state.activeSelections, clickedLocation, placementType, placementDirection)
+                    for (let index = 0; index < state.activeSelections.length; index++) {
+                        placeCell(state, state.activeSelections[index], destinationLocations[index])
+                        getContainerAndCell(state, state.activeSelections[index]).cell.selected = false
+                    }
+                    state.activeSelections = []
+                }
+            } catch (e) {
+                const originalState = original(state) ?? initialState
+                throw e
+                return {
+                    ...originalState,
+                    error: e.toString()
+                }
             }
+
+            return state
         }
     }
 })
 
-export const { loadSamplesAndContainers } = slice.actions
+export const actions = slice.actions
 export default slice.reducer
