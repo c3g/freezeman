bbf1d6ab06608f052b3abfd4fa4155f2981c905b
Nafiz Islam <nafiz.islam@computationalgenomics.ca>
2024-04-08 17:13:17 -0400

    wip

---
+++ frontend/src/components/placementVisuals/AddPlacementContainer2.tsx
@@ -0,0 +1,208 @@
+import React, { useCallback, useState } from "react"
+import { Button, Select, Tabs, notification, Row } from "antd"
+import { useAppDispatch, useAppSelector } from "../../hooks"
+import { selectCoordinatesByID, selectContainerKindsByID } from "../../selectors"
+import Modal from "antd/lib/modal/Modal"
+import SearchContainer from "../SearchContainer"
+import Input from "antd/lib/input/Input"
+import api from "../../utils/api"
+import { MAX_CONTAINER_BARCODE_LENGTH, MAX_CONTAINER_NAME_LENGTH, barcodeRules, nameRules } from "../../constants"
+import { FMSContainer, FMSId, FMSSample } from "../../models/fms_api_models"
+
+interface DestinationContainer {
+    container_barcode: string
+    container_name: string
+    container_kind: string
+    samples: { [key in FMSId]: { id: FMSId, coordinates: string | 0, name: string } }
+}
+
+interface AddPlacementContainerProps {
+    onConfirm: (destinationContainer: DestinationContainer) => void
+    destinationContainerList: DestinationContainer[]
+}
+
+//component used to handle the creation of a new destination container
+const AddPlacementContainer = ({ onConfirm, destinationContainerList }: AddPlacementContainerProps) => {
+    const [isPopup, setIsPopup] = useState<boolean>(false)
+
+    //used to hold loaded container data
+    const [loadedContainer, setLoadedContainer] = useState<Partial<DestinationContainer>>({})
+    const [selectedTab, setSelectedTab] = useState<string>('new')
+    const [error, setError] = useState<string | undefined>(undefined)
+    const [newContainer, setNewContainer] = useState<Partial<DestinationContainer>>({})
+
+    const coordinates = useAppSelector(selectCoordinatesByID)
+    const containerKinds = useAppSelector(selectContainerKindsByID)
+
+    const getContainerKindOptions = useCallback(() => {
+        if (containerKinds) {
+            const options = Object.keys(containerKinds).filter(id => containerKinds[id].children_ids.length === 0 && id !== "tube").sort().map((containerKind) => {
+                return {
+                    label: containerKind,
+                    value: containerKind
+                }
+            })
+            return options
+        }
+        return []
+    }, [containerKinds])
+
+    const dispatch = useAppDispatch()
+    //retrieves container on search
+    const handleContainerLoad = useCallback(async (loadedContainer: FMSId) => {
+        const container: FMSContainer = (await dispatch(api.containers.get(loadedContainer))).data
+        const containerName = container.name
+        const containerBarcode = container.barcode
+        const newDestination: DestinationContainer['samples']  = {}
+        if (container.samples.length > 0) {
+            const loadedSamples: FMSSample[] = (await dispatch(api.samples.list({ id__in: container.samples.join(','), limit: 100000 }))).data.results
+
+            loadedSamples.forEach(sample => {
+                newDestination[sample.id] = { id: sample.id, coordinates: (sample.coordinate && coordinates[sample.coordinate].name), name: sample.name }
+            })
+        }
+        setLoadedContainer({ container_barcode: containerBarcode, container_name: containerName, container_kind: container.kind, samples: { ...newDestination }, })
+    }, [coordinates, dispatch])
+
+    //calls addDestination prop with 'New Destination' container
+    const handleConfirm = useCallback(() => {
+        const containerAlreadyExists = async (container: DestinationContainer, destinationContainerList: DestinationContainer[]) => {
+            /* Centralized error notifications */
+            const barcodeExistsError = () => {
+                setError("Existing container barcode.")
+                const EXISTING_BARCODE_NOTIFICATION_KEY = `LabworkStep.placement-existing-container-barcode`
+                notification.error({
+                    message: `New container barcode already exists.`,
+                    key: EXISTING_BARCODE_NOTIFICATION_KEY,
+                    duration: 20
+                })
+            }
+            const nameExistsError = () => {
+                setError("Existing container name.")
+                const EXISTING_NAME_NOTIFICATION_KEY = `LabworkStep.placement-existing-container-name`
+                notification.error({
+                    message: `New container name already exists.`,
+                    key: EXISTING_NAME_NOTIFICATION_KEY,
+                    duration: 20
+                })
+            }
+    
+            let exists = false
+            /* Get locally created containers first */
+            /* Check if the barcode is used in the destinationContainerList */
+            let barcode_exists: boolean = destinationContainerList.some(containerSample => containerSample.container_barcode === container.container_barcode)
+            if (barcode_exists) {
+                barcodeExistsError()
+            }
+            /* Check if the name is used in the destinationContainerList */
+            let name_exists: boolean = destinationContainerList.some(containerSample => containerSample.container_name === container.container_name)
+            if (name_exists) {
+                nameExistsError()
+            }
+            exists = name_exists || barcode_exists
+            if (exists) {
+                /* Fast return if already exists in frontend display */
+                return exists
+            }
+    
+            /* Get existing containers from freezeman */
+            /* Check if there is already a container in Freezeman with that abrcode */
+            const barcodeResult = await dispatch(api.containers.list({ barcode: container.container_barcode }))
+            barcode_exists = barcodeResult.data.count != 0
+            if (barcode_exists) {
+                barcodeExistsError()
+            }
+            /* Check if there is already a container in Freezeman with that name */
+            const nameResult = await dispatch(api.containers.list({ name: container.container_name }))
+            name_exists = nameResult.data.count != 0
+            if (name_exists) {
+                nameExistsError()
+            }
+            exists = name_exists || barcode_exists
+            return exists
+        }    
+
+        const addContainer = (container: DestinationContainer) => {
+            onConfirm(container)
+            setIsPopup(false)
+            setNewContainer({})
+        }
+        const container = selectedTab == "load" ? loadedContainer : newContainer
+        container.container_name = container.container_name ? container.container_name : container.container_barcode
+        if (container.container_barcode && container.container_kind) {
+            if (selectedTab == "load") {
+                addContainer(container as DestinationContainer)
+            }
+            else {
+                containerAlreadyExists(container as DestinationContainer, destinationContainerList).then(exists => {
+                    if (!exists) {
+                        const barCodeResults = barcodeRules.filter((rule) => !rule.pattern.test(container.container_barcode as string))
+                        const nameResults = nameRules.filter((rule) => !rule.pattern.test(container.container_name as string))
+                        if (barCodeResults.length > 0) {
+                            setError("Invalid new container")
+                            const INVALID_BARCODE_NOTIFICATION_KEY = `LabworkStep.placement-invalid-container-barcode`
+                            notification.error({
+                                message: `Container Barcode -- ${barCodeResults.map((rule) => rule.message).join(" ")}`,
+                                key: INVALID_BARCODE_NOTIFICATION_KEY,
+                                duration: 20
+                            })
+                        } if (nameResults.length > 0) {
+                            setError("Invalid new container")
+                            const INVALID_NAME_NOTIFICATION_KEY = `LabworkStep.placement-invalid-container-name`
+                            notification.error({
+                                message: `Container Name -- ${nameResults.map((rule) => rule.message).join(" ")}`,
+                                key: INVALID_NAME_NOTIFICATION_KEY,
+                                duration: 20
+                            })
+                        } else {
+                            addContainer(container as DestinationContainer)
+                        }
+                    }
+                })
+            }
+        } else {
+            setError("Invalid container kind")
+            const INVALID_KIND_NOTIFICATION_KEY = `LabworkStep.placement-invalid-container-kind`
+            notification.error({
+                message: `Invalid destination container kind.`,
+                key: INVALID_KIND_NOTIFICATION_KEY,
+                duration: 20
+            })
+        }
+    }, [selectedTab, loadedContainer, newContainer, dispatch, onConfirm, destinationContainerList])
+
+    const handleOnChange = useCallback((e, name) => {
+        const container = { ...newContainer }
+        container[name] = e.target ? e.target.value : e
+        setNewContainer(container)
+    }, [newContainer])
+
+
+    return (
+        <>
+            <Button onClick={() => setIsPopup(true)}>Add Destination</Button>
+            <Modal title="Add Destination" open={isPopup} onOk={handleConfirm} onCancel={() => setIsPopup(false)} width={'40vw'}>
+                <div style={{ display: 'flex', flexDirection: 'column' }}>
+                    <Tabs defaultActiveKey={'New'} activeKey={selectedTab} onTabClick={(e) => setSelectedTab(e)}>
+                        <Tabs.TabPane tab='New Container' key={'new'}>
+                            <Row style={{ padding: "10px" }}>
+                                <Input value={newContainer.container_barcode} placeholder="Barcode" onChange={(e) => handleOnChange(e, 'container_barcode')} maxLength={MAX_CONTAINER_BARCODE_LENGTH}></Input>
+                            </Row>
+                            <Row style={{ padding: "10px" }}>
+                                <Input value={newContainer.container_name} placeholder="Name (optional)" onChange={(e) => handleOnChange(e, 'container_name')} maxLength={MAX_CONTAINER_NAME_LENGTH}></Input>
+                            </Row>
+                            <Row style={{ padding: "10px" }}>
+                                <Select value={newContainer.container_kind} clearIcon placeholder="Container kind" onChange={(e) => handleOnChange(e, 'container_kind')} style={{ width: "100%" }} options={getContainerKindOptions()}></Select>
+                            </Row>
+                        </Tabs.TabPane>
+                        <Tabs.TabPane tab='Load Container' key={'load'}>
+                            <SearchContainer exceptKinds={["tube"]} handleOnChange={(value) => handleContainerLoad(value)} />
+                        </Tabs.TabPane>
+                    </Tabs>
+
+                </div>
+            </Modal>
+        </>
+    )
+}
+export default AddPlacementContainer
--- frontend/src/components/placementVisuals/Placement2.tsx
+++ frontend/src/components/placementVisuals/Placement2.tsx
@@ -2,23 +2,134 @@ import React, { useCallback, useEffect, useMemo, useState } from "react"
 import { FMSId } from "../../models/fms_api_models"
 import { useAppDispatch, useAppSelector } from "../../hooks"
 import { loadSamplesAndContainers } from "../../modules/placement/actions"
+import { Button, Col, Input, Modal, Popconfirm, Radio, Row, Select, Switch, Tabs } from "antd"
+import { MAX_CONTAINER_BARCODE_LENGTH, MAX_CONTAINER_NAME_LENGTH } from "../../constants"
+import PageContainer from "../PageContainer"
+import PageContent from "../PageContent"
+import AddPlacementContainer from "./AddPlacementContainer2"
+import ContainerNameScroller from "./ContainerNameScroller"
+import PlacementContainer from "./PlacementContainer"
+import PlacementSamplesTable from "./PlacementSamplesTable"
 
 interface PlacementProps {
     sampleIDs: number[],
     stepID: FMSId,
 }
 
-const Placement = ({ stepID, sampleIDs }: PlacementProps) => {
+function Placement({ stepID, sampleIDs }: PlacementProps) {
     const dispatch = useAppDispatch()
-    const placementState = useAppSelector((state) => state.placement)
-    const sourceContainers = useMemo(() => {
-        const containers = [...Object.keys(placementState.parentContainers)]
-        containers.sort()
-        return containers
-    }, [placementState.parentContainers])
-
     useEffect(() => {
         dispatch(loadSamplesAndContainers(stepID, sampleIDs))
-    })
+    }, [dispatch, sampleIDs, stepID])
+
+
+    return (
+        <>
+            <PageContainer>
+                <PageContent>
+                    <Row justify="end" style={{ padding: "10px" }}>
+                        <Col span={3}>
+                            <AddPlacementContainer onConfirm={(container) => addDestination(container)} destinationContainerList={destinationContainerList} />
+                        </Col>
+                        <Col span={3}>
+                            <Button onClick={saveDestination} style={{ backgroundColor: "#1890ff", color: "white" }}> Save to Prefill </Button>
+                        </Col>
+                    </Row>
+                    <Row justify="start" style={{ paddingTop: "20px", paddingBottom: "40px" }}>
+                        <Col span={12}>
+                            <div className={"flex-row"}>
+                                <div className={"flex-column"}>
+                                    {sourceSamples ?
+                                        <>
+                                            <ContainerNameScroller
+                                                disabled={disableChangeSource}
+                                                containerType={SOURCE_STRING}
+                                                name={sourceSamples.container_name}
+                                                changeContainer={changeContainer} />
+                                            <PlacementContainer
+                                                selectedSampleList={selectedSamples}
+                                                containerType={SOURCE_STRING}
+                                                columns={sourceSamples.columns}
+                                                rows={sourceSamples.rows}
+                                                samples={sourceSamples.samples}
+                                                updateSamples={updateSamples} />
+                                        </>
+                                        :
+                                        <Col span={12}>
+                                            <div className={"flex-row"}>
+                                                <div className={"flex-column"} />
+                                            </div>
+                                        </Col>
+                                    }
+                                </div>
+                            </div>
+                        </Col>
+                        {sourceSamples && destinationSamples ?
+                            <Col span={12}>
+                                <div className={"flex-row"}>
+                                    <div className={"flex-column"}>
+                                        <ContainerNameScroller
+                                            disabled={disableChangeDestination}
+                                            containerType={DESTINATION_STRING}
+                                            name={destinationSamples.container_name}
+                                            changeContainer={changeContainer} />
+                                        <PlacementContainer
+                                            selectedSampleList={selectedSamples}
+                                            containerType={DESTINATION_STRING}
+                                            columns={destinationSamples.columns}
+                                            rows={destinationSamples.rows}
+                                            samples={destinationSamples.samples}
+                                            updateSamples={updateSamples}
+                                            direction={placementType ? placementDirection : undefined}
+                                            pattern={!placementType} />
+                                    </div>
+                                </div>
+                            </Col>
+                            : <Col span={12}>
+                                <div className={"flex-row"}>
+                                    <div className={"flex-column"} />
+                                </div>
+                            </Col>
+                        }
+                    </Row>
+                    <Row justify="end" style={{ padding: "10px" }}>
+                        <Col span={3}>
+                            <Switch checkedChildren="Pattern" unCheckedChildren="Group" checked={placementType} onChange={updateGroupPlacement}></Switch>
+                        </Col>
+                        <Col span={5}>
+                            <Radio.Group
+                                disabled={!placementType}
+                                value={placementDirection}
+                                onChange={evt => updatePlacementDirection(evt.target.value)}>
+                                <Radio.Button value={'row'}> row </Radio.Button>
+                                <Radio.Button value={'column'}> column </Radio.Button>
+                            </Radio.Group>
+                        </Col>
+                        <Col span={8}>
+                            <Button onClick={transferAllSamples} disabled={!destinationSamples}>Place All Source</Button>
+                            <Button onClick={clearSelection}>Deselect All</Button>
+                            <Popconfirm
+                                title={`Are you sure you want to undo selected samples? If there are no selected samples, it will undo all placements.`}
+                                onConfirm={removeSelectedCells}
+                                placement={'bottomRight'}
+                                disabled={disableUndo}
+                            >
+                                <Button disabled={disableUndo} > Undo Placement</Button>
+                            </Popconfirm>
+                        </Col>
+                    </Row>
+                    <Row justify="space-evenly" style={{ padding: "10px" }}>
+                        <Col span={10}>
+                            <PlacementSamplesTable onSampleSelect={(samples) => onSampleTableSelect(samples, SOURCE_STRING)} samples={filterPlacedSamples(sourceSamples ? sourceSamples.samples : {})} selectedSamples={filterSelectedSamples(SOURCE_STRING)} />
+                        </Col>
+                        <Col span={10}>
+                            <PlacementSamplesTable onSampleSelect={(samples) => onSampleTableSelect(samples, DESTINATION_STRING)} samples={filterPlacedSamples(destinationSamples ? destinationSamples.samples : {})} selectedSamples={filterSelectedSamples(DESTINATION_STRING)} />
+                        </Col>
+                    </Row>
+                </PageContent>
+            </PageContainer>
+        </>
+    )
 }
 export default Placement
+
