d0bd28ea2d537bbab7e5fc2224c70cb666e698db
Nafiz Islam <nafiz.islam@computationalgenomics.ca>
2024-04-17 09:49:48 -0400

    fix prefill

--- backend/fms_core/viewsets/_utils.py
+++ backend/fms_core/viewsets/_utils.py
@@ -282,15 +282,6 @@ class TemplatePrefillsMixin:
             return HttpResponseBadRequest(json.dumps({"detail": f"Template {template_id} not found"}), content_type="application/json")
 
         queryset = self.filter_queryset(self.get_queryset())
-
-        sample__id__in = request.POST.get("sample__id__in")
-        if sample__id__in:
-            if isinstance(sample__id__in, int):
-                sample__id__in = [sample__id__in]
-            elif isinstance(sample__id__in, str):
-                sample__id__in = [s.strip() for s in sample__id__in.split(",")]
-            queryset = queryset.filter(sample__id__in=sample__id__in)
-
         try:
             filename = "/".join(template["identity"]["file"].split("/")[-2:]) # Remove the /static/ from the served path to search for local path 
             template_path = os.path.join(settings.STATIC_ROOT, filename)
@@ -331,6 +322,15 @@ class TemplatePrefillsWithDictMixin(TemplatePrefillsMixin):
             return HttpResponseBadRequest(json.dumps({"detail": f"Template {template_id} not found"}), content_type="application/json")
 
         queryset = self.filter_queryset(self.get_queryset())
+        sample__id__in = request.POST.get("sample__id__in")
+        if sample__id__in:
+            if isinstance(sample__id__in, int):
+                sample__id__in = [sample__id__in]
+            elif isinstance(sample__id__in, str):
+                sample__id__in = [s.strip() for s in sample__id__in.split(",")]
+            queryset = queryset.filter(sample__id__in=sample__id__in)
+
+
         if not user_prefill_data and not template["prefill info"]:
             return HttpResponseBadRequest(json.dumps({"detail": f"No prefilling available for current template."}), content_type="application/json")
         else:
