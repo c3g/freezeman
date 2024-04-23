20108c5736523e8607974d0160e3c4cdc2b46bce
Nafiz Islam <nafiz.islam1001@gmail.com>
2024-04-23 14:31:35 -0400

    use fetchSamplesAndLibraries

--- frontend/src/hooks.ts
+++ frontend/src/hooks.ts
@@ -1,10 +1,9 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 import type { RootState, AppDispatch } from './store'
 import { FMSId } from './models/fms_api_models'
-import { useEffect, useMemo, useState } from 'react'
-import { fetchLibrariesForSamples, fetchSamples } from './modules/cache/cache'
-import { selectLibrariesByID, selectSamplesByID } from './selectors'
+import { useEffect, useState } from 'react'
 import { SampleAndLibrary } from './components/WorkflowSamplesTable/ColumnSets'
+import { fetchSamplesAndLibraries } from './modules/studySamples/services'
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch: () => AppDispatch = useDispatch
@@ -13,32 +12,15 @@ export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
 export function useSampleAndLibraryList(sampleIDs: FMSId[], offset?: number, pageSize?: number) {
 	const [isFetching, setIsFetching] = useState(false)
     const endIndex = pageSize !== undefined ? (offset ?? 0) + pageSize : undefined
+    const [availableSamples, setAvailableSamples] = useState<SampleAndLibrary[]>([])
 
 	useEffect(() => {
 		(async () => {
 			setIsFetching(true)
-			const samples = await fetchSamples(sampleIDs.slice(offset, endIndex))
-			const samplesWithLibraries = samples.filter((s) => s.is_library).map((s) => s.id)
-			await fetchLibrariesForSamples(samplesWithLibraries)
+			setAvailableSamples(await fetchSamplesAndLibraries(sampleIDs.slice(offset, endIndex)))
 			setIsFetching(false)
 		})()
 	}, [sampleIDs, offset, endIndex, pageSize])
-
-	const samplesByID = useAppSelector(selectSamplesByID)
-	const librariesByID = useAppSelector(selectLibrariesByID)
-
-	const availableSamples = useMemo(() => sampleIDs.slice(offset, endIndex).reduce((acc, sampleID) => {
-		const sample = samplesByID[sampleID]
-		if (sample) {
-			if (sample.is_library) {
-				const library = librariesByID[sampleID]
-				acc.push({ sample, library })
-			} else {
-				acc.push({ sample })
-			}
-		}
-		return acc
-	}, [] as SampleAndLibrary[]), [sampleIDs, samplesByID, librariesByID, offset, endIndex])
 
 	return [availableSamples, isFetching] as const
 }
