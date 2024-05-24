86d0e9bb54efaebb1ebd125a9387142ce2a00b34
Nafiz Islam <nafiz.islam1001@gmail.com>
2024-05-23 21:16:00 -0400

    Fix Placement (#798) (#801)
    * set default placement type to pattern
    
    * change sorting for placement samples table
    
    * more tweaks
    
    * fix sorting
    
    * reverse comparison
    
    * default to column direction
    
    * correct comment
    
    * allow wrap around
    
    * use reverse of offsets only
    
    * use enum index

--- frontend/src/components/placementVisuals/PlacementSamplesTable.tsx
+++ frontend/src/components/placementVisuals/PlacementSamplesTable.tsx
@@ -10,6 +10,7 @@ import { fetchSamples } from "../../modules/cache/cache";
 import { selectCell, selectContainer } from "../../modules/placement/selectors";
 import { selectActiveDestinationContainer, selectActiveSourceContainer } from "../../modules/labworkSteps/selectors";
 import store from "../../store";
+import { compareArray, coordinatesToOffsets, offsetsToCoordinates } from "../../utils/functions";
 export interface PlacementSamplesTableProps {
     container: string | null
 }
@@ -80,7 +81,7 @@ const PlacementSamplesTable = ({ container: containerName }: PlacementSamplesTab
                         sample,
                         selected: cell.selected,
                         name,
-                        coordinates: cell.coordinates
+                        coordinates: cell.coordinates,
                     })
                     return samples
                 }, [] as Samples)
@@ -132,17 +133,24 @@ const PlacementSamplesTable = ({ container: containerName }: PlacementSamplesTab
         const sortedSamples = [...samples]
         sortedSamples.sort((a, b) => {
 	    const MAX = 100
-            const HALF = MAX/2
-	    const QUARTER = HALF/2
 
             let orderA = MAX
             let orderB = MAX
-            if (a.selected) orderA -= HALF
-            if (b.selected) orderB -= HALF
-            if (a.sample && b.sample) {
-                if (a.sample > b.sample) orderB -= QUARTER
-                if (a.sample < b.sample) orderA -= QUARTER
+
+            if (a.selected) orderA -= MAX/2
+            if (b.selected) orderB -= MAX/2
+
+            if (container) {
+                const aOffsets = a.coordinates ? coordinatesToOffsets(container.spec, a.coordinates) : []
+                const bOffsets = b.coordinates ? coordinatesToOffsets(container.spec, b.coordinates) : []
+                const arrayComparison = compareArray(aOffsets.reverse(), bOffsets.reverse())
+                if (arrayComparison > 0) orderB -= MAX/4
+                if (arrayComparison < 0) orderA -= MAX/4
             }
+
+            if (a.sample > b.sample) orderB -= MAX/8
+            if (a.sample < b.sample) orderA -= MAX/8
+
             return orderA - orderB
         })
         return sortedSamples.reduce((sortedSamples, s) => {
@@ -156,7 +164,7 @@ const PlacementSamplesTable = ({ container: containerName }: PlacementSamplesTab
             }
             return sortedSamples
         }, [] as SortedSample[])
-    }, [samples])
+    }, [samples, container])
 
     const selectionProps: TableRowSelection<SortedSample> = {
         selectedRowKeys: placementSelectedSamples,
