3621005f972b3c350087b9ad0e185f8e85ab9515
Nafiz Islam <nafiz.islam@computationalgenomics.ca>
2024-07-22 13:25:40 -0400

    N/A to undefined

--- frontend/src/components/placementVisuals/AddPlacementContainer.tsx
+++ frontend/src/components/placementVisuals/AddPlacementContainer.tsx
@@ -74,9 +74,9 @@ const AddPlacementContainer = ({ onConfirm, existingContainers }: AddPlacementCo
             loadedSamples.forEach(sample => {
                 newDestination[sample.id] = {
                     id: sample.id,
-                    coordinates: sample.coordinate ? coordinates[sample.coordinate]?.name ?? '...' : 'N/A',
+                    coordinates: sample.coordinate ? coordinates[sample.coordinate]?.name ?? '...' : 'undefined',
                     name: sample.name,
-                    project: sample.project ? projects[sample.project]?.name ?? '...' : 'N/A'
+                    project: sample.project ? projects[sample.project]?.name ?? '...' : 'undefined'
                 }
             })
         }
