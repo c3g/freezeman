77a297122f89f15417e5406688c99a0d923483c4
Nafiz Islam <nafiz.islam@computationalgenomics.ca>
2024-09-09 13:53:27 -0400

    tweak service test

--- backend/fms_core/tests/test_services/sample_add_samples_to_study_test.py
+++ backend/fms_core/tests/test_services/sample_add_samples_to_study_test.py
@@ -14,8 +14,10 @@ class ProjectAddSamplesToStudyTestCase(TestCase):
         valid_target_end_date = "2030-12-21"
         sk_DNA, _ = SampleKind.objects.get_or_create(name="DNA", is_extracted=True, concentration_required=True)
         workflow = Workflow.objects.get(name="PCR-free Illumina")
+
         self.qc_step = 2
         self.normalization_step = 3
+        self.experiment_run_illumina_step = 8
 
         # project 1 models
         container1 = Container.objects.create(barcode="TESTBARCODE1",
@@ -35,13 +37,13 @@ class ProjectAddSamplesToStudyTestCase(TestCase):
         self.study_project1_A = Study.objects.create(letter='A',
                                           project=self.project1,
                                           workflow=workflow,
-                                          start=2,
-                                          end=3)
+                                          start=self.qc_step,
+                                          end=self.experiment_run_illumina_step)
         self.study_project1_B = Study.objects.create(letter='B',
                                             project=self.project1,
                                             workflow=workflow,
-                                            start=2,
-                                            end=3)
+                                            start=self.qc_step,
+                                            end=self.experiment_run_illumina_step)
 
         # project 2 models
         self.project2, _, _ = create_project(name="Project2",
@@ -51,8 +53,9 @@ class ProjectAddSamplesToStudyTestCase(TestCase):
         self.study_project2_A = Study.objects.create(letter='A',
                                           project=self.project2,
                                           workflow=workflow,
-                                          start=2,
-                                          end=3)
+                                          start=self.qc_step,
+                                          end=self.experiment_run_illumina_step)
+
 
         # no project models
         test_container_no_project = Container.objects.create(barcode="TESTBARCODE3",
@@ -87,6 +90,29 @@ class ProjectAddSamplesToStudyTestCase(TestCase):
         self.assertEqual(errors.get("add_sample_to_study"), [f"Sample [{self.sample_project1.name}] is not linked to project [{self.project2.name}]."])
         self.assertEqual(dict(warnings), {})
     
+    def test_sample_cannot_queue_to_non_existent_study(self):
+        non_existent_study_letter = "Z"
+        errors, warnings = add_sample_to_study(self.sample_project1, self.project1, non_existent_study_letter)
+        self.assertEqual(errors.get("get_study"), [f"Could not find a study for project {self.project1.name} and letter '{non_existent_study_letter}'"])
+        self.assertEqual(errors.get("add_sample_to_study"), [f"Specified study [{non_existent_study_letter}] doesn't exist for project [{self.project1.name}]."])
+        self.assertEqual(dict(warnings), {})
+
+    def test_sample_cannot_queue_to_non_existent_step(self):
+        non_existent_step = 1 # extraction step
+        errors, warnings = add_sample_to_study(self.sample_project1, self.project1, self.study_project1_A.letter, non_existent_step)
+        breakpoint()
+        self.assertEqual(errors.get("queue_sample_to_study_workflow"), [
+            f"Order must be a positive integer between {self.study_project1_A.start} and {self.study_project1_A.end}."
+        ])
+        self.assertEqual(dict(warnings), {})
+
+    def test_cannot_queue_to_invalid_step(self):
+        errors, warnings = add_sample_to_study(self.sample_project1, self.project1, self.study_project1_A.letter, self.experiment_run_illumina_step)
+        self.assertEqual(errors.get("queue_sample_to_study_workflow"), [
+            f"Step Experiment Run Illumina of study {self.study_project1_A.letter} expected Library but Sample {self.sample_project1.name} does not match that sample type."
+        ])
+        self.assertEqual(dict(warnings), {})
+
     def test_sample_cannot_queue_to_the_same_step_twice(self):
         errors, warnings = add_sample_to_study(self.sample_project1, self.project1, self.study_project1_A.letter, self.qc_step)
         errors, warnings = add_sample_to_study(self.sample_project1, self.project1, self.study_project1_A.letter, self.qc_step)
