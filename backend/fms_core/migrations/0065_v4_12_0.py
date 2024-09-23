b546373d87e6f439336b7c849d51b0a09a294496
sebastian <sebastian.amouzegar@computationalgenomics.ca>
2024-09-23 16:37:31 -0400

    base migration file to add for rm2283

--- backend/fms_core/migrations/0065_v4_12_0.py
+++ backend/fms_core/migrations/0065_v4_12_0.py
@@ -1,43 +1,50 @@
-import reversion
+from django.conf import settings
 from django.db import migrations
 from django.contrib.auth.models import User
+import reversion
 
 ADMIN_USERNAME = 'biobankadmin'
 
-def set_measured_volume_properties_optional(apps, schema_editor):
-    PROPERTY_TYPE_NAME = "Measured Volume (uL)"
-    PROTOCOL_NAMES = ["Sample Quality Control", "Library Quality Control"]
-
-    PropertyType = apps.get_model("fms_core", "PropertyType")
+# adding the following steps
+# important !! the column name will change because it doesn't exist in SAMPLE_QC_TEMPLATE
+# column sample kind to be added to template
+def add_sample_qc_distinction_dna_rna(apps, schema_editor):
+    Step = apps.get_model("fms_core", "Step")
     Protocol = apps.get_model("fms_core", "Protocol")
-    PropertyType = apps.get_model("fms_core", "PropertyType")
-    ContentType = apps.get_model('contenttypes', 'ContentType')
+    StepSpecification = apps.get_model("fms_core", "StepSpecification")
 
-    protocol_content_type = ContentType.objects.get_for_model(Protocol)
+    STEPS = [
+        {"name": "Sample QC (DNA)", "protocol_name": "Sample QC", "specifications": [
+            {"display_name": "Sample QC Type", "sheet_name": "SampleQC", "column_name": "Sample Kind", "value": "DNA"}]
+        },
+        {"name": "Sample QC (RNA)", "protocol_name": "Sample QC", "specifications": [
+            {"display_name": "Sample QC Type", "sheet_name": "SampleQC", "column_name": "Sample Kind", "value": "RNA"}]
+        }
+    ]
 
     with reversion.create_revision(manage_manually=True):
         admin_user = User.objects.get(username=ADMIN_USERNAME)
-
-        reversion.set_comment(f"Set '{PROPERTY_TYPE_NAME}' property types as optional.")
+        admin_user_id = admin_user.id
+        reversion.set_comment(f"Create the basic initial workflows.")
         reversion.set_user(admin_user)
-
-        for protocol_name in PROTOCOL_NAMES:
-            object_id = Protocol.objects.get(name=protocol_name).id
-            property_type = PropertyType.objects.get(name=PROPERTY_TYPE_NAME, object_id=object_id, content_type=protocol_content_type)
-            property_type.is_optional = True
-            property_type.save()
-            reversion.add_to_revision(property_type)
-
+        for step_info in STEPS:
+            protocol = Protocol.objects.get(name=step_info["protocol_name"])
+            step = Step.objects.create(name=step_info["name"],
+                                       protocol=protocol,
+                                       created_by_id=admin_user_id,
+                                       updated_by_id=admin_user_id)
+            reversion.add_to_revision(step)
+            for specification in step_info["specifications"]:
+                step_specification = StepSpecification.objects.create(display_name=specification["display_name"],
+                                                                      sheet_name=specification["sheet_name"],
+                                                                      column_name=specification["column_name"],
+                                                                      value=specification["value"],
+                                                                      step=step,
+                                                                      created_by_id=admin_user_id,
+                                                                      updated_by_id=admin_user_id)
+                reversion.add_to_revision(step_specification)
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('fms_core', '0064_v4_11_0'),
-    ]
-
-    operations = [
-        migrations.RunPython(
-            set_measured_volume_properties_optional,
-            reverse_code=migrations.RunPython.noop,
-        ),
-    ]
+    dependencies = [migrations.swappable_dependency(settings.AUTH_USER_MODEL),('fms_core', '0064_v4_11_0')]
+    operations = [migrations.RunPython(add_sample_qc_distinction_dna_rna,reverse_code=migrations.RunPython.noop)]
