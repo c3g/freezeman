423357f00337d1d94753c1a62a3994bc149bd3ed
sebastian <sebastian.amouzegar@computationalgenomics.ca>
2024-09-23 16:37:31 -0400

    base migration file to add for rm2283

--- backend/fms_core/migrations/0065_v4_12_0.py
+++ backend/fms_core/migrations/0065_v4_12_0.py
@@ -4,6 +4,42 @@ from django.contrib.auth.models import User
 
 ADMIN_USERNAME = 'biobankadmin'
 
+def add_sample_qc_distinction_dna_rna(apps, schema_editor):
+    Step = apps.get_model("fms_core", "Step")
+    Protocol = apps.get_model("fms_core", "Protocol")
+    StepSpecification = apps.get_model("fms_core", "StepSpecification")
+
+    STEPS = [
+        {"name": "Sample QC (DNA)", "protocol_name": "Sample QC", "specifications": [
+            {"display_name": "Sample QC Type", "sheet_name": "SampleQC", "column_name": "Sample Kind", "value": "DNA"}]
+        },
+        {"name": "Sample QC (RNA)", "protocol_name": "Sample QC", "specifications": [
+            {"display_name": "Sample QC Type", "sheet_name": "SampleQC", "column_name": "Sample Kind", "value": "RNA"}]
+        }
+    ]
+
+    with reversion.create_revision(manage_manually=True):
+        admin_user = User.objects.get(username=ADMIN_USERNAME)
+        admin_user_id = admin_user.id
+        reversion.set_comment(f"Create the basic initial workflows.")
+        reversion.set_user(admin_user)
+        for step_info in STEPS:
+            protocol = Protocol.objects.get(name=step_info["protocol_name"])
+            step = Step.objects.create(name=step_info["name"],
+                                       protocol=protocol,
+                                       created_by_id=admin_user_id,
+                                       updated_by_id=admin_user_id)
+            reversion.add_to_revision(step)
+            for specification in step_info["specifications"]:
+                step_specification = StepSpecification.objects.create(display_name=specification["display_name"],
+                                                                      sheet_name=specification["sheet_name"],
+                                                                      column_name=specification["column_name"],
+                                                                      value=specification["value"],
+                                                                      step=step,
+                                                                      created_by_id=admin_user_id,
+                                                                      updated_by_id=admin_user_id)
+                reversion.add_to_revision(step_specification)
+
 def set_measured_volume_properties_optional(apps, schema_editor):
     PROPERTY_TYPE_NAME = "Measured Volume (uL)"
     PROTOCOL_NAMES = ["Sample Quality Control", "Library Quality Control"]
@@ -63,6 +99,10 @@ class Migration(migrations.Migration):
         ),
         migrations.RunPython(
             remove_serial_number_from_some_instruments,
+            reverse_code=migrations.RunPython.noop,
+        ),
+        migrations.RunPython(
+            add_sample_qc_distinction_dna_rna,
             reverse_code=migrations.RunPython.noop,
         ),
     ]
