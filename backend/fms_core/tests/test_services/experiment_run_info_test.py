dd8f02ae8f8a3c04ed3c77470f9a7de607d5534a
ufgauthi <ulysse.fortiergauthier@mcgill.ca>
2024-10-21 16:43:56 -0400

    Updated test and test templates.

--- backend/fms_core/tests/test_services/experiment_run_info_test.py
+++ backend/fms_core/tests/test_services/experiment_run_info_test.py
@@ -36,7 +36,7 @@ class ExperimentRunInfoTemplatesTestCase(TestCase):
 
         self.import_template(IndexCreationImporter(), 'Index_creation_v3_7_0.xlsx')
 
-        self.import_template(SampleSubmissionImporter(), 'Sample_submission_v4_5_0.xlsx')
+        self.import_template(SampleSubmissionImporter(), 'Sample_submission_v4_12_0.xlsx')
         
         self.import_template(LibraryPreparationImporter(), 'Library_preparation_v4_4_0.xlsx')
 
