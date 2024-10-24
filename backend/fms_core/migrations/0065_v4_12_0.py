8eb9cd3598ce4da58aa73e51489e404b21a384a7
ufgauthi <ulysse.fortiergauthier@mcgill.ca>
2024-10-24 17:55:11 -0400

    Removed duplicated definition in migration during rebasing.

--- backend/fms_core/migrations/0065_v4_12_0.py
+++ backend/fms_core/migrations/0065_v4_12_0.py
@@ -4,40 +4,14 @@ from django.contrib.auth.models import User
 
 ADMIN_USERNAME = 'biobankadmin'
 
-def set_measured_volume_properties_optional(apps, schema_editor):
-    PROPERTY_TYPE_NAME = "Measured Volume (uL)"
-    PROTOCOL_NAMES = ["Sample Quality Control", "Library Quality Control"]
-
-    PropertyType = apps.get_model("fms_core", "PropertyType")
-    Protocol = apps.get_model("fms_core", "Protocol")
-    PropertyType = apps.get_model("fms_core", "PropertyType")
-    ContentType = apps.get_model('contenttypes', 'ContentType')
-
-    protocol_content_type = ContentType.objects.get_for_model(Protocol)
-
-    with reversion.create_revision(manage_manually=True):
-        admin_user = User.objects.get(username=ADMIN_USERNAME)
-
-        reversion.set_comment(f"Set '{PROPERTY_TYPE_NAME}' property types as optional.")
-        reversion.set_user(admin_user)
-
-        for protocol_name in PROTOCOL_NAMES:
-            object_id = Protocol.objects.get(name=protocol_name).id
-            property_type = PropertyType.objects.get(name=PROPERTY_TYPE_NAME, object_id=object_id, content_type=protocol_content_type)
-            property_type.is_optional = True
-            property_type.save()
-            reversion.add_to_revision(property_type)
-
 def add_sample_qc_distinction_dna_rna(apps, schema_editor):
     # load the new sample qc steps and other necessary assets
     STEPS = [
         {"name": "Sample QC (DNA)", "protocol_name": "Sample Quality Control","expected_sample_type": "EXTRACTED_SAMPLE",
-          "specifications": [
-            {"display_name": "SampleQcType", "sheet_name": "SampleQC", "column_name": "Sample Kind", "value": "DNA"}]
+          "specifications": [{"display_name": "SampleQcType", "sheet_name": "SampleQC", "column_name": "Sample Kind", "value": "DNA"}]
         },
         {"name": "Sample QC (RNA)", "protocol_name": "Sample Quality Control","expected_sample_type": "EXTRACTED_SAMPLE",
-          "specifications": [
-            {"display_name": "SampleQcType", "sheet_name": "SampleQC", "column_name": "Sample Kind", "value": "RNA"}]
+         "specifications": [{"display_name": "SampleQcType", "sheet_name": "SampleQC", "column_name": "Sample Kind", "value": "RNA"}]
         }
     ]
     StepOrder = apps.get_model("fms_core", "StepOrder")
@@ -112,7 +86,6 @@ def remove_serial_number_from_some_instruments(apps, schema_editor):
             instrument.save()
             reversion.add_to_revision(instrument)
 
-
 def set_measured_volume_properties_optional(apps, schema_editor):
     PROPERTY_TYPE_NAME = "Measured Volume (uL)"
     PROTOCOL_NAMES = ["Sample Quality Control", "Library Quality Control"]
@@ -167,8 +140,8 @@ class Migration(migrations.Migration):
             reverse_code=migrations.RunPython.noop,
         ),
         migrations.RunPython(
-          add_sample_qc_distinction_dna_rna,
-          reverse_code=migrations.RunPython.noop
+            add_sample_qc_distinction_dna_rna,
+            reverse_code=migrations.RunPython.noop
         ),
         migrations.RemoveField(
             model_name='samplekind',
