31d3fefb5dba772d3f00d865c4cbeb41eec3d6f8
Nafiz Islam <nafiz.islam@computationalgenomics.ca>
2024-10-29 09:19:49 -0400

    fix Menu overlay error in ExportDropdown

--- frontend/src/components/ExportDropdown.js
+++ frontend/src/components/ExportDropdown.js
@@ -1,29 +1,28 @@
 import React from "react";
-import {Link} from "react-router-dom";
 
 import ExportButton from "../components/ExportButton";
 
-import {Button, Menu, Dropdown, Modal} from "antd";
-import {ExportOutlined, DownloadOutlined} from "@ant-design/icons";
+import { Button, Dropdown } from "antd";
+import { DownloadOutlined } from "@ant-design/icons";
 
-const ExportDropdown = ({listExport, listExportMetadata, itemsCount}) => {    
+const ExportDropdown = ({ listExport, listExportMetadata, itemsCount }) => {
 
-    const exportMenu = (
-    <Menu>
-        <Menu.Item>
-            <ExportButton style={{border:0}} key='export' exportType="samples" exportFunction={listExport} filename="sample_list" itemsCount={itemsCount}/>
-        </Menu.Item>
-        <Menu.Item>
-            <ExportButton style={{border:0}} key='export-metadata' exportType="metadata" exportFunction={listExportMetadata} filename="sample_metadata" itemsCount={itemsCount}/>
-        </Menu.Item>
-    </Menu>
-  ) ;
-
-  return <Dropdown overlay={exportMenu} placement="bottomRight">
-           <Button>
-             <DownloadOutlined /> Export
-           </Button>
-         </Dropdown>
+    return <Dropdown menu={{
+        items: [
+            {
+                key: 'export',
+                label: <ExportButton style={{ border: 0 }} exportType="samples" exportFunction={listExport} filename="sample_list" itemsCount={itemsCount} />,
+            },
+            {
+                key: 'export-metadata',
+                label: <ExportButton style={{ border: 0 }} exportType="metadata" exportFunction={listExportMetadata} filename="sample_metadata" itemsCount={itemsCount} />,
+            }
+        ]
+    }} placement="bottomRight">
+        <Button>
+            <DownloadOutlined /> Export
+        </Button>
+    </Dropdown>
 }
 
 export default ExportDropdown;
