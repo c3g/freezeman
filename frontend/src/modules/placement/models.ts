33cdcacea4703b3d12316641ef1f1376ee5bf5ed
Nafiz Islam <nafiz.islam@computationalgenomics.ca>
2024-11-01 15:29:54 -0400

    wip

--- frontend/src/modules/placement/models.ts
+++ frontend/src/modules/placement/models.ts
@@ -1,5 +1,13 @@
 import { CoordinateSpec } from "../../models/fms_api_models"
-import { Sample } from "../../models/frontend_models"
+import { Container, Sample } from "../../models/frontend_models"
+
+export interface PlacementState {
+    samples: Record<Sample['id'], ContainerIdentifier>
+    containers: Record<Container['name'], ContainerState>
+    placementType: PlacementOptions['type']
+    placementDirection: PlacementGroupOptions['direction']
+    error: string | null
+}
 
 export enum PlacementType {
     PATTERN,
@@ -18,52 +26,57 @@ export interface PlacementGroupOptions {
 }
 export type PlacementOptions = PlacementPatternOptions | PlacementGroupOptions
 
-export type CellState = CellWithParentState | CellWithoutParentState
-export type ContainerState = ParentContainerState | TubesWithoutParentState
-export interface PlacementState {
-    containers: ContainerState[]
-    placementType: PlacementOptions['type']
-    placementDirection: PlacementGroupOptions['direction']
-    error?: string
+export interface SamplePlacement {
+    name: Sample['name']
+    id: Sample['id']
 }
+
+/**
+ * Each cell can contain more than one sample.
+ * The amount of each sample is proportional to capacity of the cell/container and the amount of that sample in the cell.
+ * If the cell already contains a sample, by default it would be amounts[sample.id] = 1, totalAmount = 1.
+ * If the sample is completely distributed among 5 cells, then amounts[sample.id] = 0, totalAmount = 5.
+ */
 
 interface CellStateBase {
-    selected: boolean
-    sample: Sample['id'] | null
-    name: string
-    projectName: string
-    placedAt: null | CellWithParentIdentifier
-}
-export interface CellWithParentState extends CellStateBase {
-    readonly parentContainerName: string
-    readonly coordinates: string
-    placedFrom: null | CellIdentifier
+    readonly parentContainerName: Container['name']
+    readonly container: Container['name']
+    amounts: Record<Sample['id'], number> // numerators (amount of each sample which sums to totalAmount)
+    totalAmount: number // denominator (the sum of amount of samples allowed)
     preview: boolean
 }
-export interface CellWithoutParentState extends CellStateBase {
-    readonly parentContainerName: null
-    readonly coordinates?: undefined
-    placedFrom?: null
-    preview?: false
+export interface CellStateWithoutParent extends CellStateBase {
+    readonly coordinates: null
 }
-
-export type CellWithParentIdentifier = Pick<CellWithParentState, 'parentContainerName' | 'coordinates'> & { sample?: CellWithoutParentState['sample'] } // sample is just to stop typescript from whining
-export type CellWithoutParentIdentifier = Pick<CellWithoutParentState, 'parentContainerName' | 'coordinates' | 'sample'>
-export type CellIdentifier = CellWithParentIdentifier | CellWithoutParentIdentifier
-
-interface BaseParentContainerState {
-    cellsIndexBySampleID: Record<Sample['id'], number>
+export interface CellStateWithParent extends CellStateBase {
+    readonly coordinates: string
 }
-export interface ParentContainerState extends BaseParentContainerState {
-    readonly name: string
+export type CellState = CellStateWithoutParent | CellStateWithParent
+
+interface BaseContainerState {
+    cells: CellState[]
+}
+export interface TubesWithoutParent extends BaseContainerState {
+    readonly containerName: null
+    readonly spec: null
+    cellsIndexByCoordinates: null
+}
+export interface ParentContainerState extends BaseContainerState {
+    readonly containerName: Container['name']
     readonly spec: CoordinateSpec
-    cells: CellWithParentState[]
-    readonly cellsIndexByCoordinates: Record<string, number> // this should never change after it's initialized
+    readonly cellsIndexByCoordinates: Record<CellStateWithParent['coordinates'], number>
 }
-export interface TubesWithoutParentState extends BaseParentContainerState {
-    readonly name: null
-    readonly spec: []
-    cells: CellWithoutParentState[]
-}
-export type ContainerIdentifier = Pick<ContainerState, 'name'>
+export type ContainerState = TubesWithoutParent | ParentContainerState
 
+export type ContainerIdentifier = {
+    containerName: Container['name'],
+} | {
+    containerName: null,
+}
+export type CellIdentifier = {
+    containerName: Container['name'],
+    coordinates: string
+} | {
+    containerName: null,
+    coordinates: null,
+}
