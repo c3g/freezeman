4ddeac8e76d1f7919d398186a22f5f57e727db9c
Nafiz Islam <nafiz.islam@computationalgenomics.ca>
2024-11-06 14:55:39 -0500

    do not abort by default

--- frontend/src/modules/samples/actions.ts
+++ frontend/src/modules/samples/actions.ts
@@ -36,7 +36,7 @@ export const update = (id, sample) => async (dispatch, getState) => {
         UPDATE, api.samples.update(sample), { meta: { id, ignoreError: 'APIError' }}));
 };
 
-export const list = (options, abort = true) => async (dispatch: AppDispatch, getState) => {
+export const list = (options, abort = false) => async (dispatch: AppDispatch, getState) => {
     const params = { limit: 100000, ...options }
     return await dispatch(networkAction(LIST,
         api.samples.list(params, { abort }),
