a89ecbd613942a0aff37621a813fef8b703f7f90
ufgauthi <ulysse.fortiergauthier@mcgill.ca>
2024-11-13 17:26:08 -0500

    Started report service. WIP.

--- backend/fms_core/viewsets/report.py
+++ backend/fms_core/viewsets/report.py
@@ -6,39 +6,38 @@ from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
 from django.http import QueryDict
 
+from ..services.report import AVAILABLE_REPORTS, REPORT_INFORMATION, get_report
+
 class ReportViewSet(viewsets.ViewSet):
     permission_classes = [IsAuthenticated]
-
-    AVAILABLE_REPORTS = ["production_report"]
-
-    REPORT_INFORMATION = {
-        "production_report": {
-            "groups": ["library_type","project","taxon","technology"],  # Add technology_detailed once the rest is working
-            "time_aggregation": ["daily", "weekly", "monthly"],
-        },
-    }
 
     def list(self, request):
         """
             Provides the list of existing reports by name.
         """
-        return Response(self.AVAILABLE_REPORTS)
+        return Response(AVAILABLE_REPORTS)
     
-    @action(details=False, methods=["get"])
+    @action(detail=False, methods=["get"])
     def report(self, request):
         """
             Produce a report with given parameters or provide guidance for the required parameters.
         """
         params = QueryDict(request.META.get("QUERY_STRING"))
         name = params.get("name", None)
-        group_list = params.get("group_by")
+        grouped_by = params.getlist("group_by", [])
+        time_aggregation = params.get("time_aggregation", None)
         start_date = params.get("start_date", None)
         end_date = params.get("end_date", None)
 
         if name is not None:
             if start_date is None or end_date is None:
                 # Provide information about the requested report if no start_date and end_date provided
-                report_information = self.REPORT_INFORMATION.get(name, f"Requested report does not exist.")
+                report_information = REPORT_INFORMATION.get(name, f"Requested report does not exist.")
                 return Response(report_information)
             
-            
+            report_data, errors, warnings = get_report(name=name,
+                                                       grouped_by=grouped_by,
+                                                       time_aggregation=time_aggregation,
+                                                       start_date=start_date,
+                                                       end_date=end_date)
+            return Response(report_data)
