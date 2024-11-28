0e4b338b0cad55123ce7b8726b8fa30ea6b29273
ufgauthi <ulysse.fortiergauthier@mcgill.ca>
2024-11-28 16:39:31 -0500

    Made more generic. Extracted detailed report.

--- backend/fms_core/services/report.py
+++ backend/fms_core/services/report.py
@@ -1,54 +1,45 @@
 from typing import List
 import datetime
 from django.apps import apps
-from django.db.models import Q, F, When, Case, BooleanField, Prefetch, Count, Subquery, OuterRef, Sum, Max
+from django.db.models import F, Count,  Sum, Max, Min, TextChoices, functions
 
-from ..models import ExperimentRun, Process
 from fms_report.models import Report, MetricField
+from fms_report.models._constants import AggregationType
 
-AVAILABLE_REPORTS = ["production_report"]
+class TimeWindow(TextChoices):
+    MONTHLY = "month", "Monthly"
+    WEEKLY = "week", "Weekly"
+    DAILY = "day", "Daily"
 
-REPORT_INFORMATION = {
-    "production_report": {
-        "aggregated_metrics": ["biosample", "library", "reads", "bases"],
-        "groups": ["library_type","project","taxon","technology"],  # Add technology_detailed once the rest is working
-        "time_aggregation": ["daily", "weekly", "monthly"],
-    },
-}
+def list_reports():
+    return Report.objects.values_list("name", flat=True)
 
-def get_report(name: str, grouped_by: List[str], time_aggregation: str, start_date: str, end_date: str):
+def list_report_information(name: str):
+    queryset = MetricField.objects.filter(report__name=name).all()
+    groups = [field.name for field in queryset if field.is_group]
+    time_windows = [TimeWindow.DAILY.label, TimeWindow.WEEKLY.label, TimeWindow.MONTHLY.label]
+    report_info = {"name": name,
+                   "groups": groups,
+                   "time_windows": time_windows}
+    return report_info
+
+def get_report(name: str, grouped_by: List[str], time_window: str, start_date: str, end_date: str):
     report_data = {}
     errors = {}
     warnings = {}
     is_detailed_report = (len(grouped_by) == 0)
-    headers = grouped_by
-    report_info = REPORT_INFORMATION.get(name, {})
     tic = datetime.datetime.now()
-    queryset = _get_queryset(name, start_date, end_date)
-    print(queryset)
-    if report_info:
-        if is_detailed_report:
-            pass
-        else:
-            # Add grouping annotations
-            queryset = queryset.values(*grouped_by)
-            # Add metrics column to the queryset
-            # sample_count
-            queryset = queryset.annotate(sample_count=Count(F("biosample"), distinct=True))
-            # library_count
-            queryset = queryset.annotate(library_count=Count(F("library"), distinct=True))
-            # read_count
-            queryset = queryset.annotate(read_count=Sum(F("reads")))
-            # base_count
-            queryset = queryset.annotate(base_count=Sum(F("bases")))
-            for entry in queryset.all().distinct():
-                print(entry)
+    queryset = _get_queryset(name, start_date, end_date, time_window, grouped_by)
+    for entry in queryset:
+        print(entry)
+    headers = [column for column in queryset.first().keys()]
+    print(headers)
     toc = datetime.datetime.now()
     duration = toc - tic
     print(duration)
     return report_data, errors, warnings
 
-def _get_queryset(name: str, start_date: str, end_date: str):
+def _get_queryset(name: str, start_date: str, end_date: str, time_window: str, grouped_by: List[str]):
     """
     Provides for each report the basic report quesyset
 
@@ -56,16 +47,62 @@ def _get_queryset(name: str, start_date: str, end_date: str):
           `name`: name of the report
           `start_date`: start of the report time period requested
           `end_date`: end of the report time period requested
+          `time_range`: size of the report page in time.
+          `grouped_by`: fields that drive the aggregation of data in order.
     """
     
     # For now this assumes only one field is designated as is_date by report. Once we set more date fields we would need to have the date field name as input.
     report_data = MetricField.objects.filter(report__name=name).annotate(date_field=F("name")).filter(is_date=True).values("report__data_model", "date_field")[:1]
-    DataModel = apps.get_model("fms_report", report_data[0]["report__data_model"])
-    print(report_data)
-    queryset = ( DataModel.objects.annotate(date_field=F(report_data[0]["date_field"]))
-                                  .filter(date_field__gte=start_date)
-                                  .filter(date_field__lte=end_date)
-                                  .distinct()
-                                  .order_by() )
+    if report_data:
+        DataModel = apps.get_model("fms_report", report_data[0]["report__data_model"])
+        print(report_data)
+        # Annotate for the time aggregation
+        kind = TimeWindow.DAILY.value # defaults to daily
+        match time_window:
+            case TimeWindow.MONTHLY.label:
+                kind = TimeWindow.MONTHLY.value
+            case TimeWindow.WEEKLY.label:
+                kind = TimeWindow.WEEKLY.value
+            case TimeWindow.DAILY.label:
+                kind = TimeWindow.DAILY.value
+
+        queryset = ( DataModel.objects.annotate(date_field=F(report_data[0]["date_field"]))
+                                      .annotate(time_window=functions.Trunc(F("date_field"), kind))
+                                      .filter(date_field__gte=start_date)
+                                      .filter(date_field__lte=end_date)
+                                      .distinct() )
+        
+        if (len(grouped_by) == 0):
+            # detailed fields
+            custom_fields = ["run_name", "lane", "project"]
+            ordering_fields = ["time_window", "date_field"]
+            ordering_fields.extend(custom_fields)
+            detailed_fields = ["time_window"]
+            detailed_fields.extend(MetricField.objects.filter(report__name=name).values_list("name", flat=True))
+            queryset = queryset.values(*detailed_fields)
+            queryset = queryset.order_by(*ordering_fields)
+        else:
+            # grouping definition
+            extended_grouped_by = ["time_window"]
+            extended_grouped_by.extend(grouped_by)
+            queryset = queryset.values(*extended_grouped_by)
+            # aggregated fields
+            aggregate_fields = MetricField.objects.filter(report__name=name).filter(aggregation__isnull=False).values_list("name", "aggregation")
+            for name, aggregation in aggregate_fields:
+                aggregate = None
+                match aggregation:
+                    case AggregationType.COUNT:
+                        aggregate = Count(F(name), distinct=True)
+                    case AggregationType.SUM:
+                        aggregate = Sum(F(name))
+                    case AggregationType.MAX:
+                        aggregate = Max(F(name))
+                    case AggregationType.MIN:
+                        aggregate = Min(F(name))
+                annotation = { f"{name}_count": aggregate}
+                queryset = queryset.annotate(**annotation)
+                queryset = queryset.order_by(*extended_grouped_by)
+    else:
+        queryset = None
 
     return queryset
--- backend/fms_core/viewsets/report.py
+++ backend/fms_core/viewsets/report.py
@@ -6,7 +6,7 @@ from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
 from django.http import QueryDict
 
-from ..services.report import AVAILABLE_REPORTS, REPORT_INFORMATION, get_report
+from ..services.report import list_reports, list_report_information, get_report
 
 class ReportViewSet(viewsets.ViewSet):
     permission_classes = [IsAuthenticated]
@@ -18,21 +18,20 @@ class ReportViewSet(viewsets.ViewSet):
         params = QueryDict(request.META.get("QUERY_STRING"))
         name = params.get("name", None)
         grouped_by = params.getlist("group_by", [])
-        time_aggregation = params.get("time_aggregation", None)
+        time_window = params.get("time_window", None)
         start_date = params.get("start_date", None)
         end_date = params.get("end_date", None)
 
         if name is None:
-            return Response(AVAILABLE_REPORTS)
+            return Response(list_reports())
         else:
             if start_date is None or end_date is None:
                 # Provide information about the requested report if no start_date and end_date provided
-                report_information = REPORT_INFORMATION.get(name, f"Requested report does not exist.")
-                return Response(report_information)
+                return Response(list_report_information(name))
             
             report_data, errors, warnings = get_report(name=name,
                                                        grouped_by=grouped_by,
-                                                       time_aggregation=time_aggregation,
+                                                       time_window=time_window,
                                                        start_date=start_date,
                                                        end_date=end_date)
             return Response(report_data)
