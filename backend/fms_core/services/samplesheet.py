707bf69560988fb7f2b68ba7023a8cf7a0210372
Nafiz Islam <nafiz.islam1001@gmail.com>
2025-01-16 16:45:04 -0500

    Update samplesheet (#906)
    * update samplesheet
    
    * update samplesheet

--- backend/fms_core/services/samplesheet.py
+++ backend/fms_core/services/samplesheet.py
@@ -80,10 +80,10 @@ def get_samplesheet(container_kind, placement):
 
 def _get_Sample_ID(sample_alias, derived_sample_id):
     MAX_SAMPLE_ID_LENGTH = 100
-    SEPERATOR = "_"
+    SEPARATOR = "_"
     sample_alias = str(sample_alias)
     derived_sample_id = str(derived_sample_id)
-    max_sample_alias_length = MAX_SAMPLE_ID_LENGTH - len(SEPERATOR) - len(derived_sample_id)
+    max_sample_alias_length = MAX_SAMPLE_ID_LENGTH - len(SEPARATOR) - len(derived_sample_id)
 
     sample_alias = fit_string_with_ellipsis_in_middle(sample_alias, max_sample_alias_length, "---")
 
@@ -123,6 +123,9 @@ def _generate_samplesheet_workbook(BCLConvert_Data: list[BCLConvert_Datum], Drag
     fillWritable = PatternFill(start_color="e8f2a1", end_color="e8f2a1", fill_type="solid")
     def style_writable(cell):
         cell.fill = fillWritable
+    fillHardcoded = PatternFill(start_color="fc8b8b", end_color="fc8b8b", fill_type="solid")
+    def style_hardcoded(cell):
+        cell.fill = fillHardcoded
 
     MAX_COLUMN = 5
     def add_section_header(section_name: str):
@@ -185,24 +188,24 @@ def _generate_samplesheet_workbook(BCLConvert_Data: list[BCLConvert_Datum], Drag
             ],
             [
                 CD(
-                    value="",
+                    value="151",
                     validation=DataValidation(type="whole", operator="greaterThan", formula1=0, allow_blank=False, showErrorMessage=True, errorTitle="Invalid Read1Cycles Value", error="Read1Cycles must be greater than 0"),
-                    apply_cell=style_writable
+                    apply_cell=style_hardcoded
                 ),
                 CD(
-                    value="",
+                    value="151",
                     validation=DataValidation(type="whole", operator="greaterThan", formula1=0, allow_blank=False, showErrorMessage=True, errorTitle="Invalid Read2Cycles Value", error="Read2Cycles must be greater than 0"),
-                    apply_cell=style_writable
+                    apply_cell=style_hardcoded
                 ),
                 CD(
-                    value="",
+                    value="10",
                     validation=DataValidation(type="whole", operator="greaterThanOrEqual", formula1=0, allow_blank=False, showErrorMessage=True, errorTitle="Invalid Index1Cycles Value", error="Index1Cycles must be greater than or equal to 0"),
-                    apply_cell=style_writable
+                    apply_cell=style_hardcoded
                 ),
                 CD(
-                    value="",
+                    value="10",
                     validation=DataValidation(type="whole", operator="greaterThanOrEqual", formula1=0, allow_blank=False, showErrorMessage=True, errorTitle="Invalid Index2Cycles Value", error="Index2Cycles must be greater than or equal to 0"),
-                    apply_cell=style_writable
+                    apply_cell=style_hardcoded
                 ),
             ]
         ]
@@ -245,10 +248,10 @@ def _generate_samplesheet_workbook(BCLConvert_Data: list[BCLConvert_Datum], Drag
                 CD(value="FastqCompressionFormat", apply_cell=style_header)
             ],
             [
-                CD(value="", apply_cell=style_writable),
+                CD(value="4.3.13", apply_cell=style_hardcoded),
                 CD(value=ADAPTERREAD1S["IlluminaDNAPCRFree"], apply_cell=style_writable),
                 CD(value=ADAPTERREAD2S["IlluminaDNAPCRFree"], apply_cell=style_writable),
-                CD(value="", apply_cell=style_writable),
+                CD(value="Y151;I10;I10;Y151", apply_cell=style_hardcoded),
                 CD(
                     value="gzip",
                     validation=DataValidation(type="list", formula1=f'"{",".join(FASTQ_COMPRESSION_FORMATS)}"', allow_blank=False, showErrorMessage=True, errorTitle="Invalid FastqCompressionFormat Value", error="Only gzip is supported"),
@@ -298,8 +301,8 @@ def _generate_samplesheet_workbook(BCLConvert_Data: list[BCLConvert_Datum], Drag
                 CD(value="KeepFastq", apply_cell=style_header)
             ],
             [
-                CD(value="", apply_cell=style_writable),
-                CD(value="", apply_cell=style_writable),
+                CD(value="4.3.13", apply_cell=style_hardcoded),
+                CD(value="1.3.11", apply_cell=style_hardcoded),
                 CD(
                     value="none",
                     validation=DataValidation(type="list", formula1=f'"{",".join(MAP_ALIGN_OUT_FORMATS)}"', allow_blank=False, showErrorMessage=True, errorTitle="Invalid MapAlignOutFormat Value", error=f"Only {', '.join(MAP_ALIGN_OUT_FORMATS)} are supported"),
@@ -317,7 +320,7 @@ def _generate_samplesheet_workbook(BCLConvert_Data: list[BCLConvert_Datum], Drag
     samplesheet.append([])
     add_section_header("DragenGermline_Data")
     REFERENCE_GENOME_REFERENCE_DIRS = [
-        "hg38-alt_masked.cnv.hla.rna-8-r2.0-1",
+        "hg38-alt_masked.cnv.graph.hla.rna-10-r4.0-2",
         "hg19-alt_masked.cnv.graph.hla.rna-10-r4.0-1",
         "chm13_v2-cnv.graph.hla.rna-10-r4.0-1",
     ]
