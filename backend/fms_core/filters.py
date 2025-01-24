6caef0e96b37b5a0718bc2cafa6fc77ecf33cd4b
Nafiz Islam <nafiz.islam@computationalgenomics.ca>
2025-01-24 14:38:05 -0500

    fix filter

--- backend/fms_core/filters.py
+++ backend/fms_core/filters.py
@@ -184,7 +184,11 @@ class IndexFilter(GenericFilter):
 class DatasetFilter(GenericFilter):
     release_flag = django_filters.NumberFilter(method="release_flag_filter")
     latest_release_update = django_filters.CharFilter(method="latest_release_update_filter")
+    latest_release_update__gte = django_filters.CharFilter(method="latest_release_update__gte_filter")
+    latest_release_update__lte = django_filters.CharFilter(method="latest_release_update__lte_filter")
     latest_validation_update = django_filters.CharFilter(method="latest_validation_update_filter")
+    latest_validation_update__gte = django_filters.CharFilter(method="latest_validation_update__gte_filter")
+    latest_validation_update__lte = django_filters.CharFilter(method="latest_validation_update__lte_filter")
 
     def release_flag_filter(self, queryset, name, value):
         return queryset.filter(release_flag=value)
@@ -194,10 +198,30 @@ class DatasetFilter(GenericFilter):
             latest_release_update=Max("readsets__release_status_timestamp")
         ).filter(latest_release_update__gt=value)
 
+    def latest_release_update__gte_filter(self, queryset, name, value):
+        return queryset.annotate(
+            latest_release_update=Max("readsets__release_status_timestamp")
+        ).filter(latest_release_update__gte=value)
+
+    def latest_release_update__lte_filter(self, queryset, name, value):
+        return queryset.annotate(
+            latest_release_update=Max("readsets__release_status_timestamp")
+        ).filter(latest_release_update__lte=value)
+
     def latest_validation_update_filter(self, queryset, name, value):
         return queryset.annotate(
             latest_validation_update=Max("readsets__validation_status_timestamp")
         ).filter(latest_validation_update__gt=value)
+
+    def latest_validation_update__gte_filter(self, queryset, name, value):
+        return queryset.annotate(
+            latest_validation_update=Max("readsets__validation_status_timestamp")
+        ).filter(latest_validation_update__gte=value)
+
+    def latest_validation_update__lte_filter(self, queryset, name, value):
+        return queryset.annotate(
+            latest_validation_update=Max("readsets__validation_status_timestamp")
+        ).filter(latest_validation_update__lte=value)
 
     class Meta:
         model = Dataset
