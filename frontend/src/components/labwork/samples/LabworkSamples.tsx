c7674dd64c87e074a812ead4674750f186f9dfea
Nafiz Islam <nafiz.islam@computationalgenomics.ca>
2025-01-31 15:22:44 -0500

    wip

--- frontend/src/components/labwork/samples/LabworkSamples.tsx
+++ frontend/src/components/labwork/samples/LabworkSamples.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useEffect, useMemo, useState } from "react";
 import { useAppSelector } from "../../../hooks";
-import { selectSamplesTable } from "../../../selectors";
+import { selectSamplesTable, selectToken } from "../../../selectors";
 import { usePagedItemsActionsCallbacks } from "../../pagedItemsTable/usePagedItemsActionCallbacks";
 import SamplesTableActions from '../../../modules/samplesTable/actions'
 import { SAMPLE_COLUMN_FILTERS, SAMPLE_FILTER_KEYS, SAMPLE_COLUMN_DEFINITIONS, SampleColumn, ObjectWithSample } from '../../samples/SampleTableColumns'
@@ -8,10 +8,12 @@ import { useFilteredColumns } from "../../pagedItemsTable/useFilteredColumns";
 import AppPageHeader from "../../AppPageHeader";
 import PageContent from "../../PageContent";
 import PagedItemsTable, { DataObjectsByID, PagedItemsTableProps } from "../../pagedItemsTable/PagedItemsTable";
-import { Sample } from "../../../models/frontend_models";
+import { Sample, Step, Study } from "../../../models/frontend_models";
 import { SampleAndLibrary } from "../../WorkflowSamplesTable/ColumnSets";
 import { Button, Col, Flex, Row } from "antd";
 import { fetchSamples } from "../../../modules/cache/cache";
+import api, { dispatchForApi } from "../../../utils/api";
+import { FilterSet } from "../../../models/paged_items";
 
 export function LabworkSamples() {
     const samplesTableState = useAppSelector(selectSamplesTable)
@@ -81,7 +83,7 @@ export function LabworkSamples() {
                         />
                     </Col>
                     <Col span={12}>
-                        <LabworkSampleActions defaultSelection={defaultSelection} exceptedSampleIDs={exceptedSampleIDs} />
+                        <LabworkSampleActions defaultSelection={defaultSelection} exceptedSampleIDs={exceptedSampleIDs} filters={filters} />
                     </Col>
                 </Row>
             </PageContent>
@@ -92,11 +94,67 @@ export function LabworkSamples() {
 interface LabworkSampleActionsProps {
     defaultSelection: boolean
     exceptedSampleIDs: Sample['id'][]
+    filters: FilterSet
 }
-function LabworkSampleActions({ defaultSelection, exceptedSampleIDs }: LabworkSampleActionsProps) {
+function LabworkSampleActions({ defaultSelection, exceptedSampleIDs, filters }: LabworkSampleActionsProps) {
+    const token = useAppSelector(selectToken)
+    const [options, setOptions] = useState<string[]>([])
+    const [isFetching, setIsFetching] = useState(false)
+    useEffect(() => {
+        if (!token) return
+        (async () => {
+            setIsFetching(true)
+            console.info({ defaultSelection, exceptedSampleIDs, filters })
+            const sampleIDs = (await dispatchForApi(token,
+                api.samples.sample_ids_by_default_selection_excepted_ids(
+                    defaultSelection,
+                    exceptedSampleIDs,
+                    Object.entries(filters).reduce<Record<string, string>>((acc, [key, filter]) => {
+                        acc[key] = filter.value?.toString() ?? ''
+                        return acc
+                    }, {})
+                ))
+            ).data
+            console.info({ sampleIDs })
 
-    return <Flex vertical gap={"middle"}>
-        <Button>hello</Button>
-        <Button>world</Button>
+            if (sampleIDs.length === 0) {
+                setOptions([])
+                setIsFetching(false)
+                return
+            }
+
+            const sampleNextSteps = (await dispatchForApi(token, api.sampleNextStep.listSamples(sampleIDs))).data.results
+            console.info({ sampleNextSteps })
+            type ValueType = [Study['id'], Step['name']]
+            const studyStepsBySample = sampleNextSteps.reduce<Record<Sample['id'], ValueType>>((acc, sampleNextStep) => {
+                acc[sampleNextStep.sample] = acc[sampleNextStep.sample] ?? []
+                for (const study of sampleNextStep.studies) {
+                    acc[sampleNextStep.sample] = [study, sampleNextStep.step.name]
+                }
+                return acc
+            }, {})
+
+            const studyStepCount: Record<string, number> = {}
+            for (const [sampleID, [study, stepName]] of Object.entries(studyStepsBySample)) {
+                const key = `${study}-${stepName}`
+                studyStepCount[key] = (studyStepCount[key] ?? 0) + 1
+            }
+            console.info({ studyStepCount })
+            const commonStudySteps = Object.entries(studyStepCount).reduce<ValueType[]>((acc, [key, count]) => {
+                if (count === sampleIDs.length) {
+                    acc.push(key.split('-') as ValueType)
+                }
+                return acc
+            }, [])
+
+            setOptions(commonStudySteps.map(([studyID, stepName]) => `Dequeue from ${studyID} - ${stepName}`))
+            setIsFetching(false)
+        })();
+    }, [defaultSelection, exceptedSampleIDs, filters, !!token])
+
+return <Flex vertical gap={"middle"}>
+        {
+            isFetching ? "Fetching options..." : options.map(option => <Button key={option} type="primary">{option}</Button>)
+        }
     </Flex>
 }
