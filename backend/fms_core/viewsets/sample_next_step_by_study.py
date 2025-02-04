7a5dc5306e1857ce317536b865c80e5b4a611199
Nafiz Islam <nafiz.islam@computationalgenomics.ca>
2025-02-04 07:45:31 -0500

    wip

--- backend/fms_core/viewsets/sample_next_step_by_study.py
+++ backend/fms_core/viewsets/sample_next_step_by_study.py
@@ -77,14 +77,16 @@ class SampleNextStepByStudyViewSet(viewsets.ModelViewSet):
         queryset = self.get_queryset().filter(sample_next_step__sample__id__in=sample_ids, study=study, step_order__order=stepOrder)
         values_list = queryset.values_list("sample_next_step__sample", "study", "step_order__order")
         errors = []
+        removed = False
         try:
             with transaction.atomic():
                 for sample_id, study_id, order in values_list:
                     sample = Sample.objects.get(id=sample_id)
                     study = Study.objects.get(id=study_id)
-                    removed, newerrors, _ = dequeue_sample_from_specific_step_study_workflow_with_updated_last_step_history(sample, study, order)
+                    newremoved, newerrors, _ = dequeue_sample_from_specific_step_study_workflow_with_updated_last_step_history(sample, study, order)
                     errors.extend(newerrors)
-                if errors:
+                    removed = removed and newremoved
+                if errors or not removed:
                     raise IntegrityError(errors)
         except IntegrityError as err:
             return HttpResponseBadRequest(err)
