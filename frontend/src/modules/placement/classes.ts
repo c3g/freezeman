94c32ef3bea29baa12e40728b665394abf1c898b
Nafiz Islam <nafiz.islam@computationalgenomics.ca>
2025-03-24 17:18:41 -0400

    wip

--- frontend/src/modules/placement/classes.ts
+++ frontend/src/modules/placement/classes.ts
@@ -1,8 +1,13 @@
-import { CoordinateSpec } from "../../models/fms_api_models"
+import { CoordinateAxis, CoordinateSpec } from "../../models/fms_api_models"
+import { coordinatesToOffsets } from "../../utils/functions"
 import { PlacementDirections, PlacementType } from "./models"
 
 class PlacementObject {
     placement: PlacementClass
+
+    // action context
+    sourceContainer: RealContainerParentClass | TubesWithoutParentClass
+    destinationContainer: RealContainerParentClass | null
 }
 
 class PlacementClass extends PlacementObject {
@@ -10,42 +15,6 @@ class PlacementClass extends PlacementObject {
     placementType: PlacementType
     placementDirection: PlacementDirections
     error: string | null | undefined
-
-    // context
-    sourceContainer: ContainerClass
-    destinationContainer: ContainerClass | null
-
-    findContainerByID(containerID: number): ContainerClass | null {
-        return this.containers.find(c => c.id === containerID) ?? null
-    }
-
-    getContainerByID(containerID: number): ContainerClass {
-        const container = this.findContainerByID(containerID)
-        if (container) {
-            return container
-        } else {
-            throw new Error(`Container with ID ${containerID} not found`)
-        }
-    }
-
-    findSampleByID(sampleID: number): SampleClass | null {
-        for (const container of this.containers) {
-            const sample = container.findSampleByID(sampleID)
-            if (sample) {
-                return sample
-            }
-        }
-        return null
-    }
-
-    getSampleByID(sampleID: number): SampleClass {
-        const sample = this.findSampleByID(sampleID)
-        if (sample) {
-            return sample
-        } else {
-            throw new Error(`Sample with ID ${sampleID} not found`)
-        }
-    }
 }
 
 class ContainerClass extends PlacementObject {
@@ -54,23 +23,50 @@ class ContainerClass extends PlacementObject {
     samples: SampleClass[]
     selectedSamples: SampleClass[]
 
-    findSampleByID(sampleID: number): SampleClass | null {
-        return this.samples.find(s => s.id === sampleID) ?? null
-    }
-
     equals(other: ContainerClass): boolean {
         return this.id === other.id
     }
 
-    isSelected(sampleID: number): boolean {
-        return this.selectedSamples.some(s => s.id === sampleID)
+    findSample(sampleID: SampleIdentifier) {
+        if ('id' in sampleID) {
+            return this.samples.find(s => s.id === sampleID.id)
+        } else {
+            return this.samples.find(s => s.fromCell?.coordinates === sampleID.coordinates
+                && s.fromCell?.fromContainer.name === sampleID.parentContainerName)
+        }
+    }
+    getSample(sampleID: SampleIdentifier) {
+        const sample = this.findSample(sampleID)
+        if (!sample) {
+            throw new Error(`Sample with ID ${sampleID} not found in container ${this.id}`)
+        }
+        return sample
+    }
+    isSampleSelected(sample: SampleClass): boolean {
+        return this.selectedSamples.some(s => s.equals(sample))
+    }
+    selectSample(sample: SampleClass) {
+        if (!this.isSampleSelected(sample)) {
+            this.selectedSamples.push(sample)
+        }
+    }
+    unSelectSample(sample: SampleClass) {
+        this.selectedSamples = this.selectedSamples.filter(s => s.equals(sample))
+    }
+    toggleSelected(...samples: SampleClass[]) {
+        const allSelected = samples.every(s => this.isSampleSelected(s))
+        if (allSelected) {
+            samples.forEach(id => this.unSelectSample(id))
+        } else {
+            samples.forEach(id => this.selectSample(id))
+        }
     }
 
     isSource(): boolean {
-        return this.placement.sourceContainer.equals(this)
+        return this.sourceContainer.equals(this)
     }
     isDestination(): boolean {
-        return this.placement.destinationContainer?.equals(this) ?? false
+        return this.destinationContainer?.equals(this) ?? false
     }
 }
 
@@ -83,44 +79,86 @@ class TubesWithoutParentClass extends ContainerClass {
 }
 
 class CellClass extends PlacementObject {
-    from: ContainerClass
+    fromContainer: RealContainerParentClass
     coordinates: string
     existingSample: SampleClass | null
     placed: SampleClass[]
-
-    selectedSamples(): SampleClass[] {
-        if (this.existingSample) {
-            return this.from.selectedSamples.filter(s => s.id === this?.existingSample?.id)
-        } else {
-            return this.from.selectedSamples.reduce<SampleClass[]>((acc, s) => {
-                if (this.placed.some(p => p.id === s.id)) {
-                    acc.push(s)
-                }
-                return acc
-            }, [])
-        }
-    }
+    preview: boolean
 
     click() {
-        const isDestination = this.placement.destinationContainer?.equals(this.from) ?? false
-        if (this.from.isSource()) {
+        if (this.fromContainer.isSource()) {
             if (this.existingSample) {
-                if (this.from.isSelected(this.existingSample.id)) {
-                    this.from.selectedSamples = this.from.selectedSamples.filter(s => s.id !== this?.existingSample?.id)
-                } else {
-                    this.from.selectedSamples.push(this.existingSample)
-                }
+                this.fromContainer.toggleSelected(this.existingSample)
             }
-        } else if (this.from.isDestination()) {
-            if 
+        } else if (this.fromContainer.isDestination()) {
+            if (this.sourceContainer.selectedSamples.length > 0) {
+
+            } else {
+                this.fromContainer.toggleSelected(...this.placed)
+            }
         }
 
+    }
+
+    #placementDestinations() {
+        if (!this.destinationContainer?.equals(this.fromContainer)) {
+            return []
+        }
+
+        const destinationContainer = this.destinationContainer
+        const sourceSamples = this.sourceContainer.selectedSamples
+
+        const [axisRow, axisCol] = destinationContainer.spec
+        const height = axisRow?.length ?? 1
+        const width = axisCol?.length ?? 1
+
+        const newOffsetsList: number[][] = []
+        const destinationStartingOffsets = coordinatesToOffsets(destinationContainer.spec, destination.coordinates)
+
+
+        switch (this.placement.placementType) {
+            case PlacementType.PATTERN: {
+                const sourceOffsetsList = sourceSamples.map((source) => {
+                    const parentContainer = source.fromContainer
+                    const coordinates = source?.fromCell?.coordinates
+                    if (parentContainer instanceof TubesWithoutParentClass || !coordinates)
+                        throw new Error(`For pattern placement, source cell must be in a parent container`)
+                    return coordinatesToOffsets(parentContainer.spec, coordinates)
+                })
+
+                // find top left corner that tightly bounds all of the selections
+                const minOffsets = sourceOffsetsList.reduce((minOffsets, offsets) => {
+                    return offsets.map((_, index) => offsets[index] < minOffsets[index] ? offsets[index] : minOffsets[index])
+                }, sourceOffsetsList[0])
+    
+    
+                newOffsetsList.push(
+                    ...sourceOffsetsList.map(
+                        (sourceOffsets) => destinationContainer.spec.map(
+                            (_: CoordinateAxis, index: number) => sourceOffsets[index] - minOffsets[index] + destinationStartingOffsets[index]
+                        )
+                    )
+                )
+    
+                break
+            }
+            case PlacementType.GROUP: {
+                break
+            }
+        }
     }
 }
 
 class SampleClass extends PlacementObject {
-    from: CellClass
+    fromContainer: RealContainerParentClass | TubesWithoutParentClass
+    fromCell: CellClass | null // null for TubesWithoutParent
     id: number
     placed: CellClass[]
     hightlight: boolean
+
+    equals(other: SampleClass): boolean {
+        return this.id === other.id
+    }
 }
+
+type SampleIdentifier = { id: number } | { coordinates: string, parentContainerName: string }
