52709bc8ec79035ee08e976067b8b410682fbe89
Nafiz Islam <nafiz.islam@computationalgenomics.ca>
2025-05-15 08:41:59 -0400

    move count to end

--- frontend/src/components/management/WorkflowAssignment.tsx
+++ frontend/src/components/management/WorkflowAssignment.tsx
@@ -481,13 +481,13 @@ function WorkflowOptions({ defaultSelection, exceptedSampleIDs, filters }: Labwo
             <Collapse key={'actions'} accordion items={[
                 {
                     key: 'dequeue',
-                    label: `(${projectDequeueActions.length}) Dequeue From Step`,
+                    label: `Dequeue From Step (${projectDequeueActions.length})`,
                     children: <Flex gap={"small"} vertical>{projectDequeueActions}</Flex>,
                     classNames: { body: 'labwork-samples-actions' },
                 },
                 {
                     key: 'queue',
-                    label: `(${projectQueueActions.length}) Queue To Step`,
+                    label: `Queue To Step (${projectQueueActions.length})`,
                     children: <Flex gap={"small"} vertical>{projectQueueActions}</Flex>,
                     classNames: { body: 'labwork-samples-actions' },
                 }
