1e21cf77acfb03ebcb7e5a0b488cab0ee67d8fb7
ufgauthi <ulysse.fortiergauthier@mcgill.ca>
2025-06-16 17:14:05 -0400

    WIP sample identity creation.

--- backend/fms_core/services/sample_identity.py
+++ backend/fms_core/services/sample_identity.py
@@ -1,10 +1,11 @@
 from fms_core.schema_validators import SAMPLE_IDENTITY_REPORT_VALIDATOR
 from django.core.exceptions import ValidationError
 from fms_core.models import Sample, Container, SampleIdentity
+from fms_core.models._constants import SEX_UNKNOWN
 from fms_core.containers import CONTAINER_KIND_SPECS
 from fms_core.coordinates import convert_alpha_digit_coord_to_ordinal, COLUMN
 
-def get_or_create_sample_identity(biosample_id: int, predicted_sex: str, passed: bool, biosample_matches: list[str]):
+def get_or_create_sample_identity(biosample_id: int, passed: bool, predicted_sex: str, biosample_id_matches: list[int], replace: bool = False):
     """
     Returns a sample identity that is either existing or created from the provided informations.
 
@@ -14,20 +15,44 @@ def get_or_create_sample_identity(biosample_id: int, predicted_sex: str, passed:
         biosample_matches: Other biosamples that matches the current identity.
 
     Returns:
-      Tuple with the following content:
-      `sample_identity`: Sample identity object.
-      `created`: Boolean that is true if the object was created.
-      `errors`: Errors generated during the processing.
-      `warnings`: Warnings generated during the processing.
+        Tuple with the following content:
+        `sample_identity`: Sample identity object.
+        `created`: Boolean that is true if the object was created.
+        `errors`: Errors generated during the processing.
+        `warnings`: Warnings generated during the processing.
     """
     sample_identity = None
     errors = []
     warnings = []
     try:
-        sample_identity, created = SampleIdentity.objects.get_or_create(biosample_id=biosample_id,
-                                                                        defaults={"predicted_sex": predicted_sex, "passed_qc": passed})
+        sample_identity, created = SampleIdentity.objects.get_or_create(biosample_id=biosample_id, defaults={"predicted_sex": predicted_sex, "passed_qc": passed})
     except ValidationError as e:
-        errors.append(';'.join(e.messages))  
+        errors.append(';'.join(e.messages))
+
+    if sample_identity and not created:
+        old_biosample_matches = [identity.biosample_id for identity in sample_identity.identity_matches].sort()
+        if replace:
+            if not sample_identity.predicted_sex == predicted_sex:
+                warnings.append(f"Identity change for biosample ID {biosample_id}. Predicted sex changed from {sample_identity.predicted_sex} to {predicted_sex}.")
+                sample_identity.predicted_sex = predicted_sex
+            if not sample_identity.passed_qc == passed:
+                warnings.append(f"Identity change for biosample ID {biosample_id}. Predicted sex changed from {sample_identity.passed_qc} to {passed}.")
+                sample_identity.passed_qc = passed
+            if not old_biosample_matches == biosample_id_matches.sort():
+                warnings.append(f"Identity change for biosample ID {biosample_id}. Identity biosample matches changed from {old_biosample_matches} to {biosample_id_matches.sort()}.")
+                sample_identity.identity_matches.clear()
+                for match in biosample_id_matches:
+                    try:
+                        matched_identity = SampleIdentity.objects.get(biosample_id=match)
+                        sample_identity.identity_matches.add(matched_identity)
+                    except Exception as err:
+                        errors.append(f"Identity for biosample {match} cannot be found.")
+        elif not replace:
+            if not sample_identity.predicted_sex == predicted_sex:
+                errors.append(f"Identity difference for biosample ID {biosample_id}. Predicted sex changed from {sample_identity.predicted_sex} to {predicted_sex}.")
+            if not sample_identity.passed_qc == passed:
+                errors.append(f"Identity difference for biosample ID {biosample_id}. Predicted sex changed from {sample_identity.passed_qc} to {passed}.")
+        
     return sample_identity, created, errors, warnings
 
 
@@ -80,7 +105,7 @@ def ingest_identity_testing_report(report_json):
             if passed:
                 predicted_sex = sample_report.get("fluidigm_predicted_sex", None)
                 if predicted_sex == INCONCLUSIVE:
-                    predicted_sex = SampleIdentity.SEX_CHOICES.UNKNOWN
+                    predicted_sex = SEX_UNKNOWN
                 matches = sample_report.get("genotype_match", [])
                 
             
