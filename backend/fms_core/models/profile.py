bc870b06bd61556f1d7d4b2887f236d7e7dab1dc
Nafiz Islam <nafiz.islam@computationalgenomics.ca>
2025-10-03 19:03:28 -0400

    implement and use services

--- backend/fms_core/models/profile.py
+++ backend/fms_core/models/profile.py
@@ -20,6 +20,9 @@ class Profile(TrackedModel):
             preferences.update(self.preferences)
         return preferences
 
+    def is_personalized(self) -> bool:
+        return self.parent is not None
+
     def clean(self) -> None:
         super().clean()
 
--- backend/fms_core/viewsets/profile.py
+++ backend/fms_core/viewsets/profile.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 
 from django.core.exceptions import ValidationError
 
+from fms_core.services.profile import get_profile, update_preferences
 from fms_core.schema_validators import PREFERENCES_VALIDATOR
 from fms_core.models import FreezemanUser, Profile
 from fms_core.serializers import ProfileSerializer
@@ -18,51 +19,20 @@ class ProfileViewSet(viewsets.ModelViewSet):
     http_method_names = ["get", "head", "options", "patch"]
 
     def retrieve(self, _request, pk, *args, **kwargs):
-        try:
-            fm_user = FreezemanUser.objects.filter(user__id=pk).get()
-        except FreezemanUser.DoesNotExist:
-            fm_user = FreezemanUser.objects.create(
-                user_id=pk,
-                profile=Profile.objects.get(name="Default"),
-            )
-        serializer = self.get_serializer(fm_user.profile)
+        # user with id=pk is assumed to exist
+        serializer = self.get_serializer(get_profile(int(pk)))
         return Response(serializer.data)
 
     def list(self, _request, *args, **kwargs):
         # Why on earth would you want to list all profiles?
         return bad_request(_request, "Listing all profiles is not allowed.")
     
-    def partial_update(self, request, pk=None, *args, **kwargs):
-        try:
-            fm_user = FreezemanUser.objects.filter(user__id=pk).get()
-        except FreezemanUser.DoesNotExist:
-            fm_user = FreezemanUser.objects.create(
-                user_id=pk,
-                profile=Profile.objects.get(name="Default"),
-            )
-        profile: Profile = fm_user.profile
+    def partial_update(self, request, pk, *args, **kwargs):
+        # user with id=pk is assumed to exist
         data = request.data
-        try:
-            if not PREFERENCES_VALIDATOR.validator.is_valid(data["preferences"]):
-                raise ValidationError({"preferences": "Invalid preferences format."})
-            old_preferences = profile.final_preferences()
-            new_preferences = {}
-            for key, new_value in data["preferences"].items():
-                if new_value != old_preferences[key]:
-                    new_preferences[key] = new_value
-            if new_preferences:
-                if profile.parent is None:
-                    profile = Profile.objects.create(
-                        name=fm_user.username,
-                        parent=profile,
-                        preferences=new_preferences,
-                    )
-                    fm_user.profile = profile
-                    fm_user.save()
-                profile.preferences = new_preferences
-                profile.save()
-            return Response(self.get_serializer(profile).data)
-        except ValidationError as e:
-            raise e
-        except Exception as e:
-            raise ValidationError({"detail": str(e)})
+        profile, errors, warnings = update_preferences(int(pk), data["preferences"])
+        if errors:
+            raise ValidationError({"preferences": errors})
+        serializer = self.get_serializer(profile)
+        response_data = serializer.data
+        return Response({**response_data})
