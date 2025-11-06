119d8c0fb29e940987e67fe3be03e6892f3cc78f
Nafiz Islam <nafiz.islam@computationalgenomics.ca>
2025-11-06 13:19:20 -0500

    wip

--- frontend/src/components/management/IndexCuration.tsx
+++ frontend/src/components/management/IndexCuration.tsx
@@ -1,13 +1,14 @@
 import React, { useCallback, useEffect, useMemo, useRef, useState } from "react"
 import { FMSPooledSample } from "../../models/fms_api_models"
-import { Input, InputRef, Table } from "antd"
+import { Checkbox, Input, InputRef, Table } from "antd"
 import { useAppDispatch } from "../../hooks"
 import api from "../../utils/api"
 import { SearchOutlined } from "@ant-design/icons"
-import { AnyObject as ReactAnyObject } from "antd/es/_util/type"
+import { AnyObject as AntdAnyObject } from "antd/es/_util/type"
 import { ColumnType } from "antd/lib/table"
 import { ColumnsType, TablePaginationConfig } from "antd/es/table"
 import { useDebounce } from "../filters/filterComponents/DebouncedInput"
+import { SelectionSelectFn, TableRowSelection } from "antd/es/table/interface"
 
 enum PooledSampleColumnID {
     ALIAS = 'ALIAS',
@@ -33,7 +34,7 @@ const FILTER_DESCRIPTIONS: FilterDescriptions<PooledSampleColumnID> = {
     [PooledSampleColumnID.INDEX]: { type: 'startswith' },
 } as const
 
-type ColumnDefinitions<ColumnID extends string, RowData extends ReactAnyObject> = Record<ColumnID, ColumnType<RowData>>
+type ColumnDefinitions<ColumnID extends string, RowData extends AntdAnyObject> = Record<ColumnID, ColumnType<RowData>>
 const COLUMN_DEFINITIONS: ColumnDefinitions<PooledSampleColumnID, FMSPooledSample> = {
     [PooledSampleColumnID.ALIAS]: {
         title: 'Name',
@@ -59,45 +60,69 @@ const COLUMN_DEFINITIONS: ColumnDefinitions<PooledSampleColumnID, FMSPooledSampl
 
 const DEFAULT_PAGE_SIZE = 20
 export function IndexCuration() {
-    const { pagination, setTotal } = usePagination(DEFAULT_PAGE_SIZE)
-
-    const { pageSize, current: currentPage } = pagination
-    const [filters, setFilters] = useState<Partial<Record<PooledSampleColumnID, string>>>({})
-    const [pooledSamples, setPooledSamples] = useState<FMSPooledSample[]>([])
-    const [loading, setLoading] = useState<boolean>(false)
     const dispatch = useAppDispatch()
-    useEffect(() => {
-        const filterParams = createQueryParamsFromFilters(FILTER_KEYS, FILTER_DESCRIPTIONS, filters)
+    const [total, setTotal] = useState<number>(0)
+    const [pooledSamples, setPooledSamples] = useState<FMSPooledSample[]>([])
+    const [filters, setFilters] = useState<Partial<Record<PooledSampleColumnID, string>>>({})
+    const [loading, setLoading] = useState<boolean>(false)
+
+    const fetchPooledSamples = useCallback((currentPage: number, pageSize: number, fiters: Partial<Record<PooledSampleColumnID, string>>) => {
         setLoading(true)
-        const offset = ((currentPage ?? 1) - 1) * (pageSize ?? DEFAULT_PAGE_SIZE)
-        dispatch(api.pooledSamples.list({ ...filterParams, include_pools_of_one: true, derived_sample__library__isnull: false, offset, limit: pageSize }, true)).then((response) => {
+        const filterParams = createQueryParamsFromFilters(FILTER_KEYS, FILTER_DESCRIPTIONS, fiters)
+        const offset = (currentPage - 1) * pageSize
+        const promise = dispatch(api.pooledSamples.list({ ...filterParams, include_pools_of_one: true, derived_sample__library__isnull: false, offset, limit: pageSize }, true))
+        promise.then((response) => {
             setTotal(response.data.count)
             setPooledSamples(response.data.results)
-        }).finally(() => {
-            setLoading(false)
         })
-    }, [currentPage, dispatch, filters, pageSize, setTotal])
+        promise.finally(() => setLoading(false))
+        return promise
+    }, [dispatch])
 
-    const search = useDebounce((searchKey: string, text: string) => setFilters(prev => ({ ...prev, [searchKey]: text })))
+    const onPaginationChange = useCallback<NonNullable<TablePaginationConfig['onChange']>>((page, pageSize) => {
+        fetchPooledSamples(page, pageSize, filters)
+    }, [fetchPooledSamples, filters])
+    
+    const pagination = usePagination(DEFAULT_PAGE_SIZE, total, onPaginationChange)
+    const { current: currentPage, pageSize } = pagination
+
+    const { defaultSelection, exceptedItems, rowSelection, resetSelection } = useSmartSelection<FMSPooledSample>(
+        total,
+        pooledSamples,
+        (record) => record.id,
+    )
+    console.info({
+        defaultSelection,
+        exceptedItems,
+        currentPage,
+    })
+
+    // Initial fetch
+    useEffect(() => {
+        fetchPooledSamples(1, DEFAULT_PAGE_SIZE, {})
+    }, [fetchPooledSamples])
+
+    const mySetFilter = useDebounce(useCallback((searchKey: string, text: string) => {
+        resetSelection()
+        setFilters(prev => {
+            const filters = { ...prev, [searchKey]: text }
+            fetchPooledSamples(1, pageSize ?? DEFAULT_PAGE_SIZE, filters)
+            return filters
+        })
+    }, [fetchPooledSamples, pageSize, resetSelection]))
     const searchInput = useRef<InputRef>(null)
     const searchDefinitions = useMemo<SearchPropertiesDefinitions<PooledSampleColumnID>>(() => ({
-        [PooledSampleColumnID.ALIAS]: { search, searchInput, searchKey: PooledSampleColumnID.ALIAS, placeholder: 'Name' },
-        [PooledSampleColumnID.CONTAINER_BARCODE]: { search, searchInput, placeholder: 'Container Barcode' },
-        [PooledSampleColumnID.COORDINATES]: { search, searchInput, placeholder: 'Coordinates' },
-        [PooledSampleColumnID.INDEX]: { search, searchInput, placeholder: 'Current Index' },
-    }), [search])
+        [PooledSampleColumnID.ALIAS]: { setFilter: mySetFilter, searchInput, searchKey: PooledSampleColumnID.ALIAS, placeholder: 'Name' },
+        [PooledSampleColumnID.CONTAINER_BARCODE]: { setFilter: mySetFilter, searchInput, placeholder: 'Container Barcode' },
+        [PooledSampleColumnID.COORDINATES]: { setFilter: mySetFilter, searchInput, placeholder: 'Coordinates' },
+        [PooledSampleColumnID.INDEX]: { setFilter: mySetFilter, searchInput, placeholder: 'Current Index' },
+    }), [mySetFilter])
 
     const columns = useMemo<ColumnsType<FMSPooledSample>>(() => createTableColumns(
         COLUMN_DEFINITIONS,
         searchDefinitions,
     ), [searchDefinitions])
 
-    console.info({
-        pooledSamples,
-        columns,
-        loading,
-        filters
-    })
     return (
         <>
             <Table<FMSPooledSample>
@@ -106,14 +131,15 @@ export function IndexCuration() {
                 columns={columns}
                 loading={loading}
                 pagination={pagination}
+                rowSelection={rowSelection}
                 scroll={{ y: '80vh' }}
             />
         </>
     )
 }
 
-function getColumnSearchProps<T = ReactAnyObject>(
-    search: (searchKey: string, value: string) => void,
+function getColumnSearchProps<T = AntdAnyObject>(
+    setFilter: (searchKey: string, value: string) => void,
     searchInput: React.RefObject<InputRef>,
     searchKey: string,
     placeholder?: string
@@ -126,7 +152,7 @@ function getColumnSearchProps<T = ReactAnyObject>(
                     placeholder={`Search ${placeholder ?? searchKey}`}
                     value={selectedKeys[0]}
                     onChange={(e) => {
-                        search(searchKey, e.target.value)
+                        setFilter(searchKey, e.target.value)
                     }}
                     onPressEnter={() => {
                         confirm()
@@ -139,7 +165,7 @@ function getColumnSearchProps<T = ReactAnyObject>(
                     }}
                     onClear={() => {
                         confirm()
-                        search(searchKey, '')
+                        setFilter(searchKey, '')
                     }}
                     allowClear
                     style={{ marginBottom: 8 }}
@@ -160,12 +186,12 @@ function getColumnSearchProps<T = ReactAnyObject>(
 }
 
 type SearchPropertiesDefinitions<ColumnID extends string> = Record<ColumnID, {
-    search: (dataIndex: string, text: string) => void,
+    setFilter: (dataIndex: string, text: string) => void,
     searchInput: React.RefObject<InputRef>,
     placeholder?: string
 }>
 
-function createTableColumns<ColumnID extends string, RowData extends ReactAnyObject>(
+function createTableColumns<ColumnID extends string, RowData extends AntdAnyObject>(
     columnDefinitions: ColumnDefinitions<ColumnID, RowData>,
     searchPropertyDefinitions: SearchPropertiesDefinitions<ColumnID>
 ): ColumnsType<RowData> {
@@ -177,7 +203,7 @@ function createTableColumns<ColumnID extends string, RowData extends ReactAnyObj
         const searchPropsArgs = searchPropertyDefinitions[columnID]
         if (searchPropsArgs) {
             Object.assign(column, getColumnSearchProps(
-                searchPropsArgs.search,
+                searchPropsArgs.setFilter,
                 searchPropsArgs.searchInput,
                 columnID,
                 searchPropsArgs.placeholder
@@ -203,24 +229,134 @@ function createQueryParamsFromFilters<ColumnID extends string>(filterKeys: Filte
     }, {})
 }
 
-function usePagination(defaultPageSize: number): { pagination: TablePaginationConfig, setTotal: (total: number) => void } {
+function usePagination(defaultPageSize: number, total: number, onChange: TablePaginationConfig['onChange']): TablePaginationConfig {
     const [current, setCurrent] = useState<number>(1)
     const [pageSize, setPageSize] = useState<number>(defaultPageSize)
-    const [total, setTotal] = useState<number>(0)
-    const onChange = useCallback<NonNullable<TablePaginationConfig['onChange']>>((page, pageSize) => {
-        if (page) {
-            setCurrent(page)
-        }
-        if (pageSize) {
+    const finalOnChange = useCallback<NonNullable<TablePaginationConfig['onChange']>>((page, pageSize) => {
+        setCurrent(page)
+        setPageSize(pageSize)
+        onChange?.(page, pageSize)
+    }, [onChange])
+
+    useEffect(() => {
+        if ((current - 1) * pageSize > total) {
             setCurrent(1)
-            setPageSize(pageSize)
         }
-    }, [])
-    const pagination = useMemo(() => ({
+    }, [current, pageSize, total])
+ 
+    return useMemo(() => ({
         current,
         pageSize,
         total,
-        onChange,
-    }), [current, pageSize, total, onChange])
-    return { pagination, setTotal}
+        onChange: finalOnChange,
+    }), [current, finalOnChange, pageSize, total])
+}
+
+function useSmartSelection<T>(totalCount: number, itemsOnPage: T[], getKey: (record: T) => React.Key, initialExceptedItems?: React.Key[]) {
+	const [defaultSelection, setDefaultSelection] = useState(false)
+	const [exceptedItems, setExceptedItems] = useState<React.Key[]>(initialExceptedItems ?? [])
+	const allIsSelected = (!defaultSelection && exceptedItems.length === totalCount) || (defaultSelection && exceptedItems.length === 0)
+	const noneIsSelected = (!defaultSelection && exceptedItems.length === 0) || (defaultSelection && exceptedItems.length === totalCount)
+
+	const setDefaultSelectionAndExceptedItems = useCallback((defaultSelection: boolean, exceptedItems: React.Key[]) => {
+		if (defaultSelection && exceptedItems.length === totalCount) {
+			setDefaultSelection(false)
+			setExceptedItems([])
+		} else if (!defaultSelection && exceptedItems.length === totalCount) {
+			setDefaultSelection(true)
+			setExceptedItems([])
+		} else {
+			setDefaultSelection(defaultSelection)
+			setExceptedItems(exceptedItems)
+		}
+	}, [totalCount])
+
+	const onSelectAll = useCallback(() => {
+		const newExceptedItems: React.Key[] = []
+		const newDefaultSelection = !allIsSelected
+        setDefaultSelectionAndExceptedItems(newDefaultSelection, newExceptedItems)
+	}, [allIsSelected, setDefaultSelectionAndExceptedItems])
+	const onSelectSingle = useCallback<SelectionSelectFn<T>>((record: T) => {
+		const key = getKey(record)
+		let newExceptedItems = exceptedItems
+		if (exceptedItems.includes(key)) {
+			newExceptedItems = exceptedItems.filter((id) => id !== key)
+		} else {
+			newExceptedItems = [...exceptedItems, key]
+		}
+		setDefaultSelectionAndExceptedItems(defaultSelection, newExceptedItems)
+	}, [getKey, exceptedItems, setDefaultSelectionAndExceptedItems, defaultSelection])
+	const onSelectMultiple = useCallback((keys: React.Key[]) => {
+		const newExceptedItems: React.Key[] = []
+		const pageItems = itemsOnPage.map((record) => getKey(record))
+		if (defaultSelection) {
+			const exceptedItemSet = new Set(exceptedItems)
+			const currentlySelectedItemsInPage = pageItems.filter((id) => exceptedItemSet.has(id))
+			const removedItems = currentlySelectedItemsInPage.filter((id) => keys.includes(id))
+			const addedItems = keys.filter((id) => !currentlySelectedItemsInPage.includes(id))
+			newExceptedItems.push(...exceptedItems.filter((id) => !addedItems.includes(id)))
+			newExceptedItems.push(...removedItems)
+		} else {
+			const currentlySelectedItemsInPage = exceptedItems.filter((id) => pageItems.includes(id))
+			const removedItems = currentlySelectedItemsInPage.filter((id) => !keys.includes(id))
+			const addedItems = keys.filter((id) => !currentlySelectedItemsInPage.includes(id))
+			newExceptedItems.push(...exceptedItems.filter((id) => !removedItems.includes(id)))
+			newExceptedItems.push(...addedItems)
+		}
+		setDefaultSelectionAndExceptedItems(defaultSelection, newExceptedItems)
+	}, [defaultSelection, exceptedItems, getKey, itemsOnPage, setDefaultSelectionAndExceptedItems])
+
+    const selectedRowKeys = useMemo(() =>
+		defaultSelection
+			? itemsOnPage.map((record) => getKey(record)).filter((key) => !exceptedItems.includes(key))
+			: exceptedItems,
+	[defaultSelection, itemsOnPage, exceptedItems, getKey])
+    const rowSelection = useMemo<TableRowSelection<T>>(() => {
+        const indeterminate = !allIsSelected && !noneIsSelected
+        return {
+            type: 'checkbox',
+            selectedRowKeys,
+            onChange(selectedRowKeys, selectedRows, info) {
+                if (info.type === 'all') {
+                    onSelectAll()
+                }
+                if (info.type === 'multiple') {
+                    // shift is held
+                    onSelectMultiple(selectedRowKeys)
+                }
+            },
+            onSelect: onSelectSingle,
+            columnTitle: (
+                <Checkbox
+                    checked={!noneIsSelected}
+                    indeterminate={indeterminate}
+                    onChange={onSelectAll}
+                />
+            )
+        }
+	}, [allIsSelected, noneIsSelected, onSelectAll, onSelectSingle, onSelectMultiple, selectedRowKeys])
+
+	const useInitialExceptedItems = useRef(Boolean(initialExceptedItems))
+	useEffect(() => {
+        setDefaultSelectionAndExceptedItems(
+            false,
+            useInitialExceptedItems.current
+            ? initialExceptedItems ?? []
+            : []
+        )
+		useInitialExceptedItems.current = false // only use initial excepted items once at the beginning
+	}, [initialExceptedItems, setDefaultSelectionAndExceptedItems])
+
+    // make sure to reset selections when filters change
+    // to avoid confusion for selectAll and selectedItems logic
+    const resetSelection = useCallback(() => {
+        setDefaultSelectionAndExceptedItems(false, [])
+    }, [setDefaultSelectionAndExceptedItems])
+
+    return useMemo(() => ({
+        defaultSelection,
+        exceptedItems,
+        rowSelection,
+        resetSelection
+    }), [defaultSelection, exceptedItems, resetSelection, rowSelection])
 }
