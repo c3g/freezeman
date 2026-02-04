88057221f28d169df0b37b0b71f4520f173ab3a7
Nafiz Islam <nafiz.islam@computationalgenomics.ca>
2026-02-04 14:09:55 -0500

    wip

--- backend/fms_core/template_importer/row_handlers/sample_rename/sample_rename.py
+++ backend/fms_core/template_importer/row_handlers/sample_rename/sample_rename.py
@@ -2,7 +2,7 @@ from typing import Set
 
 from fms_core.template_importer.row_handlers._generic import GenericRowHandler
 
-from fms_core.models import DerivedSample, Index, Sample
+from fms_core.models import DerivedBySample, Index, Sample
 from fms_core.services.library import update_library
 
 from django.db.models import Q
@@ -21,24 +21,24 @@ class SampleRenameRowHandler(GenericRowHandler):
             sq_query = Q()
             try:
                 if sample["index"]:
-                    sq_query &= Q(library__index__name=sample["index"])
+                    sq_query &= Q(derived_sample__library__index__name=sample["index"])
                 if sample["coordinates"]:
-                    sq_query &= Q(library__container__coordinate__name=sample["coordinates"])
+                    sq_query &= Q(sample__container__coordinate__name=sample["coordinates"])
                     # If coordinates are provided, barcode must also be provided to avoid ambiguity
-                    sq_query &= Q(library_container__location__barcode=sample["barcode"])
+                    sq_query &= Q(sample__container__location__barcode=sample["barcode"])
                 elif sample["barcode"]:
-                    sq_query &= Q(library__container__barcode=sample["barcode"])
+                    sq_query &= Q(sample__container__barcode=sample["barcode"])
 
-                sq_query &= Q(biosample__alias=sample["old_alias"])
+                sq_query &= Q(derived_sample__biosample__alias=sample["old_alias"])
                 
-                derived_sample = DerivedSample.objects.get(sq_query)
+                derived_by_sample = DerivedBySample.objects.get(sq_query)
 
-                derived_sample.biosample.alias = sample["new_alias"]
-                derived_sample.biosample.save()
+                derived_by_sample.derived_sample.biosample.alias = sample["new_alias"]
+                derived_by_sample.derived_sample.biosample.save()
 
-            except DerivedSample.DoesNotExist:
+            except DerivedBySample.DoesNotExist:
                 self.errors["alias"].append(f"No sample found with the provided criteria {sq_query}; please refine your criteria.")
-            except DerivedSample.MultipleObjectsReturned:
+            except DerivedBySample.MultipleObjectsReturned:
                 self.errors["alias"].append(f"Multiple samples found with the provided criteria to rename; please refine your criteria.")
 
         if not self.has_errors():
