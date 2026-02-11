95594b6febbc463841f38f4e840a2442b8534d3a
Nafiz Islam <nafiz.islam@computationalgenomics.ca>
2026-02-11 15:23:49 -0500

    BytesIOWithName instead of BytesIO

--- backend/fms_core/tests/test_template_importers/_utils.py
+++ backend/fms_core/tests/test_template_importers/_utils.py
@@ -2,12 +2,12 @@ import io
 from pathlib import Path
 import reversion
 
-from fms_core.template_importer.importers._generic import GenericImporter
+from fms_core.template_importer.importers._generic import GenericImporter, BytesIOWithName
 
 APP_DATA_ROOT = Path(__file__).parent.parent / "valid_templates"
 TEST_DATA_ROOT = Path(__file__).parent.parent / "invalid_templates"
 
-def load_template(importer: GenericImporter, file: Path | io.BytesIO):
+def load_template(importer: GenericImporter, file: Path | BytesIOWithName):
     with reversion.create_revision():
         try:
             result = importer.import_template(file=file, dry_run=False)
