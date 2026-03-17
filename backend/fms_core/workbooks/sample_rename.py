12c1375f27614078ab3b5065aceaba432e0b2110
Nafiz Islam <nafiz.islam@computationalgenomics.ca>
2026-03-17 11:49:36 -0400

    revert the use of CellRichText

--- backend/fms_core/workbooks/sample_rename.py
+++ backend/fms_core/workbooks/sample_rename.py
@@ -2,8 +2,6 @@ from typing import Sequence
 from ._generic import SheetInfo, TemplateWorkbook
 from openpyxl.styles import Color, PatternFill, Font, Alignment
 from openpyxl.cell.cell import Cell
-from openpyxl.cell.rich_text import TextBlock, CellRichText
-from openpyxl.cell.text import InlineFont
 
 from fms_core.services.workbook_utils import CD
 from fms_core.templates import SampleRenameHeaders
@@ -17,19 +15,13 @@ class SampleRenameWorkbook(TemplateWorkbook):
         def style_title(cell: Cell):
             cell.font = Font(name="Calibri", sz=15, family=2, b=True, i=False, color=Color(theme=1), scheme="minor")
 
-        OPTIONAL_PATTERN_FILL = PatternFill(start_color="d1d1d1", end_color="d1d1d1", fill_type="solid")
-        def style_optional_section(cell: Cell):
-            cell.fill = OPTIONAL_PATTERN_FILL
-
-        MANDATORY_PATTERN_FILL = PatternFill(start_color="ffa6a6", end_color="ffa6a6", fill_type="solid")
-        def style_mandatory_section(cell: Cell):
-            cell.fill = MANDATORY_PATTERN_FILL
-
         HEADER_PATTERN_FILL = PatternFill(start_color="92d050", end_color="92d050", fill_type="solid")
         def style_section_name(cell: Cell):
             cell.fill = HEADER_PATTERN_FILL
 
-        red_asterisk = TextBlock(InlineFont(rFont="Calibri", color="00ff0000"), "*")
+        def red_text(cell: Cell):
+            cell.font = Font(name="Calibri", sz=11, family=2, b=True, i=False, color=Color("ff0000"), scheme="minor")
+        red_asterisk = CD(value='*', apply_cell=red_text)
 
         self.insert_cells(
             first_cell_location=(1, 1),
@@ -39,11 +31,6 @@ class SampleRenameWorkbook(TemplateWorkbook):
                     CD(value="Sample Rename Template", apply_cell=style_title)
                 ],
                 [
-                    CellRichText(
-                        "(",
-                        red_asterisk,
-                        ") Mandatory fields",
-                    ),
                     CD(value="Version : 5.6.0"),
                 ],
                 [],
@@ -55,13 +42,13 @@ class SampleRenameWorkbook(TemplateWorkbook):
                 ],
                 [],
                 [
-                    CellRichText(red_asterisk), # barcode
+                    red_asterisk, # barcode
                     CD(value=''), # coordinates
                     CD(value=''), # index name
                     CD(value=''), # old name
                     CD(value=''), # old alias
-                    CellRichText(red_asterisk), # new name (this will be merged with the "new alias")
-                    CellRichText(red_asterisk), # new alias
+                    red_asterisk, # new name (this will be merged with the "new alias")
+                    red_asterisk, # new alias
                 ],
                 [
                     CD(
@@ -85,7 +72,6 @@ class SampleRenameWorkbook(TemplateWorkbook):
                     CD(
                         value=SampleRenameHeaders.OLD_SAMPLE_ALIAS,
                         apply_cell=style_section_name,
-                        comment="Run processing uses Alias to name samples in the files generated during run processing.",
                     ),
                     CD(
                         value=SampleRenameHeaders.NEW_SAMPLE_NAME,
@@ -109,9 +95,8 @@ class SampleRenameWorkbook(TemplateWorkbook):
         ws['F7'].alignment = center_alignment # new name
         ws['G7'].alignment = center_alignment # new alias
 
-
         for header_name in self.sheets_info[0]['headers']:
             self.set_column_width(header=header_name, width_cm=6.60, sheet_name=SHEET_NAMES[0])
 
     def headers_row_number(self, sheet_name: str | None = None) -> int:
-        return 8
+        return 7
