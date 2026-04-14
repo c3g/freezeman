13249a8aeffb0742554f5ed3aeb8a82d8c82970d
Nafiz Islam <nafiz.islam1000@gmail.com>
2026-04-14 12:35:21 -0400

    undo changes to utils (for now)

--- backend/fms_core/template_importer/_utils.py
+++ backend/fms_core/template_importer/_utils.py
@@ -1,4 +1,4 @@
-import pandas as pd
+from pandas import pandas as pd
 from typing import Any
 from fms_core.utils import float_to_decimal
 import datetime
@@ -42,8 +42,8 @@ def data_row_ids_range(starting_row, df):
     return range(starting_row, len(df))
 
 
-def panda_values_to_str_list(row_data: pd.Series):
-    return ['' if pd.isna(x) else str(x) for x in list(row_data.values)]
+def panda_values_to_str_list(row_data):
+    return ['' if x is None else str(x) for x in row_data.values.flatten().tolist()]
 
 
 def blank_and_nan_to_none(s: Any):
