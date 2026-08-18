20986e2fdfce02bc50b10151228275d421ed5eda
nabalma <nabalma_hussein@yahoo.fr>
2026-08-18 13:21:09 -0400

    Remove unused render function for external ID in ExternalProjectsPage

--- frontend/src/components/projectOverview/ExternalProjectsPage.tsx
+++ frontend/src/components/projectOverview/ExternalProjectsPage.tsx
@@ -132,7 +132,7 @@ const ExternalProjectsPage = () => {
 				(record.external_id || '')
 					.toLowerCase()
 					.includes(String(value).toLowerCase()),
-			render: (externalID: string, parentProject: FMSParentProject) => (<Link to={`/external-projects-overview/${parentProject.id}#submissions`}>{externalID}</Link>)
+			
 		},
 		{
 			title: 'External Project Name',
