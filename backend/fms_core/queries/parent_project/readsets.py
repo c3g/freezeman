1a7ddd6f764cc5fc772b925e90424a03a2a1e6bb
nabalma <nabalma_hussein@yahoo.fr>
2026-08-20 16:05:03 -0400

    get_parent_project_readsets_queryset constants are  defined within the function scope

--- backend/fms_core/queries/parent_project/readsets.py
+++ backend/fms_core/queries/parent_project/readsets.py
@@ -3,41 +3,42 @@ from fms_core.models import ParentProject, Readset
 from django.contrib.postgres.aggregates import ArrayAgg
 
 
-PARENT_PROJECT_READSET_ORDERING = [
+
+def get_parent_project_readsets_queryset(parent_project: ParentProject,):
+    PARENT_PROJECT_READSET_ORDERING = [
     "dataset__experiment_run__start_date",
     "dataset__experiment_run__name",
     "dataset__lane",
     "id",
-]
+    ]
 
-PARENT_PROJECT_READSET_VALUE_FIELDS = [
-    "id",
-    "name",
-    "average_quality",
-    "pf_reads_aligned",
-    "duplicate_aligned",
-    "readset_file_paths",
-    "readset_file_sizes",
-    "barcodes",
-    "validation_status",
-]
+    PARENT_PROJECT_READSET_VALUE_FIELDS = [
+        "id",
+        "name",
+        "average_quality",
+        "pf_reads_aligned",
+        "duplicate_aligned",
+        "readset_file_paths",
+        "readset_file_sizes",
+        "barcodes",
+        "validation_status",
+    ]
 
-PARENT_PROJECT_READSET_VALUE_ALIASES = {
-    "readset_sample_name": F("sample_name"),
-    "external_id": F("dataset__project__parent_project__external_id"),
-    "run_name": F("dataset__experiment_run__name"),
-    "lane": F("dataset__lane"),
-    "reference_genome_id": F("derived_sample__biosample__individual__reference_genome_id"),
-    "sequencing_index_name": F("derived_sample__library__index__name"),
-    "run_start_date": F("dataset__experiment_run__start_date"),
-    "alias": F("derived_sample__biosample__alias"),
-    "cohort": F("derived_sample__biosample__individual__cohort"),
-    "library_type": F("derived_sample__library__library_type__name"),
-    "number_of_reads": F("production_data__reads"),
-}
+    PARENT_PROJECT_READSET_VALUE_ALIASES = {
+        "readset_sample_name": F("sample_name"),
+        "external_id": F("dataset__project__parent_project__external_id"),
+        "run_name": F("dataset__experiment_run__name"),
+        "lane": F("dataset__lane"),
+        "reference_genome_id": F("derived_sample__biosample__individual__reference_genome_id"),
+        "sequencing_index_name": F("derived_sample__library__index__name"),
+        "run_start_date": F("dataset__experiment_run__start_date"),
+        "alias": F("derived_sample__biosample__alias"),
+        "cohort": F("derived_sample__biosample__individual__cohort"),
+        "library_type": F("derived_sample__library__library_type__name"),
+        "number_of_reads": F("production_data__reads"),
+    }
 
 
-def get_parent_project_readsets_queryset(parent_project: ParentProject,):
     return (
         Readset.objects.filter(
             dataset__project__parent_project=parent_project,
