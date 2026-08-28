76ec7afbf4f84fc0102b3be978763403f8e8f50a
nabalma <nabalma_hussein@yahoo.fr>
2026-08-28 15:23:50 -0400

    Consolidate the first production release of the external project overview with submissions, readsets, dashboards, filters, and CSV exports

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -35,6 +35,7 @@ import {
 import { AnyAction, Dispatch } from "redux"
 import { RootState } from "../store"
 import { notifyError } from "../modules/notification/actions"
+import { ProjectOverviewReadset } from "../components/projectOverview/types";
 
 const api = {
   auth: {
@@ -216,6 +217,10 @@ const api = {
       get<JsonResponse<FMSPagedResultsReponse<FMSParentProject>>>("/parent-projects/", options, {
         abort,
         requestID,
+      }),
+    readsets: (parentProjectId: FMSId,options: QueryParams,abort?: boolean,) =>
+      get<JsonResponse<FMSPagedResultsReponse<ProjectOverviewReadset>>>(`/parent-projects/${parentProjectId}/readsets/`, options, { 
+        abort
       }),
   },
 
