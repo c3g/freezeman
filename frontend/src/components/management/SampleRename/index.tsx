f61f8a5c547d9854660b442b614120d0952d2504
Nafiz Islam <nafiz.islam1000@gmail.com>
2026-09-01 11:25:39 -0400

    npx prettier --write ./src/components/management/PooledSamples/index.tsx ./src/components/management/SampleRename/index.tsx

--- frontend/src/components/management/SampleRename/index.tsx
+++ frontend/src/components/management/SampleRename/index.tsx
@@ -6,45 +6,52 @@ import api from "../../../utils/api"
 import { Alert } from "antd"
 
 const columns = [
-    PooledSampleColumnID.ALIAS,
-    PooledSampleColumnID.NAME,
-    PooledSampleColumnID.CONTAINER_BARCODE,
-    PooledSampleColumnID.COORDINATES,
-    PooledSampleColumnID.INDEX,
+  PooledSampleColumnID.ALIAS,
+  PooledSampleColumnID.NAME,
+  PooledSampleColumnID.CONTAINER_BARCODE,
+  PooledSampleColumnID.COORDINATES,
+  PooledSampleColumnID.INDEX,
 ] as const
 
-const TABLE_HEIGHT = '75vh'
+const TABLE_HEIGHT = "75vh"
 
 export function SampleRename() {
-    const dispatch = useAppDispatch()
+  const dispatch = useAppDispatch()
 
-    const [templateAction, setTemplateAction] = useState<FMSTemplateAction>()
-        useEffect(() => {
-            dispatch(api.pooledSamples.template.actions()).then(response => {
-                setTemplateAction(
-                    response.data.find(action => action.name === "Rename Sample")
-                )
-            })
-        }, [dispatch])
+  const [templateAction, setTemplateAction] = useState<FMSTemplateAction>()
+  useEffect(() => {
+    dispatch(api.pooledSamples.template.actions()).then((response) => {
+      setTemplateAction(response.data.find((action) => action.name === "Rename Sample"))
+    })
+  }, [dispatch])
 
-    const [templatePrefill, setTemplatePrefill] = useState<FMSTemplatePrefillOption>()
-        useEffect(() => {
-            dispatch(api.pooledSamples.prefill.templates()).then(response => {
-                setTemplatePrefill(
-                    response.data.find(prefill => prefill.description === "Template to rename sample (and its alias)")
-                )
-            })
-        }, [dispatch])
+  const [templatePrefill, setTemplatePrefill] = useState<FMSTemplatePrefillOption>()
+  useEffect(() => {
+    dispatch(api.pooledSamples.prefill.templates()).then((response) => {
+      setTemplatePrefill(
+        response.data.find(
+          (prefill) => prefill.description === "Template to rename sample (and its alias)",
+        ),
+      )
+    })
+  }, [dispatch])
 
-    return <PooledSamples
-        columns={columns}
-        tableHeight={TABLE_HEIGHT}
-        title={"Rename Sample"}
-        actionUrlBase={"/management/sample-rename"}
-        templateAction={templateAction}
-        templatePrefill={templatePrefill}
-        parentComponentID={"SampleRename"}
-        beforeTable={<Alert title="Pools cannot be renamed using this tool. Only individual and pooled samples." type="warning" />}
+  return (
+    <PooledSamples
+      columns={columns}
+      tableHeight={TABLE_HEIGHT}
+      title={"Rename Sample"}
+      actionUrlBase={"/management/sample-rename"}
+      templateAction={templateAction}
+      templatePrefill={templatePrefill}
+      parentComponentID={"SampleRename"}
+      beforeTable={
+        <Alert
+          title="Pools cannot be renamed using this tool. Only individual and pooled samples."
+          type="warning"
+        />
+      }
     />
+  )
 }
 export default SampleRename
