3741ee96105f7345a3b200a0d594d140873b3c84
Nafiz Islam <nafiz.islam@computationalgenomics.ca>
2026-09-21 09:58:57 -0400

    implement export_list for readset

--- backend/fms_core/viewsets/readset.py
+++ backend/fms_core/viewsets/readset.py
@@ -96,100 +96,57 @@ class ReadsetViewSet(viewsets.ModelViewSet):
             "complete_count": complete_count
         })
 
-# class ReadsetExportSerializer(ReadsetWithMetricsSerializer):
-#     sample_source_name = serializers.SerializerMethodField()
-#     biosample_alias = serializers.CharField(read_only=True, source='derived_sample.biosample.alias')
-#     cohort = serializers.CharField(read_only=True, source='derived_sample.biosample.individual.cohort')
-#     run_name = serializers.CharField(read_only=True, source='dataset.experiment_run.name')
-#     run_start_date = serializers.CharField(read_only=True, source='dataset.experiment_run.start_date')
-#     files = serializers.SerializerMethodField()
-
     @action(detail=False, methods=["get"])
     def export_list(self, _request):
         qs = self.filter_queryset(Readset.objects.all())
-        # so that library_type_distribution computes correctly
-        qs = Readset.objects.filter(id__in=qs.values_list("id", flat=True))
+        qs = queryset_for_export(qs)
+
+        return Response(qs)
 
 
-def export_queryset(queryset: QuerySet[Readset]):
-
-    PARENT_PROJECT_READSET_ORDERING = [
-        "dataset__experiment_run__start_date",
-        "dataset__experiment_run__name",
-        "dataset__lane",
-        "id",
-    ]
-
-    PARENT_PROJECT_READSET_VALUE_FIELDS = [
-        "id",
-        "name",
-        "average_quality",
-        "pf_reads_aligned",
-        "duplicate_aligned",
-        "number_of_reads",
-        "number_of_bases",
-        "readset_files",
-        "container_barcodes",
-        "run_validation_status",
-    ]
-
-    PARENT_PROJECT_READSET_VALUE_ALIASES = {
-        "readset_sample_name": F("sample_name"),
-        "biosample_id": F("derived_sample__biosample_id"),
-        "external_id": F("dataset__project__parent_project__external_id"),
-        "run_name": F("dataset__experiment_run__name"),
-        "lane": F("dataset__lane"),
-        "reference_genome_assembly_name": F("derived_sample__biosample__individual__reference_genome__assembly_name"),
-        "sequencing_index_name": F("derived_sample__library__index__name"),
-        "run_validation_status": F("validation_status"),
-        "run_start_date": F("dataset__experiment_run__start_date"),
+def queryset_for_export(queryset: QuerySet[Readset]):
+    READSET_ANNOTATIONS = {
+        "id": None,
+        "sample_name": None,
         "alias": F("derived_sample__biosample__alias"),
         "cohort": F("derived_sample__biosample__individual__cohort"),
         "library_type": F("derived_sample__library__library_type__name"),
+        "run_name": F("dataset__experiment_run__name"),
+        "run_start_date": F("dataset__experiment_run__start_date"),
+        "validation_status": None,
+        "nb_reads": Max(
+            "metrics__value_numeric",
+            filter=Q(metrics__name="nb_reads"),
+        ),
+        "avg_qual": Max(
+            "metrics__value_numeric",
+            filter=Q(metrics__name="avg_qual"),
+        ),
+        "pf_read_alignment_rate": Max(
+            "metrics__value_numeric",
+            filter=Q(metrics__name="pf_read_alignment_rate"),
+        ),
+        "duplicate_rate": Max(
+            "metrics__value_numeric",
+            filter=Q(metrics__name="duplicate_rate"),
+        ),
+        "yield": Max(
+            "metrics__value_numeric",
+            filter=Q(metrics__name="yield"),
+        ),
+        "readset_files": ArrayAgg(
+            JSONObject(
+                file_path=F("files__file_path"),
+                size=F("files__size"),
+            ),
+            filter=Q(files__isnull=False),
+            distinct=True,
+            default=Value([]),
+        ),
     }
 
     return (
         queryset
-        .annotate(
-            avg_qual=Max(
-                "metrics__value_numeric",
-                filter=Q(metrics__name="avg_qual"),
-            ),
-            pf_read_alignment_rate=Max(
-                "metrics__value_numeric",
-                filter=Q(metrics__name="pf_read_alignment_rate"),
-            ),
-            duplicate_rate=Max(
-                "metrics__value_numeric",
-                filter=Q(metrics__name="duplicate_rate"),
-            ),
-            nb_reads=Max(
-                "metrics__value_numeric",
-                filter=Q(metrics__name="nb_reads"),
-            ),
-            **{
-                "yield": Max(
-                    "metrics__value_numeric",
-                    filter=Q(metrics__name="yield"),
-                )
-            },
-            readset_files=ArrayAgg(
-                JSONObject(
-                    file_path=F("files__file_path"),
-                    size=F("files__size"),
-                ),
-                filter=Q(files__isnull=False),
-                distinct=True,
-                default=Value([]),
-            ),
-            container_barcodes=ArrayAgg(
-                "derived_sample__derived_by_samples__sample__container__barcode",
-                distinct=True,
-            ),
-        )
-        .values(
-            *PARENT_PROJECT_READSET_VALUE_FIELDS,
-            **PARENT_PROJECT_READSET_VALUE_ALIASES,
-        )
-
+        .annotate(**{k:v for k,v in READSET_ANNOTATIONS.items() if v})
+        .values(*READSET_ANNOTATIONS.keys())
     )
